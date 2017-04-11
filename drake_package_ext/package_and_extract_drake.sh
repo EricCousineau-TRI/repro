cf7693c8c70877a469b4cbf4fe6c711425d2a394
Eric Cousineau <eric.cousineau@tri.global>
2017-04-11 18:48:38 -0400

    Revert "Consider not committing artifacts, and only use git's staging area" This actually causes Git to no longer track differences. Sillyness.
    This reverts commit 1656afc555010ef449de6bcd2cb45acf566f424e.

--- drake_package_ext/package_and_extract_drake.sh
+++ drake_package_ext/package_and_extract_drake.sh
@@ -91,10 +91,17 @@ rm -rf ./*
 echo "Extract and commit current version"
 tar xfz $package
 git add -A :/
+git commit --quiet -m \
+    "Package artifacts for drake (status: $cur_git_status)" || {
+        echo "No artifact difference detected." \
+            "Skipping the rest of the rebuild.";
+        echo "$cur_git_status" > $drake_git_status_file;
+        exit 0;
+    }
 
 echo "Checkout and allow Git to handle deltas"
 echo "  (be conservative on changing timestamps)"
-git --work-tree=$install_dir checkout --quiet -f -- .
+git --work-tree=$install_dir checkout --quiet -f HEAD -- .
 
 # On success, dump the git status
 echo "$cur_git_status" > $drake_git_status_file
