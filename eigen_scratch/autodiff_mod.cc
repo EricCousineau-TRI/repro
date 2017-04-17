56dd230b5d59b9f27ec05ba4263ff6f7015964f2
Eric Cousineau <eric.cousineau@tri.global>
2017-04-17 01:38:50 -0400

    Dies on first nesting

--- eigen_scratch/autodiff_mod.cc
+++ eigen_scratch/autodiff_mod.cc
@@ -11,19 +11,25 @@ using std::endl;
 // // An autodiff variable with `num_vars` partials.
  template <int num_vars>
  using AutoDiffd = Eigen::AutoDiffScalar<double, Eigen::Matrix<double, num_vars, 1> >;
+
+
+template<int num_vars>
+using AutoDiff2d = Eigen::AutoDiffScalar<double, AutoDiffd<num_vars>>;
 /* </snippet> */
 
 #define PRINT(x) #x ": " << (x) << endl
 
 int main() {
-    AutoDiffd<1> x;
+    AutoDiff2d<1> x;
     x.value() = 4;
-    x.derivatives()(0) = 1;
+    x.derivatives()(0).value() = 1;
+    x.derivatives()(0).derivatives() = 1;
 
     auto expr = x * x;
     cout
         << PRINT(expr.value())
-        << PRINT(expr.derivatives());
+        << PRINT(expr.derivatives().value())
+        << PRINT(expr.derivatives().derivatives());
 
     return 0;
 }
