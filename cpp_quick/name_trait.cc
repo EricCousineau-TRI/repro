e12e4b739b5658dd5abf4b6ff8b4ae555cc8d5c5
Eric Cousineau <eric.cousineau@tri.global>
2017-04-17 15:04:07 -0400

    Split into header

--- cpp_quick/name_trait.cc
+++ cpp_quick/name_trait.cc
@@ -1,61 +1,18 @@
 // Goal: Provide simple display on type per template binding
 // Use variadics to simplify
+#include "name_trait.h"
 
 #include <iostream>
-#include <string>
 using std::cout;
 using std::endl;
 using std::string;
 
-template<typename T>
-struct name_trait {
-  static string name() { return "T"; }
-};
-template<typename T, typename ... Args>
-struct name_trait_list {
-  static string join(const string& delim = ", ") {
-    return name_trait<T>::name() + delim
-        + name_trait_list<Args...>::join(delim);
-  }
-};
-template<typename T>
-struct name_trait_list<T> {
-  static string join(const string& delim = ", ") {
-    return name_trait<T>::name();
-  }
-};
-// // Handle empty case?
-// template<>
-// struct name_trait_list<> {
-//   static string join(const string& delim = ", ") {
-//     return "";
-//   }
-// };
-#define NAME_TRAIT(TYPE) \
-  template<> \
-  struct name_trait<TYPE> { \
-    static string name() { return #TYPE; } \
-  };
-#define NAME_TRAIT_TPL(TYPE) \
-  template<typename ... Args> \
-  struct name_trait<TYPE<Args...>> { \
-    static string name() { \
-      return #TYPE "<" + \
-        name_trait_list<Args...>::join() + ">"; \
-      } \
-  };
-
-// Can't handle literals...
-NAME_TRAIT(int);
-NAME_TRAIT(double);
-NAME_TRAIT_TPL(name_trait);
-NAME_TRAIT_TPL(name_trait_list);
-
-#define PRINT(x) #x ": " << (x) << endl
-
 int main() {
   cout
     << PRINT(name_trait<int>::name())
-    << PRINT((name_trait<name_trait_list<int, double, name_trait<int>, name_trait_list<int, double>>>::name()));
+    << PRINT((name_trait<name_trait_list<int, double, name_trait<int>, name_trait_list<int, double>>>::name()))
+    // For unknown, should print "T"
+    << PRINT(name_trait<std::iostream>::name())
+    << PRINT(name_trait<name_trait<std::iostream>>::name());
   return 0;
 }
---
+++ cpp_quick/name_trait.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <string>
+
+
+template<typename T>
+struct name_trait {
+  static std::string name() { return "T"; }
+};
+template<typename T, typename ... Args>
+struct name_trait_list {
+  static std::string join(const std::string& delim = ", ") {
+    return name_trait<T>::name() + delim
+        + name_trait_list<Args...>::join(delim);
+  }
+};
+template<typename T>
+struct name_trait_list<T> {
+  static std::string join(const std::string& delim = ", ") {
+    return name_trait<T>::name();
+  }
+};
+// // Handle empty case?
+// template<>
+// struct name_trait_list<> {
+//   static string join(const string& delim = ", ") {
+//     return "";
+//   }
+// };
+#define NAME_TRAIT(TYPE) \
+  template<> \
+  struct name_trait<TYPE> { \
+    static std::string name() { return #TYPE; } \
+  };
+#define NAME_TRAIT_TPL(TYPE) \
+  template<typename ... Args> \
+  struct name_trait<TYPE<Args...>> { \
+    static std::string name() { \
+      return #TYPE "<" + \
+        name_trait_list<Args...>::join() + ">"; \
+      } \
+  };
+
+// Can't handle literals...
+NAME_TRAIT(int);
+NAME_TRAIT(double);
+NAME_TRAIT_TPL(name_trait);
+NAME_TRAIT_TPL(name_trait_list);
+
+
+#define PRINT(x) #x ": " << (x) << std::endl
