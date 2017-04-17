9369ed96351f412310e4a24c5f99cb3798bf7316
Eric Cousineau <eric.cousineau@tri.global>
2017-04-17 15:16:04 -0400

    Add side constexpr test

---
+++ cpp_quick/constexpr.cc
@@ -0,0 +1,7 @@
+// TODO: Figure out how to prevent linker errors with constexpr
+
+int main() {
+    // constexpr char name[] = "world";
+    // constexpr char phrase[] = "hello " name;
+    return 0;
+}
