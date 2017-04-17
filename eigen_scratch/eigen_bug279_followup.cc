98d24a3193f44f92a6403fc329291256007ca777
Eric Cousineau <eric.cousineau@tri.global>
2017-04-16 20:39:57 -0400

    Simplified implementation

--- eigen_scratch/eigen_bug279_followup.cc
+++ eigen_scratch/eigen_bug279_followup.cc
@@ -57,75 +57,61 @@ struct node<0> {
     { }
 };
 
+// Consider submitting as example for:
+// * http://eigen.tuxfamily.org/bz/show_bug.cgi?id=634
+
 // Distraction:
 // For nth order derivative, per Alexander Werner's post:
 // https://forum.kde.org/viewtopic.php?f=74&t=110376#p268948
 // and per Hongkai's TODO
-template <int order>
-struct AutoDiffNdTraits {
+template <int order, int num_vars>
+struct AutoDiffNdScalar {
     static_assert(order > 0 && order < 5, "Must have order between 1 and 4");
-    // Previous order
-    typedef AutoDiffNdTraits<order - 1> Prev;
-    template <int num_vars>
-    using PrevScalar = typename Prev::template Scalar<num_vars>;
 
-    template <int num_vars>
-    using Scalar = Eigen::AutoDiffScalar<Eigen::Matrix<PrevScalar<num_vars>, num_vars, 1> >;
-
-    // Not necessary to place these here, but keeping for simplicity
-    template <int num_vars, int rows>
-    using Vector = Eigen::Matrix<Scalar<num_vars>, rows, 1>;
-
-    typedef Vector<Eigen::Dynamic, Eigen::Dynamic> VectorXd;
+    typedef typename AutoDiffNdScalar<order - 1, num_vars>::type prev_type;
+    typedef Eigen::AutoDiffScalar<Eigen::Matrix<prev_type, num_vars, 1> > type;
 };
 
-template<>
-struct AutoDiffNdTraits<1> {
-    template <int num_vars>
-    using Scalar = AutoDiffd<num_vars>;
-    template <int num_vars, int rows>
-    using Vector = AutoDiffVecd<num_vars, rows>;
-    using VectorXd = AutoDiffVecXd;
+// Base Case
+template<int num_vars>
+struct AutoDiffNdScalar<0, num_vars> {
+    using type = double;
 };
-/* Alternative:
-template<>
-struct AutoDiffNdTraits<0> {
-    template <int nm_vars>
-    using Scalar = double;
-};
-*/
+// Alternative: AutoDiffNdScalar<1, num_vars> = AutoDiffd<num_vars>
 
 template <int order, int num_vars>
-using AutoDiffNd = typename AutoDiffNdTraits<order>::template Scalar<num_vars>;
+using AutoDiffNd = typename AutoDiffNdScalar<order, num_vars>::type;
 
 template <int order, int num_vars, int rows>
-using AutoDiffNVecd = typename AutoDiffNdTraits<order>::template Vector<num_vars, rows>;
+using AutoDiffNVecd = Eigen::Matrix<AutoDiffNd<order, num_vars>, rows, 1>;
 
 template <int order>
-using AutoDiffNVecXd = typename AutoDiffNdTraits<order>::VectorXd;
-
-// template <int order, int num_vars, int rows>
-// using AutoDiffNVecd = Eigen::Matrix<AutoDiffNd<order, num_vars>, rows, 1>;
-
-// template <int order>
-// typedef AutoDiffNVecd<order, Eigen::Dynamic, Eigen::Dynamic> AutoDiffNVecXd;
-
+using AutoDiffNVecXd = AutoDiffNVecd<order, Eigen::Dynamic, Eigen::Dynamic>;
 
 #define PRINT(x) #x ": " << (x) << endl
 
 int main() {
     auto pi = numeric_const<double>::pi;
     AutoDiffNVecXd<2> x_taylor(1);
-    /* Storage:
-        [ x   dx  ]
-        [ dx  ddx ]
+    /*
+    Layout for AutoDiffNd<2>
+
+            0   1
+        0 [ x   dx  ]
+        1 [ dx  ddx ]
+
+        0 - value(), 1 - derivative()
+
+    Investigate minimizing redundancy via:
+    * https://en.wikipedia.org/wiki/Automatic_differentiation#High_order_and_many_variables
+
     */
     auto& x = x_taylor(0).value().value();
     // First order
     auto& deriv = x_taylor(0).derivatives();
     deriv.resize(1);
     auto& xdot = deriv(0).value();
-    // Symmetric derivative (maintain tree structure...)
+    // Symmetric derivative
     auto& deriv_sym = x_taylor(0).value().derivatives();
     deriv_sym.resize(1);
     auto& xdot_sym = deriv_sym(0);
