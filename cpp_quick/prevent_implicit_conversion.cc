39978e0da4dbbfcd8db90952702a80bcfa5fea3d
Eric Cousineau <eric.cousineau@tri.global>
2017-04-17 15:21:10 -0400

    Finish implementation, record desired behavior.

--- cpp_quick/prevent_implicit_conversion.cc
+++ cpp_quick/prevent_implicit_conversion.cc
@@ -29,6 +29,15 @@ void overload_info(T&& x) {
   cout << "overload: " << name_trait<T>::name() << "&&" << endl;
 }
 
+template<typename T>
+void disable_implicit_copy(T&& x) = delete;
+
+template<typename T>
+void disable_implicit_copy(const T& x) {
+  cout << "[valid] ";
+  overload_info<T>(x);
+}
+
 // Based on: drake-distro:drake/solvers/binding.h
 // Motivation: Will provide implicit copy conversion
 typedef vector<string> VarList;
@@ -96,10 +105,44 @@ int main() {
   Binding<LinearConstraint> blc(&lc, {});
   Binding<QuadraticConstraint> bqc(&qc, {});
 
-  CALL(overload_info(bc));
+  CALL(overload_info(bc)); // Triggers copy???
   CALL(overload_info<Binding<Constraint>>(bc));
   CALL(overload_info<Binding<LinearConstraint>>(blc));
 
   CALL(overload_info<Binding<Constraint>>(blc));
   CALL(overload_info<Binding<Constraint>>(bqc));
+
+  CALL(disable_implicit_copy<Binding<Constraint>>(bc));
+  // // Causes desired error
+  // CALL(disable_implicit_copy<Binding<Constraint>>(blc));
 }
+
+/* output
+
+>>> overload_info(bc)
+overload: T&&
+
+>>> overload_info<Binding<Constraint>>(bc)
+overload: const Binding<Constraint>&
+
+>>> overload_info<Binding<LinearConstraint>>(blc)
+overload: const Binding<LinearConstraint>&
+
+>>> overload_info<Binding<Constraint>>(blc)
+implicit copy: Binding<LinearConstraint>  ->  Binding<Constraint>
+overload: Binding<Constraint>&&
+
+>>> overload_info<Binding<Constraint>>(bqc)
+implicit copy: Binding<QuadraticConstraint>  ->  Binding<Constraint>
+overload: Binding<Constraint>&&
+
+>>> disable_implicit_copy<Binding<Constraint>>(bc)
+[valid] overload: const Binding<Constraint>&
+*/
+
+/*
+error (when uncommented):
+cpp_quick/prevent_implicit_conversion.cc:116:8: error: call to deleted function 'disable_implicit_copy'
+  CALL(disable_implicit_copy<Binding<Constraint>>(blc));
+       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
