fd5e2393d4f76590753395810aebe06f0442b188
Eric Cousineau <eric.cousineau@tri.global>
2017-04-17 01:09:52 -0400

    Start testing

---
+++ eigen_scratch/autodiff_mod.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using std::cout;
+using std::endl;
+
+#include <Eigen/Dense>
+// // #include <unsupported/Eigen/AutoDiff>
+// #include "AutoDiffScalarMod.h"
+
+/* <snippet from="drake-distro:5729940:drake/common/eigen_autodiff_types.h"> */
+// // An autodiff variable with `num_vars` partials.
+// template <int num_vars>
+// using AutoDiffd = Eigen::AutoDiffScalar<double, Eigen::Matrix<double, num_vars, 1> >;
+/* </snippet> */
+
+int main() {
+    return 0;
+}
