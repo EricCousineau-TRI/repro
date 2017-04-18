f0a3856c09e2666e8f18db7c82e7f430def139a4
Eric Cousineau <eric.cousineau@tri.global>
2017-04-18 16:00:45 -0400

    Need to rename this to 'template_spec_return_type'

--- cpp_quick/template_specialization.cc
+++ cpp_quick/template_specialization.cc
@@ -1,3 +1,15 @@
-#include "name_trait.h"
+#include "template_specialization.h"
 
+// [auto / delctype(auto), defined_in_source]
 
+// // + [spec_return_auto]
+// //     Does not work. (Inference does not work)
+// template<>
+// auto Test::tpl_method_auto<int>(const int& x) {
+//     return std::string("int -> string");
+// }
+
+// template<>
+// auto Test::tpl_method_auto<double>(const double& x) {
+//     return 2 * x;
+// }
--- cpp_quick/template_specialization.h
+++ cpp_quick/template_specialization.h
@@ -4,27 +4,41 @@
 
 class Test {
 public:
-    /*
-    // Does not work
+    
+    // // [decltype_auto]
+    // //   Only works if specializations of 'decltype(auto)'
+    // template<typename T>
+    // decltype(auto) tpl_method_decltype_auto(const T& x);
+
+    // [auto]
+    //   Only works if specializations have 'auto'
     template<typename T>
-    decltype(auto) tpl_method(const T& x);
+    auto tpl_method_auto(const T& x);
 
-    template<>
-    std::string tpl_method<int>(const int& x) {
-        return "int";
-    }
+    template<typename T> struct args { using type = T; };
+    template<> struct args<int> { using return_type = std::string; }
 
-    template<>
-    auto tpl_method<double>(const double& x) {
-        return 2 * x;
-    }
-    */
-
-    // Does work
     template<typename T>
-    auto tpl_method(const T& x);
+    args<T>::return_type tpl_method_explicit(const T& x);
 };
 
+// [auto / decltype_auto, defined_in_header]
+
+// // + [spec_return_specific]
+// //     Does not work
+// template<>
+// std::string Test::tpl_method_decltype_auto<int>(const int& x) {
+//     return "int";
+// }
+
+// template<>
+// double Test::tpl_method_decltype_auto<double>(const double& x) {
+//     return 2 * x;
+// }
+
+
+// + [spec_return_auto]
+//     Works
 template<>
 auto Test::tpl_method<int>(const int& x) {
     return std::string("int -> string");
@@ -34,3 +48,5 @@ template<>
 auto Test::tpl_method<double>(const double& x) {
     return 2 * x;
 }
+
+
