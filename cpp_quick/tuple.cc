d099c52a7a1037637c82d31cf33b3e061d344b0f
Eric Cousineau <eric.cousineau@tri.global>
2017-04-18 23:08:03 -0400

    Reorganize. Alt 2 not working.

--- cpp_quick/tuple.cc
+++ cpp_quick/tuple.cc
@@ -20,6 +20,7 @@ using std::endl;
 
 // http://stackoverflow.com/a/31044718/7829525
 
+
 // From potential implementations for C++17
 namespace future {
 /* <snippet from="http://en.cppreference.com/w/cpp/utility/apply"> */
@@ -31,19 +32,8 @@ constexpr decltype(auto) apply_impl(F &&f,
     return f(std::get<I>(std::forward<Tuple>(t))...);
 }
 
-template <class F, class Tuple, std::size_t... I>
-constexpr decltype(auto) apply_reversed_impl(F &&f,
-    Tuple &&t, std::index_sequence<I...>) 
-{
-    // Reversed
-    constexpr std::size_t back_index = sizeof...(I) - 1;
-    return f(std::get<back_index - I>(std::forward<Tuple>(t))...);
-}
-
 }  // namespace detail
 
-// TODO(eric.cousineau): Figure out how 
- 
 template <class F, class Tuple>
 constexpr decltype(auto) apply(F &&f, Tuple &&t) 
 {
@@ -53,47 +43,67 @@ constexpr decltype(auto) apply(F &&f, Tuple &&t)
             std::tuple_size<std::decay_t<Tuple>>::value>{});
 }
 
+/* </snippet> */
+}
+
+//// Alternative 1: Use folded expressions
+template <class F, class Tuple, std::size_t... I>
+constexpr decltype(auto) apply_reversed_impl(F &&f,
+    Tuple &&t, std::index_sequence<I...>) 
+{
+    // Reversed
+    constexpr std::size_t back_index = sizeof...(I) - 1;
+    return f(std::get<back_index - I>(std::forward<Tuple>(t))...);
+}
+
 template <class F, class Tuple>
 constexpr decltype(auto) apply_reversed(F &&f, Tuple &&t) 
 {
-    return detail::apply_reversed_impl(
+    return apply_reversed_impl(
         std::forward<F>(f), std::forward<Tuple>(t),
         std::make_index_sequence<
             std::tuple_size<std::decay_t<Tuple>>::value>{});
 }
 
-/* </snippet> */
-}
 
-/*
+//// Alternative 2: Use reversed sequences
+
 // http://stackoverflow.com/a/31044718/7829525
-template<unsigned N, unsigned... Indices>
+template<unsigned N, unsigned... I>
 struct reversed_index_sequence
-    : reversed_index_sequence<N - 1, Indices..., N - 1>
+    : reversed_index_sequence<N - 1, I..., N - 1>
 {};
-template<unsigned... Indices>
-struct reversed_index_sequence<0, Indices...>
-    : std::index_sequence<Indices...>{};
+template<unsigned... I>
+struct reversed_index_sequence<0, I...>
+    : std::index_sequence<I...>{};
 
-auto get_reversed
-template<std::size_t N, unsigned... Indices>
-template reversed {
-    auto get() {
+template<std::size_t N>
+struct reversed_index_sequence_trait {
+    template <std::size_t... I>
+    static auto get_reversed(std::index_sequence<I...>) {
+        return reversed_index_sequence<I...>{};
     }
+    using type = decltype(get_reversed(std::make_index_sequence<N>{}));
 };
 
 template<std::size_t N>
-using make_reversed_index_sequence = reversed_index_sequence<N>;
+using make_reversed_index_sequence 
+    = typename reversed_index_sequence_trait<N>::type;
 
 template <class F, class Tuple>
-constexpr decltype(auto) apply_reversed(F &&f, Tuple &&t) 
+constexpr decltype(auto) apply_reversed_alt(F &&f, Tuple &&t) 
 {
-    return detail::apply_impl(
+    return future::detail::apply_impl(
         std::forward<F>(f), std::forward<Tuple>(t),
         make_reversed_index_sequence<
             std::tuple_size<std::decay_t<Tuple>>::value>{});
 }
-*/
+
+
+
+//// Alternative 3: Reverse tuple, then use future::apply
+// @ref http://stackoverflow.com/questions/25119048/reversing-a-c-tuple
+// TODO(eric.cousineau): Try this out... Maybe
 
 
 double func(int x, double y) {
@@ -115,7 +125,8 @@ int main() {
 
     auto t = std::make_tuple(1, 2.0);
     future::apply(func_callable, t);
-    future::apply_reversed(func_callable, t);
+    apply_reversed(func_callable, t);
+    apply_reversed_alt(func_callable, t);
 
     cout << endl;
     return 0;
