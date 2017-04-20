4faa201b1efa3f84145a083b44a929b6a03d9c8d
Eric Cousineau <eric.cousineau@tri.global>
2017-04-20 01:01:17 -0400

    Clean up naming

--- cpp_quick/tpl_spec_greedy.cc
+++ cpp_quick/tpl_spec_greedy.cc
@@ -1,4 +1,5 @@
-// Goal: Ens
+// Goal: Use template specialization to greedy match against a
+// parameter pack base definition
 
 #include <iostream>
 #include <string>
@@ -9,6 +10,7 @@ using std::string;
 using std::cout;
 using std::endl;
 
+// Base case
 template<typename T, typename Arg>
 struct is_compat : public std::false_type { };
 
@@ -16,16 +18,18 @@ struct is_compat : public std::false_type { };
 template<typename T>
 struct is_compat<T, T> : public std::true_type { };
 
+// Simple alias
 template<typename T>
 using compat_decay_t = std::remove_cv_t<std::decay_t<T>>;
 
+// enable_if specific to compatibility
 template<typename T, typename Arg, typename Result = void>
-struct compat_enable : std::enable_if<
+struct enable_if_compat : std::enable_if<
     is_compat<T, compat_decay_t<Arg>>::value, Result>
 { };
 
 template<typename T, typename Arg>
-using compat_enable_t = typename compat_enable<T, Arg>::type;
+using enable_if_compat_t = typename enable_if_compat<T, Arg>::type;
 
 // Must explicitly specialize
 template<>
@@ -33,8 +37,12 @@ struct is_compat<string, char*> : public std::true_type { };
 template<>
 struct is_compat<string, const char*> : public std::true_type { };
 
-template<>
-struct is_compat<double, int> : public std::true_type { };
+// Or:
+#define ADD_COMPAT(T, Arg) \
+    template<> struct is_compat<T, Arg> : public std::true_type { };
+
+ADD_COMPAT(double, int);
+ADD_COMPAT(double, uint);
 
 
 template<typename ... Args>
@@ -44,7 +52,7 @@ void my_func(Args&& ... args) {
 
 // Use template with enable_if to catch as many types as possible
 template<typename T1,
-    typename C1 = compat_enable_t<string, T1>>
+    typename C1 = enable_if_compat_t<string, T1>>
 void my_func(int y, T1&& z) {
     cout << "my_func<cond>(int, " << name_trait<decltype(z)>::name() << ")" << endl;
     cout << "  decay_t = " << name_trait<compat_decay_t<T1>>::name() << endl;
@@ -52,7 +60,8 @@ void my_func(int y, T1&& z) {
 
 // Example using multiple types (let compiler handle the combinatorics)
 template<typename T1, typename T2,
-    typename C1 = compat_enable_t<string, T1>, typename C2 = compat_enable_t<double, T2>>
+    typename C1 = enable_if_compat_t<string, T1>,
+    typename C2 = enable_if_compat_t<double, T2>>
 void my_func(int y, T1&& z, T2&& zz) {
     cout
         << "my_func<cond_2>(int, "
@@ -69,7 +78,7 @@ void my_func(int y, T1&& z, T2&& zz) {
 
 // // Cannot use condition as argument type
 // template<typename T>
-// void my_func(int y, compat_enable_t<string, T>&& z) {
+// void my_func(int y, enable_if_compat_t<string, T>&& z) {
 //     cout << "4. my_func(int, " << name_trait<T>::name() << ")" << endl;
 // }
 
