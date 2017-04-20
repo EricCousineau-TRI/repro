c5ec29faeb74d24d83130fb5017e151d6e19ef19
Eric Cousineau <eric.cousineau@tri.global>
2017-04-20 00:16:09 -0400

    Matches if relaxed

--- cpp_quick/non_greedy.cc
+++ cpp_quick/non_greedy.cc
@@ -9,22 +9,47 @@ using std::string;
 using std::cout;
 using std::endl;
 
-template<typename ... Args>
-struct pack { };
-NAME_TRAIT_TPL(pack);
+// By default, incompatible
+template<typename T, typename Arg>
+struct compat { using type = Arg; };
+
+template<typename T, typename Arg>
+using compat_t = typename compat<T, Arg>::type;
+
+template<typename T, typename Arg>
+using compat_enable_t = typename std::enable_if<
+    std::is_same<compat_t<T, Arg>, Arg>::value, Arg>::type;
+
+template<>
+struct compat<string, const char[]> { using type = const char[]; };
+template<std::size_t N>
+struct compat<string, const char[N]> { using type = const char[N]; };
+template<>
+struct compat<string, const char*> { using type = const char*; };
 
 template<typename ... Args>
 void my_func(Args&& ... args) {
-    using my_pack = pack<Args&&...>;
-    cout << "1. my_func variadic: " << name_trait<my_pack>::name() << endl;
+    cout << "1. my_func<variadic>(" << name_trait_list<Args&&...>::join() << ")" << endl;
 }
 
 // void my_func(const int& x, const string& y) {
 //     cout << "2. my_func(const int&, const string&)" << endl;
 // }
 
+// Does not catch string literal...
 void my_func(int y, string&& z) {
     cout << "3. my_func(int, string&&)" << endl;
+}
+
+// // Still does not catch string literal...
+// template<typename T>
+// void my_func(int y, compat_enable_t<string, T>&& z) {
+//     cout << "4. my_func(int, " << name_trait<T>::name() << ")" << endl;
+// }
+
+template<typename T, typename Cond = compat_enable_t<string, T>>
+void my_func(int y, T&& z) {
+    cout << "4. my_func<cond>(int, " << name_trait<T>::name() << ")" << endl;
 }
 
 int main() {
