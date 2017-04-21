c6370cdcc46090fd99550e3d5ba94efbf727d2c3
Eric Cousineau <eric.cousineau@tri.global>
2017-04-21 17:39:39 -0400

    Enable general recursion to specialize only one portion if needed

--- cpp_quick/tpl_check_errors.cc
+++ cpp_quick/tpl_check_errors.cc
@@ -17,10 +17,12 @@ struct is_good<double[2]> : std::false_type { };
 template<typename T, typename... Args>
 struct is_all_good_unfriendly {
     static constexpr bool value =
-        is_good<T>::value ? is_all_good_unfriendly<Args...>::value : false;
+        is_all_good_unfriendly<T>::value ?
+            is_all_good_unfriendly<Args...>::value : false;
 };
 template<typename T>
-struct is_all_good_unfriendly<T> : is_good<T> { };
+struct is_all_good_unfriendly<T>
+    : is_good<T> { };
 
 
 // Ensure that name_trait_list is unfolded
