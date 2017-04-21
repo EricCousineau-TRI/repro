0ccf0efed212f7e7450f888ab79a0c2c14fff277
Eric Cousineau <eric.cousineau@tri.global>
2017-04-21 17:39:56 -0400

    Was not able to easily achieve failure. Need to remove references.

--- cpp_quick/tpl_check_errors.cc
+++ cpp_quick/tpl_check_errors.cc
@@ -34,20 +34,23 @@ struct is_good<name_trait_list<Args...>>
 template<typename... Args> 
 typename std::enable_if<is_all_good_unfriendly<Args...>::value, void>::type
 my_func(Args&&... args) {
-    cout << name_trait_list<Args...>::join() << endl;
+    cout
+        << name_trait_list<typename std::remove_reference<Args>::type...>::join()
+        << endl;
 }
 
 int main() {
+    double array[2] = {2.0, 3.0};
     // Compilable:
     my_func(int{},
         string{},
         name_trait_list<int,string,double> {},
+        array,
         2.0);
     // Output:
     // int, std::string, name_trait_list<int, std::string, double>, double
-
-    double array[2] = {2.0, 3.0};
-    my_func(array);
+    
+    // my_func<double[2]>(array);
     cout
         << is_good<double[2]>::value << endl
         << is_all_good_unfriendly<double[2]>::value << endl;
