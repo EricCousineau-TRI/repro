2115e664b4ae54ae1f3821e237d439cf3676d8c4
Eric Cousineau <eric.cousineau@tri.global>
2017-04-21 17:47:04 -0400

    Add in error message

--- cpp_quick/tpl_check_errors.cc
+++ cpp_quick/tpl_check_errors.cc
@@ -14,23 +14,38 @@ struct is_good : std::true_type { };
 template<>
 struct is_good<double[2]> : std::false_type { };
 
+// Unfriendly version: just return false
+template<typename T>
+struct is_good_decay
+    : is_good<typename std::remove_reference<T>::type>
+{ };
+/*
+Error:
+cpp_quick/tpl_check_errors.cc:50:5: error: no matching function for call to 'my_func'
+    my_func(int{},
+    ^~~~~~~
+cpp_quick/tpl_check_errors.cc:40:25: note: candidate template ignored: disabled by 'enable_if' [with Args = <int, std::__cxx11::basic_string<char>, name_trait_list<int, std::__cxx11::basic_string<char>, double>, double (&)[2], double>]
+typename std::enable_if<is_all_good<Args...>::value, void>::type
+                        ^
+*/
+
 template<typename T, typename... Args>
-struct is_all_good_unfriendly {
+struct is_all_good {
     static constexpr bool value =
-        is_all_good_unfriendly<T>::value ?
-            is_all_good_unfriendly<Args...>::value : false;
+        is_all_good<T>::value ?
+            is_all_good<Args...>::value : false;
 };
 template<typename T>
-struct is_all_good_unfriendly<T>
-    : is_good<typename std::remove_reference<T>::type> { };
+struct is_all_good<T>
+    : is_good_decay<T> { };
 
 // Ensure that name_trait_list is unfolded
 template<typename... Args>
 struct is_good<name_trait_list<Args...>>
-    : is_all_good_unfriendly<Args...> { };
+    : is_all_good<Args...> { };
 
 template<typename... Args> 
-typename std::enable_if<is_all_good_unfriendly<Args...>::value, void>::type
+typename std::enable_if<is_all_good<Args...>::value, void>::type
 my_func(Args&&... args) {
     cout
         << name_trait_list<typename std::remove_reference<Args>::type...>::join()
@@ -51,6 +66,6 @@ int main() {
     // my_func<double[2]>(array);
     cout
         << is_good<double[2]>::value << endl
-        << is_all_good_unfriendly<double[2]>::value << endl;
+        << is_all_good<double[2]>::value << endl;
     return 0;
 }
