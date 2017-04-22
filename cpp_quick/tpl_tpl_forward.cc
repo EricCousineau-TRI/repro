5547616569666fe0f767a7e29dd8f38900be6676
Eric Cousineau <eric.cousineau@tri.global>
2017-04-22 17:18:41 -0400

    Possibly found an (esoteric) bug

--- cpp_quick/tpl_tpl_forward.cc
+++ cpp_quick/tpl_tpl_forward.cc
@@ -6,26 +6,33 @@
 using std::cout;
 using std::endl;
 
+// Use pack to store / access parameters
 template<typename T, typename... Args>
 struct pack {
     using first = T;
     using subpack = pack<Args...>;
-    static constexpr std::size_t subsize = sizeof...(Args);
+    static constexpr std::size_t size = 1 + sizeof...(Args);
 };
 template<typename T>
 struct pack<T> {
     using first = T;
+    static constexpr std::size_t size = 1;
 };
 
+// Base Case
 template<typename T>
 struct is_nested : std::false_type { };
 
-template<template <typename> class T, typename A>
-struct is_nested<T<A>> : std::true_type {
-    template<typename B>
-    using outer_type = T<B>;
-    using inner_type = A;
+// General Case
+template<template <typename...> class T, typename A, typename... Args>
+struct is_nested<T<A, Args...>> : std::true_type {
+    using pack = pack<A, Args...>;
+
+    template<typename B, typename... BArgs>
+    using outer_type = T<B, BArgs...>;
+    using first_inner_type = A;
 };
+
 
 // Helper 1: Connect to enable_if
 template<typename T>
@@ -40,19 +47,21 @@ using enabel_if_nested_decay = enable_if_nested<
         typename std::decay<T>::type
         >;
 
+
 // Put it into use
 template <typename T_A, typename Result =
     typename enabel_if_nested_decay<T_A>::type>
 decltype(auto) f (T_A&& t)
 {
     // Blech, but necessary
-    using A = typename Result::inner_type;
+    using A = typename Result::first_inner_type;
     // // Cannot use template aliases directly :(
     // // @ref http://stackoverflow.com/q/34419603/170413
     // using T = typename Info::outer_type;
 
     cout << "t.bar = " << t.bar << endl;
     cout << " - default inner: " << A() << endl;
+    cout << " - pack size: " << Result::pack::size << endl;
 
     // Reinstantiate the class with inner type double
     using T_double = typename Result::template outer_type<double>;
@@ -68,17 +77,26 @@ struct foo
     A bar;
 };
 
+template<typename A, typename B>
+struct baz
+{
+    A bar;
+    B boo;
+};
+
 int main() {
     struct foo<int>        x1 { .bar = 1 };
     struct foo<int> const  x2 { .bar = 2 };
     struct foo<int> &      x3 = x1;
     struct foo<int> const& x4 = x2;
+    // struct baz<int, double> const&  x6 = { .bar = 1, .boo = 1.5 };
 
     auto r1 = f (x1);
     auto r2 = f (x2);
     auto r3 = f (x3);
     auto r4 = f (x4);
-    auto r5 = f (foo<double> {3.5}); // only rvalue works
+    auto r5 = f (foo<double> {3.5});
+    // auto r6 = f (x6);
 
     return 0;
 }
