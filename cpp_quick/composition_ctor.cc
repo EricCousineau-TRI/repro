3cdc5231f7b050d89a088d50cc45aac6c48075d4
Eric Cousineau <eric.cousineau@tri.global>
2017-04-22 03:52:57 -0400

    Trying alternatives

--- cpp_quick/composition_ctor.cc
+++ cpp_quick/composition_ctor.cc
@@ -25,26 +25,27 @@ protected:
 
 class Vars {
 public:
-    // inline Vars(const string& s)
-    //     : vars_{{s}}
-    // { }
-    // inline Vars(const Var& var)
-    //     : vars_{var}
-    // { }
+    inline Vars(const string& s)
+        : vars_{{s}}
+    { }
+    inline Vars(const Var& var)
+        : vars_{var}
+    { }
     template<typename T>
     inline Vars(const vector<T>& items)
     {
         for (const auto& item : items)
             add_var(item);
     }
+    // Explicit intiailize_list
     inline Vars(std::initializer_list<string> list)
         : Vars(vector<string>(list))
     { }
 protected:
     vector<Var> vars_;
-    void add_var(const Var& v) {
-        vars_.push_back(v);
-    }
+    // void add_var(const Var& v) {
+    //     vars_.push_back(v);
+    // }
     void add_var(const Vars& vs) {
         for (const auto& v : vs.vars_)
             vars_.push_back(v);
@@ -54,6 +55,7 @@ protected:
 int main() {
     Var var {"a"};
     Vars vars = {"a", "b"}; // {"a"} does not work
+    // Vars vars_nest = vector<Vars>({vars, vars, "a"});
     // Vars vars2("a"); // does not work
     return 0;
 }
