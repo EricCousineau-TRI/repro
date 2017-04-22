66310c37110c130000f8612b724d90d5a6c0a9ba
Eric Cousineau <eric.cousineau@tri.global>
2017-04-21 23:17:34 -0400

    Identified workaround.

--- bug/stdcpp_4.9_is_convertible/test.cc
+++ bug/stdcpp_4.9_is_convertible/test.cc
@@ -26,11 +26,50 @@ Work around this problem by providing an explicit overload for
 #include <type_traits>
 #include <memory>
 #include <iostream>
+
+using std::cout;
+using std::endl;
+
 struct A {};
 struct B {};
+
+template<typename A, typename B>
+struct workaround
+    : std::is_convertible<A, B> { };
+template<typename A, typename B>
+struct workaround<std::unique_ptr<A>, std::shared_ptr<B>>
+    : std::is_convertible<A*, B*> { };
+
+template<typename T1, typename T2>
+void check() {
+  cout
+    << "is_convertible: " << std::is_convertible<T1, T2>::value << endl
+    << "workaround: " << workaround<T1, T2>::value << endl
+    << endl;
+}
+
 int main() {
-  bool bad = std::is_convertible<
-        std::unique_ptr<A>, std::shared_ptr<B>>::value;
-  std::cout << "Bad: " << bad << std::endl;
-  return bad ? 1 : 0;
+  using APtr = A*;
+  using AUPtr = std::unique_ptr<A>;
+  using ASPtr = std::shared_ptr<A>;
+  using BPtr = B*;
+  using BUPtr = std::unique_ptr<B>;
+  using BSPtr = std::shared_ptr<B>;
+
+  check<APtr, BPtr>();
+  check<APtr, BUPtr>();
+  check<APtr, BSPtr>();
+  cout << endl;
+
+  check<AUPtr, BPtr>();
+  check<AUPtr, BUPtr>();
+  check<AUPtr, BSPtr>();
+  cout << endl;
+
+  check<ASPtr, BPtr>();
+  check<ASPtr, BUPtr>();
+  check<ASPtr, BSPtr>();
+  cout << endl;
+
+  return 0;
 }
