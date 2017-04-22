78d53c45d14c4e11f0c34438a536edc1ec8fbdac
Eric Cousineau <eric.cousineau@tri.global>
2017-04-22 17:02:12 -0400

    Works as expected

--- cpp_quick/tpl_tpl_forward.cc
+++ cpp_quick/tpl_tpl_forward.cc
@@ -7,10 +7,7 @@ using std::cout;
 using std::endl;
 
 template<typename T>
-struct is_nested : std::false_type {
-    using outer_type = void;
-    using inner_type = void;
-};
+struct is_nested : std::false_type { };
 
 template<template <typename> class T, typename A>
 struct is_nested<T<A>> : std::true_type {
@@ -19,32 +16,37 @@ struct is_nested<T<A>> : std::true_type {
     using inner_type = A;
 };
 
-//
-// It **seems** that the templated type T<A> should
-// behave the same as an bare type T with respect to
-// universal references, but this is not the case.
-//
-template <typename T_A,
-    typename T_A_rr = typename std::remove_reference<T_A>::type,
-    typename Result =
-    typename std::enable_if<
-        is_nested<T_A_rr>::value,
-        is_nested<T_A_rr> // Return info so that we can extract it
-        >::type>
+// Helper 1: Connect to enable_if
+template<typename T>
+using enable_if_nested = std::enable_if<
+    is_nested<T>::value,
+    is_nested<T> // Return info so that we can extract it
+    >;
+
+// Helper 2: Connect to std::decay to remove const-ref stuff (will do other simplifications too)
+template<typename T>
+using enabel_if_nested_decay = enable_if_nested<
+        typename std::decay<T>::type
+        >;
+
+// Put it into use
+template <typename T_A, typename Result =
+    typename enabel_if_nested_decay<T_A>::type>
 decltype(auto) f (T_A&& t)
 {
     // Blech, but necessary
     using A = typename Result::inner_type;
     // // Cannot use template aliases directly :(
-    // template<typename B>
-    // using T = Result::outer_type<B>;
+    // // @ref http://stackoverflow.com/q/34419603/170413
+    // using T = typename Info::outer_type;
 
-    cout << "Default inner: " << A() << endl;
+    cout << "t.bar = " << t.bar << endl;
+    cout << " - default inner: " << A() << endl;
 
-    // Reinstantiate the class of type double
-    // using T_double = typename Result::template outer_type<double>;
-    // T_double c { .bar = 0.5 };
-    // cout << "T_double: " << c.bar << endl;
+    // Reinstantiate the class with inner type double
+    using T_double = typename Result::template outer_type<double>;
+    T_double c { .bar = 0.5 * t.bar };
+    cout << "T_double: " << c.bar << endl;
     // Permit da forwarding
     return std::forward<T_A>(t);
 }
@@ -57,15 +59,15 @@ struct foo
 
 int main() {
     struct foo<int>        x1 { .bar = 1 };
-//     struct foo<int> const  x2 { .bar = 1 };
-//     struct foo<int> &      x3 = x1;
-//     struct foo<int> const& x4 = x2;
+    struct foo<int> const  x2 { .bar = 2 };
+    struct foo<int> &      x3 = x1;
+    struct foo<int> const& x4 = x2;
 
-//     // all calls to `f` **fail** to compile due
-//     // to **unsuccessful** binding of T&& to the required types
     auto r1 = f (x1);
-//     auto r2 = f (x2);
-//     auto r3 = f (x3);
-//     auto r4 = f (x4);
-//     auto r5 = f (foo<int> {1}); // only rvalue works
+    auto r2 = f (x2);
+    auto r3 = f (x3);
+    auto r4 = f (x4);
+    auto r5 = f (foo<double> {3.5}); // only rvalue works
+
+    return 0;
 }
