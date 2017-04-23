84afc05586be1fb9d8f3f4befd20380f65758b0a
Eric Cousineau <eric.cousineau@tri.global>
2017-04-23 00:30:24 -0400

    Start test case

--- eigen_scratch/matrix_stack.cc
+++ eigen_scratch/matrix_stack.cc
@@ -43,3 +43,38 @@ Achievable via:
 
 */
 
+#include "cpp_quick/name_trait.h"
+
+#include <string>
+#include <iostream>
+
+#include <Eigen/Dense>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+using MatrixXc = Eigen::Matrix<char, Eigen::Dynamic, Eigen::Dynamic>;
+
+int main() {
+    MatrixXc A(1, 2), B(1, 2),
+        C(2, 1),
+        D(1, 3), E(1, 3),
+        F(2, 6);
+    A.setConstant('A');
+    B.setConstant('B');
+    C.setConstant('C');
+    D.setConstant('D');
+    E.setConstant('E');
+    F.setConstant('F');
+
+    cout
+        << A << endl
+        << B << endl
+        << C << endl
+        << D << endl
+        << E << endl
+        << F << endl;
+
+    return 0;
+}
