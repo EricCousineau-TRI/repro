f2014a6e2aeee0860bb6f7814103f8340d66ca78
Eric Cousineau <eric.cousineau@tri.global>
2017-04-23 00:34:41 -0400

    Update for strings for uniqueness

--- eigen_scratch/matrix_stack.cc
+++ eigen_scratch/matrix_stack.cc
@@ -54,19 +54,25 @@ using std::cout;
 using std::endl;
 using std::string;
 
-using MatrixXc = Eigen::Matrix<char, Eigen::Dynamic, Eigen::Dynamic>;
+using MatrixXc = Eigen::Matrix<string, Eigen::Dynamic, Eigen::Dynamic>;
+
+void fill(MatrixXc& X, string prefix) {
+    static const string hex = "01234566789abcdef";
+    for (int i = 0; i < X.size(); ++i)
+        X(i) = prefix + hex[i];
+}
 
 int main() {
     MatrixXc A(1, 2), B(1, 2),
         C(2, 1),
         D(1, 3), E(1, 3),
         F(2, 6);
-    A.setConstant('A');
-    B.setConstant('B');
-    C.setConstant('C');
-    D.setConstant('D');
-    E.setConstant('E');
-    F.setConstant('F');
+    fill(A, "A");
+    fill(B, "B");
+    fill(C, "C");
+    fill(D, "D");
+    fill(E, "E");
+    fill(F, "F");
 
     cout
         << A << endl
