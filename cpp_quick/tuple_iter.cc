b7f94cd52ae3b06811a458da17a0372cfc797d6b
Eric Cousineau <eric.cousineau@tri.global>
2017-04-24 00:43:00 -0400

    Quick test on is_specialization_of

--- cpp_quick/tuple_iter.cc
+++ cpp_quick/tuple_iter.cc
@@ -9,6 +9,15 @@
 using std::cout;
 using std::endl;
 
+/* <snippet from="https://bitbucket.org/martinhofernandes/wheels/src/default/include/wheels/meta/type_traits.h%2B%2B?fileviewer=file-view-default#cl-161"> */
+// @ref http://stackoverflow.com/a/13101086/170413
+//! Tests if T is a specialization of Template
+template <typename T, template <typename...> class Template>
+struct is_specialization_of : std::false_type {};
+template <template <typename...> class Template, typename... Args>
+struct is_specialization_of<Template<Args...>, Template> : std::true_type {};
+/* </snippet> */
+
 int main() {
     int i = 0;
     auto f = [&](auto&& x) {
@@ -21,6 +30,10 @@ int main() {
     i = 0;
     auto t = std::make_tuple(1, "hello", 10.5);
     visit_tuple(f, t);
+
+    cout
+         << PRINT(( is_specialization_of<decltype(t), std::tuple>::value ))
+         << PRINT(( is_specialization_of<decltype(i), std::tuple>::value ));
 
     return 0;
 }
