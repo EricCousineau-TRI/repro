64031d01585f6c3e5819100c5876d646816de99c
Eric Cousineau <eric.cousineau@tri.global>
2017-05-02 17:29:28 -0400

    When rvalue is bound to receiving rvalue or lvalue, lifetime extends.

--- cpp_quick/lifetime.cc
+++ cpp_quick/lifetime.cc
@@ -2,48 +2,64 @@
 // Purpose: Too lazy to fully parse:
 // @ref http://en.cppreference.com/w/cpp/language/lifetime
 
-
 #include <iostream>
+#include <utility>
 
 #include "name_trait.h"
 
 using std::cout;
 using std::endl;
 
-template <typename T>
+template <int T>
 class Lifetime {
 public:
     Lifetime() {
-        cout << "ctor (default): " << name_trait<T>::name() << endl;
+        cout << "ctor (default): " << T << endl;
     }
     Lifetime(const Lifetime&) {
-        cout << "copy ctor (const lvalue): " << name_trait<T>::name() << endl;
-    }
-    template <typename U>
-    Lifetime(const Lifetime<U>&) {
-        cout
-             << "copy ctor (const lvalue, implicit copy): "
-             << name_trait<T>::name() << " <- " << name_trait<U>::name()
-             << endl;
+        cout << "copy ctor (const lvalue): " << T << endl;
     }
     Lifetime(Lifetime&&) {
-        cout << "copy ctor (rvalue): " << name_trait<T>::name() << endl;
+        cout << "copy ctor (rvalue): " << T << endl;
+    }
+    template <int U>
+    Lifetime(const Lifetime<U>&) {
+        cout
+             << "ctor (const Lifetime<" << U << ">&): "
+             << T << endl;
+    }
+    template <int U>
+    Lifetime(Lifetime<U>&&) {
+        cout
+             << "ctor (Lifetime<" << U << ">&&): "
+             << T << endl;
     }
     ~Lifetime() {
-        cout << "dtor: " << name_trait<T>::name() << endl;
+        cout << "dtor: " << T << endl;
     }
 protected:
     using Base = Lifetime<T>;
 };
-NAME_TRAIT_TPL(Lifetime);
 
-void func(const Lifetime<int>&) {
-    cout << "func(const lvalue)" << endl;
+void func_in(const Lifetime<1>&) {
+    cout << "func_in(const Lifetime<1>&)" << endl;
 }
 
+Lifetime<4> func_out() {
+    cout << "func_out()" << endl;
+    return Lifetime<4>();
+}
+
+
 int main() {
-    EVAL(Lifetime<int> obj{});
-    EVAL({ Lifetime<int>(); });
+    EVAL(Lifetime<1> obj1{});
+    EVAL({ Lifetime<2>(); });
+    EVAL({ Lifetime<3> obj3 = obj1; });
+    EVAL({ Lifetime<3> obj3 = Lifetime<1>(); });
+    EVAL(func_in(Lifetime<3>()));
+    EVAL({ func_out(); cout << "finish" << endl; });
+    EVAL({ const Lifetime<4>& obj4 = func_out(); cout << "finish" << endl; });
+    EVAL({ Lifetime<4>&& obj4 = func_out(); cout << "finish" << endl; });
 
     return 0;
 }
