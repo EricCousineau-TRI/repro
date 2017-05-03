194ab9c13ef4176eb0a21e4b401cf34ff60e5d00
Eric Cousineau <eric.cousineau@tri.global>
2017-05-03 00:47:54 -0400

    Expand and get failure

--- bindings/python/pydrake/test/testTypeBinding.py
+++ bindings/python/pydrake/test/testTypeBinding.py
@@ -6,10 +6,23 @@ import pydrake
 from pydrake import typebinding as tb
 
 class TestTypeBinding(unittest.TestCase):
-    def test_basic(self):
+    def test_env(self):
         import os
         os.system('echo "PATH=$PATH\n\nPYTHONPATH=$PYTHONPATH\n\nLD_LIBRARY_PATH=$LD_LIBRARY_PATH"')
+
+    def test_basic(self):
         obj = tb.SimpleType(1)
+        self.assertEqual(obj.value(), 1)
+
+    def test_flexible(self):
+        """
+        Presently fails:
+        
+            TypeError: __init__(): incompatible constructor arguments. The following argument types are supported:
+                1. pydrake._pydrake_typebinding.SimpleType()
+                2. pydrake._pydrake_typebinding.SimpleType(arg0: int)
+        """
+        obj = tb.SimpleType(1.)
         self.assertEqual(obj.value(), 1)
 
 if __name__ == '__main__':
