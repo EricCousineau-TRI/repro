880c2b7a78f4bd4682329d79e67f4e604210c5b4
Eric Cousineau <eric.cousineau@tri.global>
2017-05-03 00:29:05 -0400

    Empirically determine how Bazel affects runtime paths

--- bindings/python/pydrake/test/testTypeBinding.py
+++ bindings/python/pydrake/test/testTypeBinding.py
@@ -7,6 +7,8 @@ from pydrake import typebinding as tb
 
 class TestTypeBinding(unittest.TestCase):
     def test_basic(self):
+        import os
+        os.system('echo "PATH=$PATH\n\nPYTHONPATH=$PYTHONPATH\n\nLD_LIBRARY_PATH=$LD_LIBRARY_PATH"')
         obj = tb.SimpleType(1)
         self.assertEqual(obj.value(), 1)
 
---
+++ bindings/scratch/env_info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+{
+    bazel run ../pydrake_type_binding_test \
+        | sed \
+            -e "s#$PATH#\${PATH}#g" -e "s#$LD_LIBRARY_PATH#\${LD_LIBRARY_PATH}#g" \
+            -e "s#$PYTHONPATH#\${PYTHONPATH}#g" \
+            -e "s#$USER#\${USER}#g" \
+        | regex_sub.py \
+            '/home/.*?/execroot/.*?/bin/' '${WORKSPACE}/bazel-bin/' -
+             # From amber_developer_stack/common_scripts
+} | tee env_info.output.txt
