05c73558622827f4c6801ba044c03630fad2a3bc
Eric Cousineau <eric.cousineau@tri.global>
2017-05-12 15:35:21 -0400

    Additional constraints

--- shell/valgrind_norm/drake_mosek_license/run.sh
+++ shell/valgrind_norm/drake_mosek_license/run.sh
@@ -2,5 +2,5 @@
 
 cur=$(cd $(dirname $BASH_SOURCE) && pwd)
 {
-    bazel run --run_under='valgrind --tool=memcheck --track-origins=yes --leak-check=full' :mosek_solver_test 2>&1 ;
+    bazel run --run_under='valgrind --tool=memcheck --track-origins=yes --leak-check=full --show-leak-kinds=all' :mosek_solver_test 2>&1 ;
 } | tee $cur/mosek_solver_test-$(git ref).output.txt
