733fff94136b6b2e124788f99fcdf7dcbc1223b6
Eric Cousineau <eric.cousineau@tri.global>
2017-05-17 16:08:50 -0400

    Simplify hack

---
+++ shell/bazel_hack/bazel.followup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Place the following in ~/.bash_completion:
+
+# Use the same bash completion as `bazel build` for `test` and `run`
+BAZEL_COMMAND_RUN_ARGUMENT="label"
+BAZEL_COMMAND_TEST_ARGUMENT="label"
