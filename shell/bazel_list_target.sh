a48a062d220948acbf91eabde2fd23e58f42fc1d
Eric Cousineau <eric.cousineau@tri.global>
2017-05-17 16:01:25 -0400

    Sort output

--- shell/bazel_list_target.sh
+++ shell/bazel_list_target.sh
@@ -6,4 +6,6 @@ eecho() { echo "$@" >&2; }
 for target in $@; do
     eecho "Target: $target"
     bazel query --output=location 'filter(".*'${target}'$", //...)'
-done | regex_sub.py '([/\w\d\-:]+):\d+:.*' '\1' -
+done \
+    | regex_sub.py '([/\w\d\-:]+):\d+:.*' '\1' - \
+    | sort
