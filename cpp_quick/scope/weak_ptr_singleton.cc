df6becc9550a2962784f19e34522a7f23de0988a
Eric Cousineau <eric.cousineau@tri.global>
2017-05-18 22:50:53 -0400

    Works as expected.

--- cpp_quick/scope/weak_ptr_singleton.cc
+++ cpp_quick/scope/weak_ptr_singleton.cc
@@ -1,34 +1,32 @@
-// Goal: Ensure that we can hide internal implentation details.
+// Goal: Ensure that we can have a shared singleton with a confined scope.
 
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <mutex>
 
 using std::cout;
 using std::endl;
 using std::shared_ptr;
 using std::weak_ptr;
 using std::make_shared;
+using std::mutex;
 
 template <typename T, typename Extra = void>
-class ScopedSingleton {
- public:
-  typedef shared_ptr<T> ResourcePtr;
-
-  static shared_ptr<T> instance() {
-    static weak_ptr<T> ref;
-    auto instance = ref.lock();
-    if (!instance) {
-      instance = make_shared<T>();
-      ref = instance;
-    }
-    return instance;
+shared_ptr<T> GetScopedSingleton() {
+  struct Singleton {
+    weak_ptr<T> weak_ref_;
+    mutex mutex_;
+  };
+  static Singleton singleton;
+  std::lock_guard<mutex> lock(singleton.mutex_);
+  auto instance = singleton.weak_ref_.lock();
+  if (!instance) {
+    instance = make_shared<T>();
+    singleton.weak_ref_ = instance;
   }
-
- private:
-  typedef weak_ptr<T> ResourceWeakPtr;
-};
-
+  return instance;
+}
 
 class Resource {
  public:
@@ -40,19 +38,32 @@ struct A {};
 
 int main() {
   weak_ptr<Resource> wref;
+  weak_ptr<Resource> wref_a;
+  cout << "- default: " << wref.use_count() << endl;
+  cout << "- A: " << wref_a.use_count() << endl;
   {
-    auto ref_1 = ScopedSingleton<Resource>::instance();
+    auto ref_1 = GetScopedSingleton<Resource>();
     wref = ref_1;
-    cout << wref.use_count() << endl;
-    auto ref_2 = ScopedSingleton<Resource>::instance();
-    cout << wref.use_count() << endl;
+    cout << "- default: " << wref.use_count() << endl;
+    auto ref_2 = GetScopedSingleton<Resource>();
+    cout << "- default: " << wref.use_count() << endl;
     {
-      auto ref_a_1 = ScopedSingleton<Resource, A>::instance();
-    cout << wref.use_count() << endl;
-      auto ref_3 = ScopedSingleton<Resource>::instance();
-    cout << wref.use_count() << endl;
+      auto ref_3 = GetScopedSingleton<Resource>();
+      cout << "- default: " << wref.use_count() << endl;
+      auto ref_a_1 = GetScopedSingleton<Resource, A>();
+      wref_a = ref_a_1;
+      cout << "- A: " << wref_a.use_count() << endl;
     }
+    cout << "- default: " << wref.use_count() << endl;
+    cout << "- A: " << wref_a.use_count() << endl;
   }
-  cout << wref.use_count() << endl;
+  cout << "- default: " << wref.use_count() << endl;
+
+  {
+    auto ref_1 = GetScopedSingleton<Resource>();
+    wref = ref_1;
+    cout << "- default: " << wref.use_count() << endl;
+  }
+  cout << "- default: " << wref.use_count() << endl;
   return 0;
 }
