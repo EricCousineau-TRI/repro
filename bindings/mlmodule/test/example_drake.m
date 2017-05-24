32ef3eb0a488937cec494b9d63edc9d0f5e59d4a
Eric Cousineau <eric.cousineau@tri.global>
2017-05-24 14:33:13 -0400

    Initial crack at directly incorporating MathematicalProgram

---
+++ bindings/mlmodule/test/example_drake.m
@@ -0,0 +1,21 @@
+% Adapted from: drake:96c2ca1:drake/matlab/solvers/test/testMathematicalProgram.m
+
+% Simple example of calling MathematicalProgram through the python
+% interface.
+
+mp = pyimport_proxy('pydrake.solvers.mathematicalprogram');
+
+% QP test
+prog = mp.MathematicalProgram();
+x = prog.NewContinuousVariables(int32(2),'x');
+prog.AddLinearConstraint(x.item(0) >= 1);
+prog.AddLinearConstraint(x.item(1) >= 1);
+prog.AddQuadraticCost(eye(2), zeros(2), x);
+result = prog.Solve()
+
+% Note: int32(0) is kSolutionFound; can't reference it directly.
+assert(result == mp.SolutionResult(int32(0)));
+
+x_sol = prog.GetSolution(x);
+assert(abs(x_sol(1)-1.0)<1e-6);
+assert(abs(x_sol(2)-1.0)<1e-6);
--- bindings/pymodule/_util/setup_target_env.sh
+++ bindings/pymodule/_util/setup_target_env.sh
@@ -7,36 +7,40 @@
 # 
 #    $ source setup_target_env.sh [--build] <PACKAGE> <IMPORTS> <TARGET>
 
-set -x
+# https://unix.stackexchange.com/questions/219314/best-way-to-make-variables-local-in-a-sourced-bash-script
+setup_target_env_impl() {
+    set -x
+    local source_dir=$(cd $(dirname $BASH_SOURCE) && pwd)
 
-cur=$(cd $(dirname $BASH_SOURCE) && pwd)
+    local build=
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --build)
+                build=1
+                shift;;
+            *)
+                break;;
+        esac
+    done
 
-build=
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        --build)
-            build=1
-            shift;;
-        *)
-            break;;
-    esac
-done
+    local workspace=$(bazel info workspace)
+    # Use workaround for bazel <= 0.4.5
+    # @ref: https://github.com/bazelbuild/bazel/issues/2317#issuecomment-284725684
+    local workspace_name=$($(cd ${source_dir} && bazel info workspace)/shell/bazel_workspace_name.py)
 
-workspace=$(bazel info workspace)
-# Use workaround for bazel <= 0.4.5
-# @ref: https://github.com/bazelbuild/bazel/issues/2317#issuecomment-284725684
-workspace_name=$($workspace/shell/bazel_workspace_name.py)
+    local package=${1}
+    local imports=${2}
+    local target=${3}
+    local runfiles=${workspace}/bazel-bin/${package}/${target}.runfiles
 
-package=${1}
-imports=${2}
-target=${3}
-runfiles=${workspace}/bazel-bin/${package}/${target}.runfiles
+    if [[ -n ${build} ]]; then
+        bazel build //${package}:${target}
+    fi
 
-if [[ -n ${build} ]]; then
-    bazel build //${package}:${target}
-fi
+    export PYTHONPATH=${runfiles}:${runfiles}/${workspace_name}/${imports}:${runfiles}/${workspace_name}:${PYTHONPATH}
 
-export PYTHONPATH=${runfiles}:${runfiles}/${workspace_name}/${imports}:${runfiles}/${workspace_name}:${PYTHONPATH}
+    set +x
+    echo "[ Exposed \${PYTHONPATH} for target: //${package}:${target} ]"
+}
 
-set +x
-echo "[ Exposed \${PYTHONPATH} for target: //${package}:${target} ]"
+setup_target_env_impl "$@"
