4ffbb05954920d180153512d233c36d6adb4a62b
Eric Cousineau <eric.cousineau@tri.global>
2017-05-27 01:26:38 -0400

    Add simple scoped timre.

---
+++ cpp_quick/timing/scoped_timer_main.cc
@@ -0,0 +1,18 @@
+#include <sstream>
+
+#include "scoped_timer.h"
+
+using std::ostringstream;
+using namespace timing;
+
+int main() {
+  double dt = 0.1;
+  Timer timer;
+  for (int i = 0; i < 5; ++i) {
+    ostringstream oss;
+    oss << "iter " << i << ": ";
+    ScopedTimerMessage timer_scope(timer, oss.str());
+    sleep(dt);
+  }
+  return 0;
+}
