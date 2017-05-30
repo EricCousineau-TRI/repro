7504daf373fdc6ea17bb0e00e7558ddb74b22c8d
Eric Cousineau <eric.cousineau@tri.global>
2017-05-30 19:23:30 -0400

    Add access to inheritance and private values

--- cpp_quick/tpl_spec_switch.h
+++ cpp_quick/tpl_spec_switch.h
@@ -7,30 +7,41 @@ class A {
 
 class B {};
 
+// Use `Extra` to permit this to be partially specialized, such that
+// we can define it within the class.
+template <typename U, bool kIsA_>
+struct stuff_helper {
+  static int impl(U* obj) {
+    return -1;
+  }
+};
+template <typename U>
+struct stuff_helper<U, true> {
+  static int impl(U* obj) {
+    return obj->private_value();
+  }
+};
+
+template <typename T>
+class Base {
+ protected:
+  int private_value() {
+    return T::value;
+  }
+};
+
 // @ref https://stackoverflow.com/questions/10178598/specializing-a-templated-member-of-a-template-class
 template <typename T>
-class Example {
+class Example : public Base<T> {
  public:
   int Stuff() {
-    return stuff_helper<kIsA>::impl();
+    return helper::impl(this);
   }
  private:
   static constexpr bool kIsA = std::is_base_of<A, T>::value;
-
-  // Use `Extra` to permit this to be partially specialized, such that
-  // we can define it within the class.
-  template <bool kIsA_, typename Extra = void>
-  struct stuff_helper {
-    static int impl() {
-      return -1;
-    }
-  };
-  template <typename Extra>
-  struct stuff_helper<true, Extra> {
-    static int impl() {
-      return T::value;
-    }
-  };
+  using helper = stuff_helper<Example, kIsA>;
+  // Permit the helper to have access
+  friend helper;
 };
 
 int extra_stuff();
