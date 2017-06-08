49fa2351fef9ac31925942cf2383087f34d64d56
Eric Cousineau <eric.cousineau@tri.global>
2017-06-08 11:41:02 -0400

    No out-of-the-box clang support for OpenMP.

--- cpp_quick/openmp/gen_timing.sh
+++ cpp_quick/openmp/gen_timing.sh
@@ -4,17 +4,27 @@ set -e -u
 show-timing() {
     prefix=${1-}
     shift
-    { {
+    {
         echo "Prefix: ${prefix}"
         echo "  Args: $@"
         echo "[ With OpenMP ]"
-        ( set -x; ./build/${prefix}with_openmp "$@" )
+        ( set -x; ./build/${CXX}/${prefix}with_openmp "$@" )
         echo "[ Without OpenMP ]"
-        ( set -x; ./build/${prefix}without_openmp "$@" )
-    } 2>&1 ; } | tee ${prefix}timing.txt 
+        ( set -x; ./build/${CXX}/${prefix}without_openmp "$@" )
+    } 2>&1
 }
 
-make -j
-show-timing ''
-show-timing '' --no-sleep
-show-timing '' --no-pragma
+compile-run() {
+    echo "CXX: ${CXX}"
+    make -j
+    prefix=
+    show-timing "${prefix}"
+    show-timing "${prefix}" --no-sleep
+    show-timing "${prefix}" --no-pragma
+    show-timing "${prefix}" --no-pragma --no-sleep
+}
+
+{
+    CXX=g++ compile-run
+    # CXX=clang++ compile-run  # Does not work...
+} | tee timing.txt
