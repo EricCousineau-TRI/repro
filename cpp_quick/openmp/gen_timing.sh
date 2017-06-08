2cba7bb4bfb31f545d7506d8a1f5bfb662ccf004
Eric Cousineau <eric.cousineau@tri.global>
2017-06-08 11:20:15 -0400

    More indicative example.

--- cpp_quick/openmp/gen_timing.sh
+++ cpp_quick/openmp/gen_timing.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -e -u
 
 show-timing() {
     prefix=${1-}
-    echo "Prefix: ${prefix}"
-    echo "[ With OpenMP ]"
-    ( set -x; ./build/${prefix}with_openmp )
-    echo "[ Without OpenMP ]"
-    ( set -x; ./build/${prefix}without_openmp )
+    { {
+        echo "Prefix: ${prefix}"
+        echo "[ With OpenMP ]"
+        ( set -x; ./build/${prefix}with_openmp )
+        echo "[ Without OpenMP ]"
+        ( set -x; ./build/${prefix}without_openmp )
+    } 2>&1 ; } | tee ${prefix}timing.txt 
 }
 
 make -j
--- cpp_quick/openmp/simple.cc
+++ cpp_quick/openmp/simple.cc
@@ -1,5 +1,7 @@
 #include <chrono>
+#include <cmath>
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
@@ -7,13 +9,23 @@ using Clock = std::chrono::steady_clock;
 using Duration = std::chrono::duration<double>;
 using TimePoint = std::chrono::time_point<Clock, Duration>;
 
+inline void sleep(double seconds) {
+  int ms = std::round(seconds * 1000);
+  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+double expensive(int i) {
+  sleep(0.05);
+  return i * i;
+}
+
 int main() {
-  const int count = 1000000;
+  const int count = 100;
   double value[count];
   auto start = Clock::now();
   #pragma omp parallel for
   for (int i = 0; i < count; ++i) {
-    value[i] = i*i;
+    value[i] = expensive(i);
   }
   cout << "Elapsed time: " << Duration(Clock::now() - start).count() << endl;
   return 0;
