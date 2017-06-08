fbb598bb09288d1fb5983545414c34cb7bcc5d41
Eric Cousineau <eric.cousineau@tri.global>
2017-06-08 11:24:28 -0400

    Decent amount of overhead if initializing openmp

--- cpp_quick/openmp/gen_timing.sh
+++ cpp_quick/openmp/gen_timing.sh
@@ -3,14 +3,17 @@ set -e -u
 
 show-timing() {
     prefix=${1-}
+    shift
     { {
         echo "Prefix: ${prefix}"
+        echo "  Args: $@"
         echo "[ With OpenMP ]"
-        ( set -x; ./build/${prefix}with_openmp )
+        ( set -x; ./build/${prefix}with_openmp "$@" )
         echo "[ Without OpenMP ]"
-        ( set -x; ./build/${prefix}without_openmp )
+        ( set -x; ./build/${prefix}without_openmp "$@" )
     } 2>&1 ; } | tee ${prefix}timing.txt 
 }
 
 make -j
 show-timing ''
+show-timing '' --no-sleep
--- cpp_quick/openmp/simple.cc
+++ cpp_quick/openmp/simple.cc
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <cmath>
+#include <string>
 #include <iostream>
 #include <thread>
 
@@ -14,18 +15,34 @@ inline void sleep(double seconds) {
   std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-double expensive(int i) {
-  sleep(0.05);
+double expensive(int i, bool no_sleep) {
+  if (!no_sleep) {
+    sleep(0.05);
+  }
   return i * i;
 }
 
-int main() {
+int main(int argc, char** argv) {
+  bool no_sleep = false;
+  {
+    int i = 1;
+    while (i < argc) {
+      string arg = argv[i];
+      if (arg == "--no-sleep") {
+        no_sleep = true;
+      } else {
+        cerr << "usage:  " << argv[0] << " [--no-sleep]" << endl;
+        return 1;
+      }
+      i++;
+    }
+  }
   const int count = 100;
   double value[count];
   auto start = Clock::now();
   #pragma omp parallel for
   for (int i = 0; i < count; ++i) {
-    value[i] = expensive(i);
+    value[i] = expensive(i, no_sleep);
   }
   cout << "Elapsed time: " << Duration(Clock::now() - start).count() << endl;
   return 0;
