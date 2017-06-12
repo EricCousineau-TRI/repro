8eb757d2fa7c753c7816d59cbfd8613b039e1f3b
Eric Cousineau <eric.cousineau@tri.global>
2017-06-12 13:34:13 -0400

    Just double-checking on Ref re-assignment.

--- eigen_scratch/ref_check.cc
+++ eigen_scratch/ref_check.cc
@@ -4,6 +4,11 @@
 using namespace std;
 using namespace Eigen;
 
+// Check if we can re-assign structs with refs.
+struct A {
+  Ref<VectorXd> X;
+};
+
 int main() {
   VectorXd X(1);
   X.setZero();
@@ -11,6 +16,13 @@ int main() {
   Ref<VectorXd> X_ref = X;
   X_ref.resize(1);
   // X_ref.resize(2);  // Throws at runtime as expected.
+
+  // VectorXd Y(5);
+  // Ref<VectorXd> Y_head = Y.head(3);
+  // Ref<VectorXd> Y_tail = Y.tail(2);
+  // A a {Y_head};
+  // A b {Y_tail};
+  // a = b;
 
   return 0;
 }
