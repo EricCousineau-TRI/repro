1d9db398566cf5d5e27f883bfb849d00d0fd71db
Eric Cousineau <eric.cousineau@tri.global>
2017-07-11 11:27:15 -0400

    A little confused on how this works, but meh.

--- cpp_quick/stl_ref.cc
+++ cpp_quick/stl_ref.cc
@@ -5,26 +5,40 @@
 
 using namespace std;
 
-namespace std {
+// namespace std {
 
-template <typename A, typename B>
-bool operator<(const reference_wrapper<A>& a, const reference_wrapper<B>& b) {
-  return a.get() < b.get();
-}
+// template <typename A, typename B>
+// bool operator<(const reference_wrapper<A>& a, const reference_wrapper<B>& b) {
+//   return a.get() < b.get();
+// }
 
-}
+// }
+
+template <typename A, typename B = A>
+struct reference_less {
+  bool operator()(
+      const reference_wrapper<A>& a,
+      const reference_wrapper<B>& b) const {
+    return a.get() < b.get();
+  }
+};
 
 // TODO:
 // https://stackoverflow.com/a/10717235/7829525
 
 template <typename K, typename V>
-using cref_map = map<reference_wrapper<const K>, reference_wrapper<const V>>;
+using ref_map =
+    map<reference_wrapper<K>, reference_wrapper<V>,
+        reference_less<K>>;
+
+template <typename K, typename V>
+using cref_map = ref_map<const K, const V>;
 
 int main() {
   string a_k = "a";
   int a_v = 1;
 
-  string b_k = "a";
+  string b_k = "b";
   int b_v = 2;
 
   // The key should NOT be changed.
@@ -39,6 +53,13 @@ int main() {
   cout << x.at(a_k) << endl;
   a_v *= 10;
   cout << x.at(a_k) << endl;
+
+  string a_k_o = "a";  // Test for value comparison.
+  cout << x.at(a_k_o) << endl;
+
+  cout << x.at(b_k) << endl;
+  b_v *= 100;
+  cout << x.at(b_k) << endl;
 
   return 0;
 }
