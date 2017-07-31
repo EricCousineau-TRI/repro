609ff5d386e4dcb020e311b4bbd8968c198894eb
Eric Cousineau <eric.cousineau@tri.global>
2017-07-31 14:32:38 -0400

    Add comments

--- cpp/gcc_flags/shadow/test.sh
+++ cpp/gcc_flags/shadow/test.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
+# @see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=57709
+
+# Error
 set -x
 CXX=g++-4.9 make VERBOSE=1 -B
 
+# Error
 echo
 CXX=g++-5 make VERBOSE=1 -B
 
+# No error
 echo
 CXX=clang++ make VERBOSE=1 -B
