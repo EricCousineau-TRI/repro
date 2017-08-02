ba7251b61a0ed6cc29dbca29b7279c721c21adaa
Eric Cousineau <eric.cousineau@tri.global>
2017-08-02 17:10:46 -0400

    Update ubsan info.

--- cpp/eigen/ubsan_zero_size_mult.cc
+++ cpp/eigen/ubsan_zero_size_mult.cc
@@ -1,6 +1,10 @@
 // From Mmanu: https://gist.github.com/m-chaturvedi/aed94a7fa4b51d5bfcf8ea5f063790dc
 /*
-clang++-3.9 -std=c++11 -I /usr/include/eigen3 -fsanitize=undefined -fno-sanitize-recover=undefined -fsanitize-trap=undefined ubsan_zero_size_mult.cc && gdb -ex run ./a.out
+{
+  mkdir -p build/
+  name=ubsan_zero_size_mult
+  clang++-3.9 -g -std=c++11 -I /usr/include/eigen3 -fsanitize=undefined -fno-sanitize-recover=undefined -fsanitize-trap=undefined ${name}.cc -o build/${name} && gdb -ex run ./build/${name}
+}
 */
 #include <bits/stdc++.h>
 #include <Eigen/Core>
