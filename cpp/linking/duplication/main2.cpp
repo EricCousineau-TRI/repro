c1f0a47360e56301dd491b1b3d744af71b5f4907
Eric Cousineau <eric.cousineau@tri.global>
2017-08-02 19:45:44 -0400

    Yar

---
+++ cpp/linking/duplication/main2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <dlfcn.h>
+#include "singleton.h"
+
+typedef void (*hello_t)();
+using std::cout;
+using std::cerr;
+using std::endl;
+
+void call_hello(const char* lib, const char* name) {
+    std::cout << singleton::pInstance << std::endl;
+    // open the library
+    void* handle = dlopen(lib, RTLD_LAZY);
+    if (!handle) {
+        cerr << "Cannot open library: " << dlerror() << '\n';
+        return;
+    }
+    // load the symbol
+    
+    // reset errors
+    dlerror();
+    hello_t hello = (hello_t) dlsym(handle, name);
+    const char *dlsym_error = dlerror();
+    if (dlsym_error) {
+        cerr << "Cannot load symbol 'hello': " << dlerror() << '\n';
+        dlclose(handle);
+        return;
+    }
+    hello(); // call plugin function hello
+    dlclose(handle);
+}
+
+int main() {
+    call_hello("./hello.so", "hello");
+    call_hello("./hello2.so", "hello2");
+    return 0;
+}
