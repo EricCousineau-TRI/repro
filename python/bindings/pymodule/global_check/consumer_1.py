238aa0952ca69677f7dd1d9d6070c26463a374c6
Eric Cousineau <eric.cousineau@tri.global>
2017-08-02 23:18:12 -0400

    Works... but very cautious about this...

--- python/bindings/pymodule/global_check/consumer_1.py
+++ python/bindings/pymodule/global_check/consumer_1.py
@@ -1,3 +1,32 @@
 from __future__ import absolute_import, division, print_function
+import sys
 
+# From DLFCN.py
+# Included from bits/dlfcn.h
+RTLD_LAZY = 0x00001
+RTLD_NOW = 0x00002
+RTLD_BINDING_MASK = 0x3
+RTLD_NOLOAD = 0x00004
+RTLD_GLOBAL = 0x00100
+RTLD_LOCAL = 0
+RTLD_NODELETE = 0x01000
+
+sys.setdlopenflags(RTLD_NOW | RTLD_GLOBAL)
 from ._consumer_1 import *
+
+# # import imp
+# # _tmp = imp.load_dynamic("_consumer_1", "_consumer_1.so")
+# print(__name__)
+# pieces = __name__.split('.')
+# print(pieces)
+# c_name = '.'.join(pieces[:-1]) + "._consumer_1"
+# print(c_name)
+
+# cur = __import__(c_name)
+# # print(mod.__file__)
+# print(mod)
+
+# # print(mod.__dict__.keys())
+# # locals().update(mod.__dict__)
+# # do_stuff_1 = mod.do_stuff_1
+# # from ._consumer_1 import *
--- python/bindings/pymodule/global_check/test/global_check_test.py
+++ python/bindings/pymodule/global_check/test/global_check_test.py
@@ -2,14 +2,19 @@
 from __future__ import print_function, absolute_import
 
 import unittest
-from pymodule.global_check.consumer_1 import do_stuff_1
-from pymodule.global_check.consumer_2 import do_stuff_2
+import pymodule.global_check.consumer_1 as c1
+import pymodule.global_check.consumer_2 as c2
+
+# import pymodule.global_check._consumer_1 as _c1
+
+# print(_c1)
+# print(_c1.__dict__)
 
 class TestInheritance(unittest.TestCase):
     def test_basic(self):
         value = 2
-        print(do_stuff_1(value))
-        print(do_stuff_2(value))
+        print(c1.do_stuff_1(value))
+        print(c2.do_stuff_2(value))
 
 if __name__ == '__main__':
     unittest.main()
