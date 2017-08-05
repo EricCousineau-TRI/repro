62ad771766dd83200e12960a02e974b8f27075ab
Eric Cousineau <eric.cousineau@tri.global>
2017-08-05 15:22:32 -0400

    Not getting desired death...

--- matlab/mex_py_handle/pybind11_ml_callback_test.m
+++ matlab/mex_py_handle/pybind11_ml_callback_test.m
@@ -4,13 +4,20 @@ MexPyProxy.init();
 
 %% 
 fp = pyimport_proxy('pymodule.sub.func_ptr');
-py_simp = @py.simple.pass_thru;
+py_simple = pyimport_proxy('simple');
+PyProxy.reloadPy(py_simple);
+py_pass_thru = py_simple.pass_thru;
 
 %%
 % First test with just Python
 % (C++ calling Python)
-fp.call(py_simp)
+fp.call(py_pass_thru)
 
 %% Now with a MATLAB callback
 % (C++ calling Python calling MATLAB)
 fp.call(@adder)
+
+%% Now try to get Python -> pybind11 (C++) -> Python -> MATLAB -> Python
+meta_call = @(f) fp.call(f);
+
+py_simple.call_check(meta_call, @adder)
