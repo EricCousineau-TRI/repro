85ecd20d2d9c9a171156219353739feebfcf132b
Eric Cousineau <eric.cousineau@tri.global>
2017-08-05 15:02:40 -0400

    Add function ptr test

---
+++ python/bindings/pymodule/sub/_func_ptr.cc
@@ -0,0 +1,27 @@
+#include <cstddef>
+#include <cmath>
+#include <sstream>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+
+namespace py = pybind11;
+using std::string;
+using std::ostringstream;
+
+namespace func_ptr {
+
+int call(const std::function<int(int)>& func) {
+  return func(1);
+}
+
+PYBIND11_PLUGIN(_func_ptr) {
+  py::module m("_func_ptr", "Call Python method.");
+
+  m.def("call", &call);
+
+  return m.ptr();
+}
+
+}  // namespace inherit_check
---
+++ python/bindings/pymodule/sub/test/func_ptr_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from __future__ import print_function, absolute_import
+
+import unittest
+import numpy as np
+npa = np.array
+import pymodule
+from pymodule.sub import func_ptr as fp
+
+class TestInheritance(unittest.TestCase):
+    def test_basic(self):
+        # https://stackoverflow.com/questions/4851463/python-closure-write-to-variable-in-parent-scope
+        value = [1]
+        def tmp(x):
+            value[0] += x
+            return value[0]
+        out = fp.call(tmp)
+        self.assertEqual(2, out)
+        self.assertEqual(2, value[0])
+        print(value[0])
+
+if __name__ == '__main__':
+    unittest.main()
