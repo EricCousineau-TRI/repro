0e6e180bb3375ad72d240a6a9b30164221cfea0b
Eric Cousineau <eric.cousineau@tri.global>
2017-08-05 16:43:25 -0400

    Can reproduce error much more simply

--- python/bindings/mlmodule/test/example_inherit_check.m
+++ python/bindings/mlmodule/test/example_inherit_check.m
@@ -1,9 +1,12 @@
 ic = pyimport_proxy('pymodule.sub.inherit_check');
 ice = pyimport_proxy('example_inherit_check');
 
+PyProxy.reloadPy(ic);
+PyProxy.reloadPy(ice);
+
 %%
 cpp = ic.CppExtend()
-py = ice.PyExtend()
+py = ice.PyExtend(ice.pass_thru)
 value = int64(3);
 
 c = cpp.dispatch(value)
