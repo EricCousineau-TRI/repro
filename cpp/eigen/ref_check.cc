dd06ceb209341b30f941f31012abec98a8d510cc
Eric Cousineau <eric.cousineau@tri.global>
2017-09-05 13:25:00 -0400

    Add quick comment for reszing for blocks and such.

--- cpp/eigen/ref_check.cc
+++ cpp/eigen/ref_check.cc
@@ -13,8 +13,20 @@ int main() {
   VectorXd X(1);
   X.setZero();
 
+  VectorBlock<VectorXd> X_blk = X.segment(0, 1);
+  X_blk.resize(1);
+  // X_blk.resize(2);
+
   Ref<VectorXd> X_ref = X;
   X_ref.resize(1);
+  /*
+  In debug mode, this next line will throw the following exception:
+
+  Assertion `newSize == this->size() && "DenseBase::resize() does not actually allow to resize.
+
+  However, it does not throw in release mode.
+  This is the same behavior as above, for VectorBlock<>.
+  */
   // X_ref.resize(2);  // Throws at runtime as expected.
 
   // VectorXd Y(5);
