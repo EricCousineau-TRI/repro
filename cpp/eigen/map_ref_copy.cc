e7d4136822f5f7c09757d3e0d8e0d3bd5bc6edc4
Eric Cousineau <eric.cousineau@tri.global>
2017-09-26 16:38:38 -0400

    Failing example...

--- cpp/eigen/map_ref_copy.cc
+++ cpp/eigen/map_ref_copy.cc
@@ -16,28 +16,39 @@ class RefMap : public Eigen::Map<PlainObjectType> {
 int main() {
   using namespace Eigen;
 
-  VectorXd X(3);
-  const VectorXd& X_c(X);
-  X << 1, 2, 3;
+  MatrixXd X(3, 3);
+  const MatrixXd& X_c(X);
+  X << 1, 2, 3,
+       4, 5, 6,
+       7, 8, 9;
+
+  auto Xr = X.row(0).transpose();
+  auto X_cr = X_c.row(0).transpose();
 
   // Map<Vector3d> Y(X);
-  RefMap<Vector3d> Y(X);
-  RefMap<const Vector3d> Y_c(X);
+  RefMap<Vector3d> Y(Xr);
+  RefMap<const Vector3d> Y_c(Xr);
 
-  // RefMap<Vector3d> Yc(X_c);  // Fails as expected.
-  RefMap<const Vector3d> Yc_c(X_c);
+  // RefMap<Vector3d> Yc(X_cr);  // Fails as expected.
+  RefMap<const Vector3d> Yc_c(X_cr);
 
   std::cout << Y.transpose() << std::endl;
 
   // Try to induce a copy.
-  Ref<const Vector3d> Z_c(X);
-  Ref<Matrix<double, 1, 3, RowMajor>> A_c(X.transpose());
+  Ref<const Vector3d> Z_c(Xr);
+  Ref<const Matrix<double, 1, 3, RowMajor>> A_c(X_cr);
 
   std::cout << A_c << std::endl;
 
   X *= 3;
 
-  std::cout << "---\n" << Z_c.transpose() << "\n" << A_c << "\n";
+  std::cout
+      << "---\n"
+      << Y.transpose() << "\n"
+      << Y_c.transpose() << "\n"
+      << Xr.transpose() << "\n"
+      << Z_c.transpose() << "\n"
+      << A_c << "\n";
 
   return 0;
 }
