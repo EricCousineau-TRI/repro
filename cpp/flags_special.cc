1d43ae13174c6151c657b7be76b205685eeef412
Eric Cousineau <eric.cousineau@tri.global>
2017-09-27 12:24:01 -0400

    Update names

--- cpp/flags_special.cc
+++ cpp/flags_special.cc
@@ -7,21 +7,21 @@ using std::string;
 using std::cout;
 using std::endl;
 
-enum FlagA : int {
-  kFlagANone = 0,
-  kFlagA1 = 1 << 0,
-  kFlagA2 = 1 << 1,
-  kFlagA3 = 1 << 2,
+enum Field : int {
+  kFieldNone = 0,
+  kField1 = 1 << 0,
+  kField2 = 1 << 1,
+  kField3 = 1 << 2,
 };
 
-struct FlagB {
+struct Descriptor {
   string name;
 };
 
-bool operator==(const FlagB& lhs, const FlagB& rhs) {
+bool operator==(const Descriptor& lhs, const Descriptor& rhs) {
   return lhs.name == rhs.name;
 }
-bool operator!=(const FlagB& lhs, const FlagB& rhs) {
+bool operator!=(const Descriptor& lhs, const Descriptor& rhs) {
   return lhs.name != rhs.name;
 }
 
@@ -31,33 +31,33 @@ bool operator!=(const FlagB& lhs, const FlagB& rhs) {
 class Flags;
 std::ostream& operator<<(std::ostream& os, const Flags& flags);
 
-const FlagB
-  kFlagBNone{"None"},
-  kFlagBInherit{"Inherit"},
-  kFlagB1{"1"},
-  kFlagB2{"2"};
+const Descriptor
+  kDescriptorNone{"None"},
+  kDescriptorInherit{"Inherit"},
+  kDescriptor1{"1"},
+  kDescriptor2{"2"};
 
 class Flags {
  public:
   Flags(const int& flag)
-    : flags_a_(static_cast<FlagA>(flag)) {}
+    : flags_a_(static_cast<Field>(flag)) {}
 
-  Flags(const FlagB& flag)
+  Flags(const Descriptor& flag)
     : flag_b_(flag) {
-    // Explicit prevent kFlagBNone from being used.
-    if (flag == kFlagBNone) {
-      throw std::runtime_error("Cannot explicit pass kFlagBNone");
+    // Explicit prevent kDescriptorNone from being used.
+    if (flag == kDescriptorNone) {
+      throw std::runtime_error("Cannot explicit pass kDescriptorNone");
     }
   }
 
   Flags& operator|=(const Flags& other) {
-    flags_a_ = static_cast<FlagA>(flags_a_ | other.flags_a_);
-    if (flag_b_ == kFlagBNone) {
+    flags_a_ = static_cast<Field>(flags_a_ | other.flags_a_);
+    if (flag_b_ == kDescriptorNone) {
       flag_b_ = other.flag_b_;
     } else {
       throw std::runtime_error(
-          "Cannot have multiple FlagB flags. "
-          "Can only do operator| iff (flags.b() == kFlagBNone) == true.");
+          "Cannot have multiple Descriptor flags. "
+          "Can only do operator| iff (flags.b() == kDescriptorNone) == true.");
     }
     return *this;
   }
@@ -77,47 +77,47 @@ class Flags {
   operator string() const {
     std::ostringstream os;
     os << "(";
-    if (flags_a_ & kFlagA1)
-      os << "kFlagA1 | ";
-    if (flags_a_ & kFlagA2)
-      os << "kFlagA2 | ";
-    if (flags_a_ & kFlagA3)
-      os << "kFlagA3 | ";
-    if (flag_b_ != kFlagBNone)
-      os << "kFlagB::" << flag_b_.name << " | ";
+    if (flags_a_ & kField1)
+      os << "kField1 | ";
+    if (flags_a_ & kField2)
+      os << "kField2 | ";
+    if (flags_a_ & kField3)
+      os << "kField3 | ";
+    if (flag_b_ != kDescriptorNone)
+      os << "kDescriptor::" << flag_b_.name << " | ";
     os << ")";
     return os.str();
   }
 
  private:
   // Allow these to accumulate.
-  FlagA flags_a_{kFlagANone};
+  Field flags_a_{kFieldNone};
   // These cannot accumulate.
-  FlagB flag_b_{kFlagBNone};
+  Descriptor flag_b_{kDescriptorNone};
 };
 
 std::ostream& operator<<(std::ostream& os, const Flags& flags) {
   return os << string(flags);
 }
 
-// Make | compatible for FlagA + FlagB.
-Flags operator|(const int& lhs, const FlagB& rhs) {
+// Make | compatible for Field + Descriptor.
+Flags operator|(const int& lhs, const Descriptor& rhs) {
   return Flags(lhs) | rhs;
 }
-// Use implicit conversion since there is no ambiguity with FlagA.
-Flags operator|(const FlagB& lhs, const Flags& rhs) {
+// Use implicit conversion since there is no ambiguity with Field.
+Flags operator|(const Descriptor& lhs, const Flags& rhs) {
   return Flags(lhs) | rhs;
 }
 
 int main() {
-  EVAL(Flags flags = kFlagA1 | kFlagA2 | kFlagB1);
+  EVAL(Flags flags = kField1 | kField2 | kDescriptor1);
   cout
     << PRINT(flags)
-    << PRINT(flags & kFlagA1)
-    << PRINT(flags & kFlagA2)
-    << PRINT(flags & kFlagA3)
-    << PRINT(flags & kFlagB1)
-    << PRINT(flags & kFlagB2)
-    << PRINT(flags & kFlagBInherit);
+    << PRINT(flags & kField1)
+    << PRINT(flags & kField2)
+    << PRINT(flags & kField3)
+    << PRINT(flags & kDescriptor1)
+    << PRINT(flags & kDescriptor2)
+    << PRINT(flags & kDescriptorInherit);
   return 0;
 }
