7f26b4a59f7ace921fc99b0a682c35faa96ce74b
Eric Cousineau <eric.cousineau@tri.global>
2017-10-03 14:39:39 -0400

    Dirtiness...

--- python/bindings/pymodule/tpl/test/scalar_type_test.py
+++ python/bindings/pymodule/tpl/test/scalar_type_test.py
@@ -14,19 +14,21 @@ base_types = {
     (float, long): st.Base__T_double__U_int,
     }
 
-def BaseT(*args):
-    return base_types[args]
+def BaseT(T=long, U=float):
+    types = (T, U)
+    return base_types[types]
 
 # Default class.
-Base = BaseT(long, float)
+Base = BaseT()
 
 print(Base)
 print(BaseT(long, float))
 print(BaseT(float, long))
 
 # Should only define these classes once.
-def _Child(T, U):
+def _Child(T=long, U=float):
     Base = BaseT(T, U)
+
     class Child(Base):
         def __init__(self, t, u):
             Base.__init__(self, t, u)
@@ -39,6 +41,8 @@ def _Child(T, U):
             print("py: optional")
             return T(2 * u)
 
+    # Change name for clarity.
+    Child.__name__ = "Child__T_{}__U_{}".format(T.__name__, U.__name__)
     return Child
 
 child_types = {
@@ -46,9 +50,12 @@ child_types = {
     (float, long): _Child(float, long),
     }
 
-def ChildT(*args):
-    return child_types[args]
-Child = ChildT(long, float)
+def ChildT(T=long, U=float):
+    types = (T, U)
+    return child_types[types]
+
+# Default instantiation.
+Child = ChildT()
 
 print(Child)
 print(ChildT(long, float))
@@ -56,3 +63,8 @@ print(ChildT(float, long))
 
 print(Child == Child)
 print(ChildT(long, float) == ChildT(float, long))
+
+c = Child(2, 5.5)
+c.pure(1)
+c.optional(2)
+c.dispatch(3)
