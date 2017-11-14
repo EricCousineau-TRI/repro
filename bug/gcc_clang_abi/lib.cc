60813f0ec4ca41e3ad5a2c63905454ed94d8c1fc
Eric Cousineau <eric.cousineau@tri.global>
2017-11-13 23:03:36 -0500

    Cannot reproduce issue?

---
+++ bug/gcc_clang_abi/lib.cc
@@ -0,0 +1,9 @@
+#include "lib.h"
+
+namespace lib {
+
+std::string get_value() {
+  return "Hello";
+}
+
+}  // namespace lib
---
+++ bug/gcc_clang_abi/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "lib.h"
+
+int main() {
+  std::cout << lib::get_value() << std::endl;
+  return 0;
+}
