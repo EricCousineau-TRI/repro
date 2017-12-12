038a35002421b14128c1cb5fb136430c541b3c43
Eric Cousineau <eric.cousineau@tri.global>
2017-12-12 14:09:03 -0500

    Can draw basic stuff

---
+++ python/matplotlib/graphviz.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+# import tempfile
+# import networkx as nx
+import pydotplus as pydot
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# sudo apt install python-networkx python-pygraphviz # DOES NOT WORK
+# pip install networkx pygraphviz pydotplus
+
+dot = """
+digraph _46955600 {
+rankdir=LR
+subgraph cluster46955600diagram {
+color=black
+concentrate=true
+label="test_diagram";
+subgraph cluster46955600inputports {
+rank=same
+color=lightgrey
+style=filled
+label="input ports"
+_46955600_u0[color=blue, label="u0"];
+_46955600_u1[color=blue, label="u1"];
+_46955600_u2[color=blue, label="u2"];
+}
+subgraph cluster46955600outputports {
+rank=same
+color=lightgrey
+style=filled
+label="output ports"
+_46955600_y0[color=green, label="y0"];
+_46955600_y1[color=green, label="y1"];
+_46955600_y2[color=green, label="y2"];
+}
+subgraph cluster46955600subsystems {
+color=white
+label=""
+42291536 [shape=record, label="adder0|{{<u0>u0|<u1>u1} | {<y0>y0}}"];
+46915616 [shape=record, label="adder1|{{<u0>u0|<u1>u1} | {<y0>y0}}"];
+46913232 [shape=record, label="adder2|{{<u0>u0|<u1>u1} | {<y0>y0}}"];
+46944592 [shape=record, label="integrator0|{{<u0>u0} | {<y0>y0}}"];
+46948240 [shape=record, label="integrator1|{{<u0>u0} | {<y0>y0}}"];
+42291536:y0 -> 46913232:u0;
+46915616:y0 -> 46913232:u1;
+42291536:y0 -> 46915616:u0;
+42291536:y0 -> 46944592:u0;
+46944592:y0 -> 46948240:u0;
+_46955600_u0 -> 42291536:u0 [color=blue];
+_46955600_u1 -> 42291536:u1 [color=blue];
+_46955600_u2 -> 46915616:u1 [color=blue];
+46915616:y0 -> _46955600_y0 [color=green];
+46913232:y0 -> _46955600_y1 [color=green];
+46948240:y0 -> _46955600_y2 [color=green];
+}
+}
+}
+"""
+
+filename = 'tmp.dot'
+with open(filename, 'w') as f:
+    f.write(dot)
+
+# https://stackoverflow.com/questions/4596962/display-graph-without-saving-using-pydot
+
+g = pydot.graph_from_dot_file(filename)
+png = filename + '.png'
+g.write_png(png)
+
+plt.imshow(plt.imread(png), aspect="equal")
+plt.show()
+
+# G = nx.drawing.nx_pydot.read_dot('tmp.dot')
+# print(G)
+# nx.drawing.nx_pydot.write_dot(G, 'tmp_2.dot')
+
+
+
+# nx.draw(G, with_labels=True)
+# plt.draw()
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+# import networkx as nx
+# G=nx.dodecahedral_graph()
+# print(G)
+# nx.drawing.nx_pydot.write_dot(G, 'tmp_do.dot')
+# nx.draw(G)  # networkx draw()
+# plt.draw()  # pyplot draw()
+# plt.show()
+
