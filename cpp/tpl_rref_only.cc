d6cfb592235f4c16b92ff402c735b76f57268aa8
Eric Cousineau <eric.cousineau@tri.global>
2018-01-04 17:54:58 -0500

    Simplify

--- cpp/tpl_rref_only.cc
+++ cpp/tpl_rref_only.cc
@@ -19,27 +19,27 @@ struct greedy_struct {
 
 // Per Toby's answer.
 template <typename T>
-void greedy_sfinae(T&&,
-    std::enable_if_t<!std::is_rvalue_reference<T&&>::value, void*> = {}) {
-  cout << "non-T&& (sfinae)" << endl;
+void greedy_sfinae(const T&) {
+  cout << "const T&& (sfinae)" << endl;
 }
 
-template <typename T>
-void greedy_sfinae(T&&,
-    std::enable_if_t<std::is_rvalue_reference<T&&>::value, void*> = {}) {
+template <
+    typename T,
+    typename = std::enable_if_t<std::is_rvalue_reference<T&&>::value>>
+void greedy_sfinae(T&&) {
   cout << "T&& (sfinae)" << endl;
 }
 
 // Bad.
 template <typename T>
-void greedy_sfinae_bad(T&&,
-    std::enable_if_t<!std::is_rvalue_reference<T>::value, void*> = {}) {
-  cout << "non-T&& (sfinae bad)" << endl;
+void greedy_sfinae_bad(const T&) {
+  cout << "const T&& (sfinae bad)" << endl;
 }
 
-template <typename T>
-void greedy_sfinae_bad(T&&,
-    std::enable_if_t<std::is_rvalue_reference<T>::value, void*> = {}) {
+template <
+    typename T,
+    typename = std::enable_if_t<std::is_rvalue_reference<T>::value>>
+void greedy_sfinae_bad(T&&) {
   cout << "T&& (sfinae bad)" << endl;
 }
 
