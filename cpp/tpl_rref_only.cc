5e803063848708693a60cfbc9a19e90daf512926
Eric Cousineau <eric.cousineau@tri.global>
2018-01-04 15:55:27 -0500

    Being direct does not work...

--- cpp/tpl_rref_only.cc
+++ cpp/tpl_rref_only.cc
@@ -8,7 +8,10 @@ using namespace std;
 struct Value {};
 
 template <typename T>
-using rvalue = T&&;
+using direct = T; //std::conditional_t<std::is_same<T, T>::value, T, void>;
+
+template <typename T>
+using rvalue = std::add_rvalue_reference_t<T>; //direct<T>&&;
 
 template <typename T>
 void greedy(const T&) {
@@ -16,7 +19,7 @@ void greedy(const T&) {
 }
 
 template <typename T>
-void greedy(rvalue<T>) {
+void greedy(direct<T&&>) {
   cout << "T&&" << endl;
 }
 
@@ -27,7 +30,7 @@ int main() {
   greedy(x);
   greedy(y);
   greedy(Value{});
-  greedy(std::move(Value{}));
+  greedy(std::move(x));
 
   return 0;
 }
