3ad9c6562c78a9ca8d6c6abab9bb37368c8a55fa
Eric Cousineau <eric.cousineau@tri.global>
2018-01-04 15:48:56 -0500

    Struggling with rvalues and greediness...

--- cpp/tpl_rref_only.cc
+++ cpp/tpl_rref_only.cc
@@ -1,21 +1,42 @@
 // @ref https://stackoverflow.com/questions/7863603/how-to-make-template-rvalue-reference-parameter-only-bind-to-rvalue-reference
 
 #include <iostream>
+#include <type_traits>
 
 using namespace std;
 
+struct Value {};
+
 template <typename T>
+void greedy(const T&) {
+  cout << "const T&" << endl;
+}
+
+template <typename T, typename =
+    std::enable_if_t<std::is_rvalue_reference<T>::value>>
 void greedy(T&&) {
-  cout << "Greedy" << endl;
+  cout << "T&&" << endl;
 }
 
 int main() {
-  int x{};
-  const int y{};
+  Value x;
+  const Value y;
 
   greedy(x);
   greedy(y);
-  greedy(3);
+  greedy(Value{});
+  greedy(std::move(Value{}));
 
   return 0;
 }
+
+/*
+Output:
+
+const T&
+const T&
+const T&
+const T&
+
+Notes: Why the last two???
+*/
