de5e0bfb15d86a4a0572f12e875e827ad0af5b79
Eric Cousineau <eric.cousineau@tri.global>
2018-01-05 17:11:44 -0500

    Compact

--- cpp/tpl_perfect_fwd.cc
+++ cpp/tpl_perfect_fwd.cc
@@ -8,25 +8,15 @@ template <typename T>
 void func(T&& value) {
   cout
     << nice_type_name<T>() << " = T" << endl
+    << nice_type_name<T&&>() << " = T&&" << endl
     << nice_type_name<decltype(value)>() << " = decltype(value)" << endl;
 }
 
 struct Value {};
 
 int main() {
-  cout << R"""(
-template <typename T>
-void func(T&& value) {
-  cout
-    << nice_type_name<T>() << " = T" << endl
-    << nice_type_name<decltype(value)>() << " = decltype(value)" << endl;
-}
-
-)""";
   const Value x_const;
   Value x;
-
-  cout << nice_type_name<const int&>() << endl;
 
   EVAL(func(x_const));
   EVAL(func(x));
@@ -38,23 +28,19 @@ void func(T&& value) {
 /*
 Output:
 
-template <typename T>
-void func(T&& value) {
-  cout
-    << nice_type_name<T>() << " = T" << endl
-    << nice_type_name<decltype(value)>() << " = decltype(value)" << endl;
-}
-
-const int&
 >>> func(x_const);
 const Value& = T
+const Value& = T&&
 const Value& = decltype(value)
 
 >>> func(x);
 Value& = T
+Value& = T&&
 Value& = decltype(value)
 
 >>> func(std::move(x));
 Value = T
+Value&& = T&&
 Value&& = decltype(value)
+
 */
