e70641c415f0501d481ed91726292a890c73f820
Eric Cousineau <eric.cousineau@tri.global>
2018-01-06 15:39:58 -0500

    Will have to reinstate reclaimation information.

--- python/pybind11/custom_tests/test_unique_ptr.py
+++ python/pybind11/custom_tests/test_unique_ptr.py
@@ -4,8 +4,11 @@ import _test_unique_ptr as m
 try:
     stats = m.ConstructorStats.get(m.UniquePtrHeld)
 
+    print("Create")
     obj = m.UniquePtrHeld(1)
+    print("Call")
     obj_ref = m.unique_ptr_pass_through(obj)
+    print("Check")
     assert stats.alive() == 1
     assert obj.value() == 1
     assert obj == obj_ref
