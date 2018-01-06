d19468e734c5ba4db3cbf959d459439b80d4faf2
Eric Cousineau <eric.cousineau@tri.global>
2018-01-06 16:51:41 -0500

    Workin

--- python/pybind11/custom_tests/test_unique_ptr.py
+++ python/pybind11/custom_tests/test_unique_ptr.py
@@ -1,10 +1,20 @@
+import gc
 import traceback
+
 import _test_unique_ptr as m
 
 try:
     stats = m.ConstructorStats.get(m.UniquePtrHeld)
 
     print("Create")
+    assert stats.alive() == 0
+    obj = m.UniquePtrHeld(1)
+    assert stats.alive() == 1
+    print(" - Destroy")
+    m.unique_ptr_terminal(obj)
+    assert stats.alive() == 0
+
+    print("Create (b)")
     obj = m.UniquePtrHeld(1)
     print("Call")
     obj_ref = m.unique_ptr_pass_through(obj)
