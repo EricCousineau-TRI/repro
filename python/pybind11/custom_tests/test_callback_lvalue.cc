6909a9509992fa352817812357bf8aeb6ce03224
Eric Cousineau <eric.cousineau@tri.global>
2018-01-07 15:26:17 -0500

    Cleanup

--- python/pybind11/custom_tests/test_callback_lvalue.cc
+++ python/pybind11/custom_tests/test_callback_lvalue.cc
@@ -20,29 +20,14 @@ using namespace std;
 
 void bind_ConstructorStats(py::module &m);
 
-class UniquePtrHeld {
-public:
-    UniquePtrHeld() = delete;
-    UniquePtrHeld(const UniquePtrHeld&) = delete;
-    UniquePtrHeld(UniquePtrHeld&&) = delete;
-
-    UniquePtrHeld(int value)
-            : value_(value) {
-        print_created(this, value);
-    }
-    ~UniquePtrHeld() {
-        print_destroyed(this);
-    }
-    int value() const { return value_; }
-private:
-    int value_{};
-};
-
 int main(int argc, char* argv[]) {
     py::scoped_interpreter guard{};
     py::module m("_test_callback_lvalue");
 
-//    bind_ConstructorStats(m);
+    py::dict globals = py::globals();
+    globals["m"] = m;
+
+    bind_ConstructorStats(m);
 
     struct CppCopyable {
         int value{};
@@ -51,32 +36,79 @@ int main(int argc, char* argv[]) {
     cls.def(py::init<>());
     cls.def_readwrite("value", &CppCopyable::value);
 
-//    CppCopyable cpp_obj_orig{1};
-//    py::object obj = py::cast<CppCopyable&>(cpp_obj_orig);
-////    py::object obj = cls();
-//    obj.attr("value") = 20;
-//    CppCopyable& cpp_obj = py::cast<CppCopyable&>(obj);
-//    cpp_obj.value = 200;
-//    py::print("value: ", cpp_obj_orig.value, cpp_obj.value, obj.attr("value"));
+    // Test mutable lvalue references.
+     struct Item {
+         Item(int value_in) : value(value_in) { print_created(this, value); }
+         Item(const Item& other)
+                : value(other.value) {
+             print_copy_created(this, value);
+         }
+         ~Item() { print_destroyed(this); }
+         int value{};
+     };
+     py::class_<Item>(m, "Item")
+         .def(py::init<int>())
+         .def_readwrite("value", &Item::value);
 
-    // Output:
-    // value:  200 200 200
+     struct Container {
+         Item* new_ptr() { return new Item{100}; }
+         Item* get_ptr() { return &item; }
+         Item& get_ref() { return item; }
 
-    py::exec(R"""(
-def incr(obj):
-    obj.value += 1
-    )""", py::globals(), m.attr("__dict__"));
+         // Test casting behavior.
+         py::object cast_copy() {
+             return py::cast(item);
+         }
+         py::object cast_ptr() { return py::cast(&item); }
+         py::object cast_ref() {
+             return py::cast(item, py::return_value_policy::reference);
+         }
 
-    auto func = py::cast<std::function<void(CppCopyable&)>>(m.attr("incr"));
+         Item item{10};
+     };
+     py::class_<Container>(m, "Container")
+         .def(py::init<>())
+         .def("new_ptr", &Container::new_ptr)
+         .def("get_ptr_unsafe", &Container::get_ptr, py::return_value_policy::automatic_reference)
+         .def("get_ref_unsafe", &Container::get_ref)
+         .def("get_ptr", &Container::get_ptr, py::return_value_policy::reference_internal)
+         .def("get_ref", &Container::get_ref, py::return_value_policy::reference_internal)
+         .def("cast_copy", &Container::cast_copy)
+         .def("cast_ref", &Container::cast_ref);
 
-    CppCopyable cpp_obj{10};
-    func(cpp_obj);
-    py::print("value: ", cpp_obj.value);
+    {
+        py::object c = py::eval("m.Container()");
+        c.attr("cast_copy")();
+    }
 
-    // Output: (NOW works)
-    // value: 11
+    {
+        CppCopyable cpp_obj_orig{1};
+        py::object obj = py::cast(cpp_obj_orig);
+        obj.attr("value") = 20;
+        CppCopyable& cpp_obj = py::cast<CppCopyable&>(obj);
+        cpp_obj.value = 200;
+        py::print("value: ", cpp_obj_orig.value, cpp_obj.value, obj.attr("value"));
 
-    // Does not work as expected, because pybind will copy the instance when
+        // Should be: "value:  1 200 200"
+    }
+
+    {
+        py::dict locals;
+        py::exec(R"""(
+    def incr(obj):
+        obj.value += 1
+        )""", py::globals(), locals);
+
+        auto func = py::cast<std::function<void(CppCopyable&)>>(locals["incr"]);
+        CppCopyable cpp_obj{10};
+        func(cpp_obj);
+        py::print("value: ", cpp_obj.value);
+
+        // Without patch: "value: 10"
+        // With patch: "value: 11"
+    }
+
+    // Works as expected with patch, because pybind will copy the instance when
     // binding.
     py::cpp_function func_ref(
         [](std::function<void(CppCopyable&)> f, int value) {
@@ -93,9 +125,6 @@ def incr(obj):
             return obj;
         });
     m.attr("callback_mutate_copyable_cpp_ptr") = func_ptr;
-
-    py::dict globals = py::globals();
-    globals["m"] = m;
 
     py::str file;
     if (argc < 2) {
--- python/pybind11/custom_tests/test_callback_lvalue.py
+++ python/pybind11/custom_tests/test_callback_lvalue.py
@@ -6,7 +6,7 @@ def incr(obj):
     obj.value += 1
 
 obj = m.callback_mutate_copyable_cpp_ref(incr, 10)
-assert obj.value == 11
+assert obj.value == 11  # without patch: obj.value == 10
 
 obj = m.callback_mutate_copyable_cpp_ptr(incr, 10)
 assert obj.value == 11
