7a4d9c14d45faedd5c6f0038f3ff6e95de2cea98
Eric Cousineau <eric.cousineau@tri.global>
2018-01-08 09:52:01 -0500

    Generalize a wee bit more.

--- cpp/unique_ptr_tracked.cc
+++ cpp/unique_ptr_tracked.cc
@@ -5,104 +5,107 @@
 
 using namespace std;
 
-// Allow tracking a unique ptr.
-// ... er... Can this track the value too?
-// `Deleter` must be trivially destructible.
+// // Allow tracking a unique ptr.
+// // ... er... Can this track the value too?
+// // `Deleter` must be trivially destructible.
+// template <typename T>
+// class Tracker {
+//  public:
+//   Tracker() = default;
+//   template <typename Deleter>
+//   Tracker(Deleter&&) {
+//     cout << "Moved from another unique_ptr" << endl;
+//   }
+//   ~Tracker() {
+//     cout << "Tracker removed" << endl;
+//   }
+//   template <typename Deleter>
+//   Tracker& operator=(Deleter&&) {
+//     cout << "Moved from another unique_ptr" << endl;
+//     return *this;
+//   }
+//   void operator()(T* ptr) {
+//     // Only called when `ptr` is non-null.
+//     cout << "Ptr Delete: " << ptr << endl;
+//     assert(ptr);
+//     delete ptr;
+//   }
+//   template <typename Deleter>
+//   operator Deleter() {
+//     cout << "Moved to another unique_ptr" << endl;
+//     return Deleter{};
+//   }
+// };
+
+// template <typename T>
+// using unique_ptr_tracked_meh = unique_ptr<T, Tracker<T>>;
+
 template <typename T>
-class Tracker {
+class ptr_tracker {
  public:
-  Tracker() = default;
-  template <typename Deleter>
-  Tracker(Deleter&&) {
-    cout << "Moved from another unique_ptr" << endl;
+  static void on_claim(T* ptr) {
+    cout << "Claim: " << ptr << endl;
   }
-  ~Tracker() {
-    cout << "Tracker removed" << endl;
+
+  static void on_release(T* ptr) {
+    cout << "Release: " << ptr << endl;
   }
-  template <typename Deleter>
-  Tracker& operator=(Deleter&&) {
-    cout << "Moved from another unique_ptr" << endl;
-    return *this;
-  }
-  void operator()(T* ptr) {
-    // Only called when `ptr` is non-null.
-    cout << "Ptr Delete: " << ptr << endl;
-    assert(ptr);
-    delete ptr;
-  }
-  template <typename Deleter>
-  operator Deleter() {
-    cout << "Moved to another unique_ptr" << endl;
-    return Deleter{};
+
+  static void on_delete(std::unique_ptr<T> ptr) {
+    cout << "Delete: " << ptr.get() << endl;
   }
 };
 
-template <typename T>
-using unique_ptr_tracked_meh_meh = unique_ptr<T, Tracker<T>>;
-
-template <typename T, typename D = std::default_delete<T>>
+template <
+  typename T, typename Tracker = ptr_tracker<T>,
+  typename D = std::default_delete<T>>
 class unique_ptr_tracked {
  public:
   unique_ptr_tracked() = default;
   unique_ptr_tracked(T* ptr)
     : ptr_(ptr) {
-    on_claim();
+    Tracker::on_claim(ptr_.get());
   }
   unique_ptr_tracked(const unique_ptr_tracked&) = delete;
   ~unique_ptr_tracked() {
-    on_delete();
+    Tracker::on_delete(std::move(ptr_));
   }
 
   template <typename U, typename E>
   unique_ptr_tracked(std::unique_ptr<U, E>&& ptr) {
     ptr_ = std::move(ptr);
-    on_claim();
+    Tracker::on_claim(ptr_.get());
   }
 
   template <typename U, typename E>
   unique_ptr_tracked(unique_ptr_tracked<U, E>&& other) {
     ptr_ = std::move(other);  // Let it invoke it's `on_release()`.
-    on_claim();
+    Tracker::on_claim(ptr_.get());
   }
 
   template <typename U, typename E>
   unique_ptr_tracked& operator=(std::unique_ptr<U, E>&& ptr) {
     ptr_ = std::move(ptr);
-    on_claim();
+    Tracker::on_claim(ptr_.get());
     return *this;
   }
   unique_ptr_tracked& operator=(const unique_ptr_tracked&) = delete;
 
   template <typename U, typename E>
   operator std::unique_ptr<U, E>() {
-    on_release();
+    Tracker::on_release(ptr_.get());
     return std::move(ptr_);
   }
 
   T* get() const { return ptr_.get(); }
   T* release() {
-    on_release();
+    Tracker::on_release(ptr_.get());
     return ptr_.release();
   }
   T& operator*() const { return *ptr_; }
   T* operator->() const { return ptr_.operator->(); }
  private:
   std::unique_ptr<T, D> ptr_;
-
-  void on_claim() {
-    cout << "Claim: " << ptr_.get() << endl;
-  }
-
-  void on_release() {
-    cout << "Release: " << ptr_.get() << endl;
-  }
-
-  void on_delete() {
-    cout << "Delete: " << ptr_.get() << endl;
-  }
-
-  // template <typename U, typename E>
-  // friend class unique_ptr_tracked;
 };
 
 
