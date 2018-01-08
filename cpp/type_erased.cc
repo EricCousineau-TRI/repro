c10c765bbe8d87e5b8fedc491620f20db8e56e17
Eric Cousineau <eric.cousineau@tri.global>
2018-01-08 14:30:06 -0500

    Generalize.

--- cpp/type_erased.cc
+++ cpp/type_erased.cc
@@ -10,45 +10,58 @@ using std::endl;
       throw std::runtime_error("assert failed: " #expr); \
     }
 
+struct type_hash_ptr {
+  using type = const std::type_info*;
+  template <typename T>
+  static type hash() { return &typeid(T); }
+};
+
 class ptr_erased {
  public:
+  using type_hasher = type_hash_ptr;
+
+  ptr_erased() = default;
+  ptr_erased(std::nullptr_t) : ptr_erased() {}
+
   template <typename T>
   ptr_erased(T* ptr)
-    : ptr_(ptr), const_(false), tinfo_(typeid(T).hash_code()) {}
+    : ptr_(ptr), is_const_(false), type_(type_hasher::hash<T>()) {}
 
   template <typename T>
   ptr_erased(const T* ptr)
-    : ptr_(const_cast<T*>(ptr)), const_(true),
-      tinfo_(typeid(T).hash_code()) {}
-
-  ptr_erased() = default;
+    : ptr_(const_cast<T*>(ptr)), is_const_(true),
+      type_(type_hasher::hash<T>()) {}
 
   operator bool() const {
     return ptr_ != nullptr;
   }
 
+  bool is_const() const { return is_const_; }
+
+  type_hasher::type type_hash() const { return type_; }
+
   template <typename T>
   const T* cast() const {
-    assert_ex(typeid(T).hash_code() == tinfo_);
+    assert_ex(type_hasher::hash<T>() == type_);
     return reinterpret_cast<const T*>(ptr_);
   }
 
   template <typename T>
   T* mutable_cast() const {
-    assert_ex(!const_);
-    assert_ex(typeid(T).hash_code() == tinfo_);
+    assert_ex(!is_const_);
+    assert_ex(type_hasher::hash<T>() == type_);
     return reinterpret_cast<T*>(ptr_);
   }
  
  private:
   void* ptr_{};
-  bool const_{};
-  size_t tinfo_{};
+  bool is_const_{};
+  type_hasher::type type_{};
 };
 
 int main() {
-  int x{};
-  const double y{};
+  int x{1};
+  const double y{1.5};
   ptr_erased ptr = &x;
   assert_ex(ptr);
   cout << *ptr.cast<int>() << endl;
@@ -57,10 +70,16 @@ int main() {
   ptr = &y;
   cout << *ptr.cast<double>() << endl;
   try {
+    cout << *ptr.cast<int>() << endl;  // Should trigger exception.
+  } catch (const std::runtime_error& e) {
+    cout << e.what() << endl;
+  }
+  try {
     *ptr.mutable_cast<double>() = 1.5;  // Should trigger exception.
   } catch (const std::runtime_error& e) {
     cout << e.what() << endl;
   }
+  ptr = nullptr;
 
   return 0;
 }
