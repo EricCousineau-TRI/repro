7454883a087bf1ceb5b2798015d1c8993b801103
Eric Cousineau <eric.cousineau@tri.global>
2018-01-08 09:23:38 -0500

    Generalize to `unique_ptr_tracked`.

--- cpp/unique_ptr_tracked.cc
+++ cpp/unique_ptr_tracked.cc
@@ -5,41 +5,47 @@
 
 using namespace std;
 
+// Allow tracking a unique ptr.
+// `Deleter` must be trivially destructible.
 template <typename T>
 class Tracker {
  public:
   Tracker() = default;
-  Tracker(std::default_delete<T>&&) {
-    cout << "Converted from original" << endl;
-  }
-  Tracker(Tracker&& other) {
-    cout << "Moved Tracker" << endl;
+  template <typename Deleter>
+  Tracker(Deleter&&) {
+    cout << "Moved from another unique_ptr" << endl;
   }
   ~Tracker() {
-    cout << "Deleted Tracker" << endl;
+    cout << "Tracker removed" << endl;
   }
-  Tracker& operator=(std::default_delete<T>&&) {
-    cout << "Converted from original" << endl;
+  template <typename Deleter>
+  Tracker& operator=(Deleter&&) {
+    cout << "Moved from another unique_ptr" << endl;
     return *this;
   }
   void operator()(T* ptr) {
     // Only called when `ptr` is non-null.
-    cout << "Delete: " << ptr << endl;
+    cout << "Ptr Delete: " << ptr << endl;
     assert(ptr);
     delete ptr;
   }
-  operator std::default_delete<T>() {
-    cout << "Converted to original" << endl;
-    return std::default_delete<T>{};
+  template <typename Deleter>
+  operator Deleter() {
+    cout << "Moved to another unique_ptr" << endl;
+    return Deleter{};
   }
 };
+
+template <typename T>
+using unique_ptr_tracked = unique_ptr<T, Tracker<T>>;
 
 struct Test { int value{}; };
 
 int main() {
   unique_ptr<Test> ptr = make_unique<Test>();
-  unique_ptr<Test, Tracker<Test>> ptr_2 = std::move(ptr);
-  ptr = std::move(ptr_2);
+  unique_ptr_tracked<Test> ptr_2 = std::move(ptr);
+  unique_ptr_tracked<Test> ptr_3 = std::move(ptr_2);
+  ptr = std::move(ptr_3);
   ptr_2 = std::move(ptr);
   cout << "Done" << endl;
   return 0;
