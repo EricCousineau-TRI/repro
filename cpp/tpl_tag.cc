740f31f385d79858dd3b9c603186af49cf8038da
Eric Cousineau <eric.cousineau@tri.global>
2018-01-16 15:15:19 -0500

    Focus on aliasing.

--- cpp/tpl_tag.cc
+++ cpp/tpl_tag.cc
@@ -2,65 +2,106 @@
 
 using namespace std;
 
-template <template <typename> class Tpl>
-struct template_tag {
-  template <typename T>
-  using bind = Tpl<T>;
-};
-
 template <template <typename...> class Tpl>
-struct template_pack_tag {
+struct template_tag {
   template <typename ... Ts>
   using bind = Tpl<Ts...>;
 };
 
+template <template <typename> class Tpl>
+struct template_single_tag {
+  template <typename T>
+  using bind = Tpl<T>;
+};
+
+// // Cannot do this: Mixes parameter types.
+// template <template <typename...> class Tpl>
+// struct template_single_tag<Tpl, void> : public template_tag<Tpl> {};
 
 template <typename T>
 struct single_tpl {};
 
+template <typename T>
+using single_tpl_alias = single_tpl<T>;
+
+template <typename ... Ts>
+using single_tpl_alias_pack = single_tpl<Ts...>;
+
 template <typename T, typename U>
 struct double_tpl {};
+
+template <typename T, typename U>
+using double_tpl_alias = double_tpl<T, U>;
 
 template <typename T, typename ... Ts>
 struct pack_tpl {};
 
-struct nested {
-  template <typename T>
-  using type = single_tpl<T>;
-};
+template <typename T, typename ... Ts>
+using pack_tpl_alias = pack_tpl<T, Ts...>;
+
+template <template <typename> class Tpl>
+auto create_template_tag(template_single_tag<Tpl> tag = {}) {
+  return tag;
+}
+
+// // Ambiguous with above.
+// template <template <typename...> class Tpl>
+// auto create_template_tag(template_tag<Tpl> tag = {}) {
+//   return tag;
+// }
 
 int main() {
+  {
+    template_single_tag<single_tpl> tag{};
+    decltype(tag)::bind<int> t{};
+    (void)t;
+  }
+
   {
     template_tag<single_tpl> tag{};
     decltype(tag)::bind<int> t{};
     (void)t;
   }
 
+  // Try aliasing.
   {
-    template_pack_tag<single_tpl> tag{};
+    // template_tag<single_tpl_alias> tag{};  // Does NOT work. 
+    // auto tag = create_template_tag<nested::type>();  // Cannot overload.
+    template_single_tag<single_tpl_alias> tag{};
+    decltype(tag)::bind<int> t{};
+    (void)t;
+  }
+  {
+    template_tag<single_tpl_alias_pack> tag{};  // DOES work.
     decltype(tag)::bind<int> t{};
     (void)t;
   }
 
   {
-    template_pack_tag<double_tpl> tag{};
+    template_tag<double_tpl> tag{};
     decltype(tag)::bind<int, double> t{};
     (void)t;
   }
 
+  // {
+  //   // Does NOT work.
+  //   template_tag<double_tpl_alias> tag{};
+  //   decltype(tag)::bind<int, double> t{};
+  //   (void)t;
+  // }
+
   {
-    template_pack_tag<pack_tpl> tag{};
+    template_tag<pack_tpl> tag{};
     decltype(tag)::bind<int, double, char> t{};
     (void)t;
   }
 
-  // Try nesting.
-  {
-    // template_pack_tag<nested::type> tag{};  // Does not work???
-    template_tag<nested::type> tag{};
-    decltype(tag)::bind<int> t{};
-    (void)t;
-  }
+  // {
+  //   // Does NOT work.
+  //   template_tag<pack_tpl_alias> tag{};
+  //   decltype(tag)::bind<int, double, char> t{};
+  //   (void)t;
+  // }
 
   return 0;
 }
