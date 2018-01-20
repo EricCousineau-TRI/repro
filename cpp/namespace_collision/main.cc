68700b76d912c859c4901ebe62b5aad458f85c3f
Eric Cousineau <eric.cousineau@tri.global>
2018-01-20 11:37:37 -0500

    No solution in sight. *sigh*

--- cpp/namespace_collision/main.cc
+++ cpp/namespace_collision/main.cc
@@ -1,9 +1,11 @@
 #include "header_1.h"
 // #include "header_2.h"
 
-namespace alias = my_lib;
+// namespace alias = my_lib;
 using namespace header_1;
 // using namespace header_2;
+
+namespace alias = my_lib;
 
 int main() {
   alias::my_func();
