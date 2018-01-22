3baaf432cde54c932344baaa9e4e041ae472b9d2
Eric Cousineau <eric.cousineau@tri.global>
2018-01-22 01:23:58 -0500

    Some refactoring.

--- python/const_wrapper.py
+++ python/const_wrapper.py
@@ -42,79 +42,85 @@ def _rebind_method(bound, new_self):
     # https://stackoverflow.com/a/14574713/7829525
     return MethodType(bound.__func__, new_self, bound.im_class)
 
-class ConstInfo(object):
-    def __init__(self, owned={}, mutable={}):
-        self.owned = owned
-        self.mutable = mutable
-        self.finished = False
-
-    def merge(self, parent):
-        self.owned.update(parent.owned)
-        self.mutable.update(parent.mutable)
-
-
-    @staticmethod
-    def from_cls(cls, owned=None, mutable=None):
-        # No inheritance, handled by `_ConstRegistry`.
+class _ConstClassMeta(object):
+    def __init__(self, cls, owned=None, mutable=None):
+        self._cls = cls
         if owned is None:
             owned = set()
         if mutable is None:
             mutable = set()
-        for name, value in inspect.getmembers(cls, predicate=inspect.ismethod):
-            if getattr(value, '_is_mutable_method', False):
+        self._owned = set(owned)  # set of strings
+        self._mutable = set(mutable)  # set of strings
+        # Add any decorated mutable methods.
+        methods = inspect.getmembers(cls, predicate=inspect.ismethod)
+        for name, method in methods:
+            if getattr(method, '_is_mutable_method', False):
                 mutable.add(name)
-        return ConstInfo(set(owned), set(mutable))
+        # Recursion / inheritance is handled by `_ConstClasses`, to handle
+        # caching.
 
+    def update(self, parent):
+        self._owned.update(parent._owned)
+        self._mutable.update(parent._mutable)
 
-class _ConstRegistry(object):
-    def __init__(self, info):
-        self._info = info
+    def is_owned(self, name):
+        return name in self._owned
 
-    def _get_info(self, cls):
-        # Assume class has not changed if it's already registered.
-        info = self._info.get(cls, None)
-        if info is not None:
-            assert info.finished
-            return info
-        info = getattr(cls, '_const_info', None) or ConstInfo.from_cls(cls)
+    def is_mutable(self, name):
+        # Limitation: This would not handle overloads.
+        # (e.g. `const T& get() const` and `T& get()`.
+        return name in self._mutable
+
+class _ConstClasses(object):
+    def __init__(self):
+        self._info = {}
+
+    def emplace(self, cls, owned=None, mutable=None):
+        info = _ConstClassMeta(cls, owned, mutable)
+        return self._add_info(cls, info)
+
+    def _add_info(self, cls, info):
+        assert cls not in self._info
+        # Recurse through bases for complete information.
         for base in cls.__bases__:
-            info.merge(self._get_info(base))
+            info.update(self.get_info(base))
         self._info[cls] = info
-        info.finished = True
         return info
 
-    def is_owned(self, cls, name):
-        return name in self._get_info(cls).owned
+    def get_info(self, cls):
+        # Assume class has not changed if it's already registered.
+        info = self._info.get(cls, None)
+        if info:
+            return info
+        else:
+            # Construct default.
+            return self._add_info(cls, _ConstClassMeta(cls))
 
-    def is_mutable(self, cls, name):
-        return name in self._get_info(cls).mutable
+_const_classes = _ConstClasses()
+_emplace = _const_classes.emplace
+# Register common mutators.
+_emplace(list, mutable={
+    'append', 'clear', 'insert', 'extend', 'insert', 'pop',
+    'remove', 'sort'})
+_emplace(dict, mutable={'clear', 'setdefault'})
 
-_const_registry = _ConstRegistry({
-        list: ConstInfo(
-            mutable={
-                'append', 'clear', 'insert', 'extend', 'insert', 'pop',
-                'remove', 'sort'}),
-        dict: ConstInfo(
-            mutable={'clear', 'setdefault'}),
-    })
- 
 class Const(ObjectProxy):
     def __init__(self, wrapped):
         ObjectProxy.__init__(self, wrapped)
 
     def __getattr__(self, name):
         wrapped = object.__getattribute__(self, '__wrapped__')
-        cls = type(wrapped)
+        info = _const_classes.get_info(type(wrapped))
         value = getattr(wrapped, name)
-        if _const_registry.is_mutable(cls, name):
+        if info.is_mutable(name):
             # If explicitly mutable, raise an error.
             # (For complex situations.)
-            _raise_error(self, name)
+            _raise_mutable_method_error(self, name)
         elif _is_method_of(value, wrapped):
             # Propagate const-ness into method (replace `im_self`) to catch
             # basic violations.
             return _rebind_method(value, self)
-        elif _const_registry.is_owned(cls, name):
+        elif info.is_owned(name):
             # References (pointer-like things) should not be const, but
             # internal values should.
             return to_const(value)
@@ -150,16 +156,22 @@ class ConstIter(object):
         return to_const(n)
 
 def to_const(obj):
-    # TODO: Check if literal type?
     if obj is not None:
         if _is_literal(type(obj)):
+            # Literals / immutable types have no references.
+            # No need to waste a proxy.
             return obj
         else:
             return Const(obj)
 
-def const_cast(obj):
+def to_mutable(obj, force=False):
+    # Forced conversion.
+    if not force and is_const(obj):
+        _raise_mutable_cast_error(obj)
     if isinstance(obj, Const):
         return obj.__wrapped__
+    elif isinstance(obj, ConstIter):
+        return obj._iter
     else:
         return obj
 
@@ -180,7 +192,11 @@ def type_ex(obj):
 do_get = object.__getattribute__
 do_set = setattr #object.__setattr__
 
-def _raise_error(self, name):
+def _raise_mutable_cast_error(self):
+    raise ConstError(
+        "Cannot cast const {} to mutable instance".format(type_ex(self)))
+
+def _raise_mutable_method_error(self, name):
     raise ConstError(
         ("'{}' is a mutable method that cannot be called on a " +
         "const {}.").format(name, type_ex(self)))
@@ -188,7 +204,7 @@ def _raise_error(self, name):
 for f in mutable_functions:
     def _new_scope(f):
         def _no_access(self, *args, **kwargs):
-            _raise_error(self, f)
+            _raise_mutable_method_error(self, f)
         _no_access.__name__ = f
         do_set(Const, f, _no_access)
     _new_scope(f)
@@ -198,7 +214,7 @@ def mutable(func):
     return func
 
 def add_const_info(cls, owned=None, mutable=None):
-    cls._const_info = ConstInfo.from_cls(cls, owned, mutable)
+    _const_classes.emplace(cls, owned, mutable)
     return cls
 
 def const_info(owned=None, mutable=None):
@@ -227,7 +243,7 @@ class Check(object):
 
     def do_something(self, stuff):
         print("{}: {}".format(stuff, self._value))
-        # self.set_value(10)  # Causes error.
+        # self.set_value(10)  # Raises error.
 
     @mutable
     def mutate(self): pass
@@ -247,19 +263,19 @@ class SubCheck(Check):
         self.set_map(10, 10000)
 
     def more(self):
-        # self._my_value.append(10)
+        # self._my_value.append(10)  # Raises error.
         return self._my_value
 
 c = Check(10)
 c_const = to_const(c)
 print(c_const.value)
-# c_const.value = 100
-# c_const.set_value(100)
+# c_const.value = 100  # Raises error.
+# c_const.set_value(100)  # Raises error.
 c_const.do_something("yo")
 print(c_const == c_const)
 print(c_const.get_map(10))
-# print(c_const.set_map(10, 100))
-# c_const[10] = 10
+# print(c_const.set_map(10, 100))  # Raises error.
+# c_const[10] = 10  # Raises error.
 
 c.value = 100
 print(c_const.value)
@@ -276,22 +292,23 @@ s_const = to_const(s)
 c.print_self()
 c_const.print_self()
 s_const.print_self()
-# s_const.extra()
-# s_const.mutate()
+# s_const.extra()  # Raises error.
+# s_const.mutate()  # Raises error.
 x = s_const.more()
 print(x)
 print(type(x))
-# x[:] = []
+# x[:] = []  # Raises error.
 
 obj = to_const([1, 2, [10, 10]])
 print(is_const(obj))
 print(is_const([]))
-# obj[1] = 3
-const_cast(obj)[1] = 10
+# obj[1] = 3  # Raises error.
+# to_mutable(obj)[1] = 10  # Raises error.
+to_mutable(obj, force=True)[1] = 10
 print(obj)
 
 for i in obj:
     print(i)
     if isinstance(i, list):
         print("woo")
-        # i[0] = 10
+        # i[0] = 10  # Raises error.
