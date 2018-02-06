ab375b29d7ba119e785edab1a72a726bba937d9c
Eric Cousineau <eric.cousineau@tri.global>
2018-02-06 16:58:41 -0500

    Can simplify further.

--- bug/bazel_external_py_shared_lib/example_ldd.sh
+++ bug/bazel_external_py_shared_lib/example_ldd.sh
@@ -5,14 +5,15 @@ src_dir=$(cd $(dirname $0) && pwd)
 runfiles_dir=${PWD}
 mkdir -p no_neighbor && cd no_neighbor
 
-check-lib() {
+check() {
     local name=$1
     ldd ${src_dir}/${name} | grep "not found"
     ldd ${runfiles_dir}/${name} | grep "not found"
 }
 
-check-lib libexample_lib_cc.so
-check-lib example_lib_py.so
-check-lib libsecond_lib_cc.so
+check example_cc
+check libexample_lib_cc.so
+check libsecondary_direct_lib_cc.so
+check libsecondary_indirect_lib_cc.so
 
 exit 0
