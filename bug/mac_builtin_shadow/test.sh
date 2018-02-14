b08bc47c21a10c951a256334480d83beff097403
Eric Cousineau <eric.cousineau@tri.global>
2018-02-14 18:07:41 -0500

    Simplify

--- bug/mac_builtin_shadow/test.sh
+++ bug/mac_builtin_shadow/test.sh
@@ -11,71 +11,12 @@ cd build
 
 mkdir example_module
 cd example_module
+
 touch __init__.py
-
-use_py=${USE_PY:-}
-
-if [[ -z ${use_py} ]]; then
-
-echo "Using shared library"
-
-cat > math.c <<EOF
-// https://docs.python.org/2/extending/extending.html
-
-#include <Python.h>
-
-static PyObject *
-spam_system(PyObject *self, PyObject *args)
-{
-const char *command;
-int sts;
-
-if (!PyArg_ParseTuple(args, "s", &command))
-    return NULL;
-sts = system(command);
-return Py_BuildValue("i", sts);
-}
-
-static PyMethodDef SpamMethods[] = {
-{"stuff",  spam_system, METH_VARARGS,
- "Execute a shell command."},
-{NULL, NULL, 0, NULL}        /* Sentinel */
-};
-
-PyMODINIT_FUNC
-initmath(void)
-{
-(void) Py_InitModule("math", SpamMethods);
-}
-EOF
-
-cat > _math_build.py <<EOF
-# https://docs.python.org/2/extending/building.html#building
-
-from distutils.core import setup, Extension
-
-module1 = Extension('math',
-                sources = ['math.c'])
-
-setup (name = 'PackageName',
-   version = '1.0',
-   description = 'This is a demo package',
-   ext_modules = [module1])
-EOF
-
-python _math_build.py build
-cp $(find . -name '*.so') .
-
-else
-
-echo "Using .py"
 
 cat > math.py <<EOF
 stuff = 1
 EOF
-
-fi
-
 
 cat > other.py <<EOF
 from __future__ import absolute_import
