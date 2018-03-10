782446a6769ddf5f6074d0c97a8e41a3e887682e
Eric Cousineau <eric.cousineau@tri.global>
2018-03-10 01:59:36 -0500

    Embrace type erasure

--- python/pybind11/dtype_stuff/test_basic.cc
+++ python/pybind11/dtype_stuff/test_basic.cc
@@ -17,31 +17,31 @@ using std::endl;
 namespace py = pybind11;
 
 class Custom {
- public:
-  using Self = Custom;
+public:
+    using Self = Custom;
 
-  Custom() {}
-  Custom(double value) : value_{value} {}
-  Custom(const Custom&) = default;
-  Custom& operator=(const Custom&) = default;
+    Custom() {}
+    Custom(double value) : value_{value} {}
+    Custom(const Custom&) = default;
+    Custom& operator=(const Custom&) = default;
 
-  Self operator==(const Self& rhs) const { return value_ + rhs.value_; }
+    Self operator==(const Self& rhs) const { return value_ + rhs.value_; }
 
-  Custom operator*(const Custom& rhs) const {
-      return value_ * rhs.value_;
-  }
+    Custom operator*(const Custom& rhs) const {
+        return value_ * rhs.value_;
+    }
 
-  double value() const { return value_; }
+    double value() const { return value_; }
 
-  static Self equal(const Self& lhs, const Self& rhs) {
-    return lhs == rhs;
-  }
-  static Self multiply(const Self& lhs, const Self& rhs) {
-    return lhs * rhs;
-  }
+    static Self equal(const Self& lhs, const Self& rhs) {
+        return lhs == rhs;
+    }
+    static Self multiply(const Self& lhs, const Self& rhs) {
+        return lhs * rhs;
+    }
 
- private:
-  double value_{};
+private:
+    double value_{};
 };
 
 void module(py::module m) {}
@@ -52,7 +52,7 @@ namespace pybind11 { namespace detail {
 
 template <>
 struct npy_format_descriptor<Custom> {
-  static const int& value;
+    static const int& value;
 };
 
 const int& npy_format_descriptor<Custom>::value = npy_rational;
@@ -72,91 +72,91 @@ byteswap(npy_int32* x) {
 }
 
 int main() {
-  py::scoped_interpreter guard;
+    py::scoped_interpreter guard;
 
-  py::module m("__main__");
+    py::module m("__main__");
 
-  using Class = Custom;
-  py::class_<Class> cls(m, "Custom");
-  cls
-      .def(py::init<double>())
-      .def(py::self == Class{})
-      .def(py::self * Class{})
-      .def("value", &Class::value)
-      .def("__repr__", [](const Class* self) {
-        return py::str("Custom({})").format(self->value());
-      });
+    using Class = Custom;
+    py::class_<Class> cls(m, "Custom");
+    cls
+        .def(py::init<double>())
+        .def(py::self == Class{})
+        .def(py::self * Class{})
+        .def("value", &Class::value)
+        .def("__repr__", [](const Class* self) {
+            return py::str("Custom({})").format(self->value());
+        });
 
-  // Register thing.
+    // Register thing.
 
-  _import_array();
-  _import_umath();
+    _import_array();
+    _import_umath();
 
-  py::module numpy = py::module::import("numpy");
-  auto py_type = (PyTypeObject*)cls.ptr();
+    py::module numpy = py::module::import("numpy");
+    auto py_type = (PyTypeObject*)cls.ptr();
 
-  typedef struct { char c; Class r; } align_test;
+    typedef struct { char c; Class r; } align_test;
 
-  static PyArray_ArrFuncs npyrational_arrfuncs;
-  static PyArray_Descr npyrational_descr = {
-      PyObject_HEAD_INIT(0)
-      py_type,                /* typeobj */
-      'V',                    /* kind (V = arbitrary) */
-      'r',                    /* type */
-      '=',                    /* byteorder */
-      /*
-       * For now, we need NPY_NEEDS_PYAPI in order to make numpy detect our
-       * exceptions.  This isn't technically necessary,
-       * since we're careful about thread safety, and hopefully future
-       * versions of numpy will recognize that.
-       */
-      NPY_NEEDS_PYAPI | NPY_USE_GETITEM | NPY_USE_SETITEM, /* flags */
-      0,                      /* type_num */
-      sizeof(Class),       /* elsize */
-      offsetof(align_test,r), /* alignment */
-      0,                      /* subarray */
-      0,                      /* fields */
-      0,                      /* names */
-      &npyrational_arrfuncs,  /* f */
-  };
+    static PyArray_ArrFuncs npyrational_arrfuncs;
+    static PyArray_Descr npyrational_descr = {
+        PyObject_HEAD_INIT(0)
+        py_type,                /* typeobj */
+        'V',                    /* kind (V = arbitrary) */
+        'r',                    /* type */
+        '=',                    /* byteorder */
+        /*
+         * For now, we need NPY_NEEDS_PYAPI in order to make numpy detect our
+         * exceptions.  This isn't technically necessary,
+         * since we're careful about thread safety, and hopefully future
+         * versions of numpy will recognize that.
+         */
+        NPY_NEEDS_PYAPI | NPY_USE_GETITEM | NPY_USE_SETITEM, /* flags */
+        0,                      /* type_num */
+        sizeof(Class),       /* elsize */
+        offsetof(align_test,r), /* alignment */
+        0,                      /* subarray */
+        0,                      /* fields */
+        0,                      /* names */
+        &npyrational_arrfuncs,  /* f */
+    };
 
-  PyArray_InitArrFuncs(&npyrational_arrfuncs);
-  npyrational_arrfuncs.getitem = [](void* in, void* arr) -> PyObject* {
-    return py::cast(*(const Class*)in).release().ptr();
-  };
-  npyrational_arrfuncs.setitem = [](PyObject* in, void* out, void* arr) {
-    *(Class*)out = *py::handle(in).cast<Class*>();
-    return 0;
-  };
-  npyrational_arrfuncs.copyswap = [](void* dst, void* src, int swap, void* arr) {
-      if (!src) return;
-      Class* r_dst = (Class*)dst;
-      Class* r_src = (Class*)src;
-      if (swap) {
-          std::swap(*r_dst, *r_src);
-      } else {
-        *r_dst = *r_src;
-      }
-  };
-  Py_TYPE(&npyrational_descr) = &PyArrayDescr_Type;
-  npy_rational = PyArray_RegisterDataType(&npyrational_descr);
-  cls.attr("dtype") = py::reinterpret_borrow<py::object>(
-      py::handle((PyObject*)&npyrational_descr));
+    PyArray_InitArrFuncs(&npyrational_arrfuncs);
+    npyrational_arrfuncs.getitem = [](void* in, void* arr) -> PyObject* {
+        return py::cast(*(const Class*)in).release().ptr();
+    };
+    npyrational_arrfuncs.setitem = [](PyObject* in, void* out, void* arr) {
+        *(Class*)out = *py::handle(in).cast<Class*>();
+        return 0;
+    };
+    npyrational_arrfuncs.copyswap = [](void* dst, void* src, int swap, void* arr) {
+        if (!src) return;
+        Class* r_dst = (Class*)dst;
+        Class* r_src = (Class*)src;
+        if (swap) {
+            std::swap(*r_dst, *r_src);
+        } else {
+            *r_dst = *r_src;
+        }
+    };
+    Py_TYPE(&npyrational_descr) = &PyArrayDescr_Type;
+    npy_rational = PyArray_RegisterDataType(&npyrational_descr);
+    cls.attr("dtype") = py::reinterpret_borrow<py::object>(
+        py::handle((PyObject*)&npyrational_descr));
 
-  auto ufunc = [&numpy](const char* name) {
-    return (PyUFuncObject*)numpy.attr(name).ptr();
-  };
+    auto ufunc = [&numpy](const char* name) {
+        return (PyUFuncObject*)numpy.attr(name).ptr();
+    };
 
-  BinaryUFunc<Class, Class, Class, Class::equal>::Register(ufunc("equal"));
-//    BinaryUFunc<Class, Class, Class, Class::multiply>::Register(ufunc("multiply"));
+    RegisterBinaryUFunc<Class>(ufunc("equal"), Class::equal);
+    RegisterBinaryUFunc<Class>(ufunc("multiply"), Class::multiply);
 
     py::str file = "python/pybind11/dtype_stuff/test_basic.py";
     py::print(file);
     py::eval_file(file);
 
-  py::exec(R"""(
+    py::exec(R"""(
 x = np.array([Custom(1)])
 )""");
 
-  return 0;
+    return 0;
 }
--- python/pybind11/dtype_stuff/ufunc_utility.h
+++ python/pybind11/dtype_stuff/ufunc_utility.h
@@ -11,21 +11,19 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
+#include "cpp/wrap_function.h"
+
 namespace py = pybind11;
 using py::detail::npy_format_descriptor;
 
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/ufuncobject.h>
 
-template <typename T>
-void* heapit(const T& x) { return new T(x); }
-void* heapit(std::nullptr_t) { return nullptr; }
-
-template <typename Type, typename ... Args, typename Data>
+template <typename Type, typename ... Args>
 void RegisterUFunc(
     PyUFuncObject* py_ufunc,
     PyUFuncGenericFunction func,
-    Data data) {
+    void* data) {
   constexpr int N = sizeof...(Args);
   int dtype = npy_format_descriptor<Type>::value;
   int dtype_args[] = {npy_format_descriptor<Args>::value...};
@@ -33,34 +31,38 @@ void RegisterUFunc(
     throw py::cast_error("bad stuff");
   }
   int result = PyUFunc_RegisterLoopForType(
-      py_ufunc, dtype, func, dtype_args, heapit(data));
+      py_ufunc, dtype, func, dtype_args, data);
   if (result < 0) throw py::cast_error("badder stuff");
 }
 
 template <typename Return, typename ... Args>
 using Func = Return (*)(Args...);
 
-template <
-    typename Arg0, typename Arg1, typename Out,
-    Func<Out, const Arg0&, const Arg1&> func>
-struct BinaryUFunc {
-  static void ufunc(
-      char** args, npy_intp* dimensions, npy_intp* steps, void* data) {
-    auto& func_in = *(Func<Out, const Arg0&, const Arg1&>*)data;
-    int step_0 = steps[0];
-    int step_1 = steps[1];
-    int step_out = steps[2];
-    int n = *dimensions;
-    char *in_0 = args[0], *in_1 = args[1], *out = args[2];
-    for (int k = 0; k < n; k++) {
-        *(Out*)out = func_in(*(Arg0*)in_0, *(Arg1*)in_1);
-        in_0 += step_0;
-        in_1 += step_1;
-        out += step_out;
-    }
-  }
-  template <typename Type = Arg0>
-  static void Register(PyUFuncObject* py_ufunc) {
-    RegisterUFunc<Type, Arg0, Arg1, Out>(py_ufunc, ufunc, func);
-  }
+template <typename T>
+void* heapit(const T& x) { return new T(x); }
+void* heapit(std::nullptr_t) { return nullptr; }
+
+template <typename Type, typename Func>
+void RegisterBinaryUFunc(PyUFuncObject* py_ufunc, Func func) {
+    auto info = detail::infer_function_info(func);
+    using Info = decltype(info);
+    using Arg0 = std::decay_t<typename Info::Args::template type_at<0>>;
+    using Arg1 = std::decay_t<typename Info::Args::template type_at<1>>;
+    using Out = std::decay_t<typename Info::Return>;
+    auto ufunc = [](char** args, npy_intp* dimensions, npy_intp* steps, void* data) {
+        auto& func = *(Func*)data;
+        int step_0 = steps[0];
+        int step_1 = steps[1];
+        int step_out = steps[2];
+        int n = *dimensions;
+        char *in_0 = args[0], *in_1 = args[1], *out = args[2];
+        for (int k = 0; k < n; k++) {
+            // TODO(eric.cousineau): Support pointers being changed.
+            *(Out*)out = func(*(Arg0*)in_0, *(Arg1*)in_1);
+            in_0 += step_0;
+            in_1 += step_1;
+            out += step_out;
+        }
+    };
+    RegisterUFunc<Type, Arg0, Arg1, Out>(py_ufunc, ufunc, new Func(func));
 };
