36de119f0e0ac44900f1d1be85fb0ff18096d9f6
Eric Cousineau <eric.cousineau@tri.global>
2018-03-09 23:11:55 -0500

    Blech. Inference sucks now.

--- cpp/func_ptr_c.cc
+++ cpp/func_ptr_c.cc
@@ -1,57 +1,62 @@
 #include <iostream>
 using namespace std;
 
-using Func = void(*)(int);
+using Func = void(*)();
 
-// Func make_func() {
-//   Func out = [](int x) {
-//     cout << "Valid func " << x << endl;
-//   };
-//   cout << "Return" << endl;
-//   return out;
-// }
+template <Func a>
+Func wrap() {
+  return []() { a(); };
+}
+// Cannot be in function scope.
+struct tmp { static void a2() { cout << "A2\n"; } };
 
-template <int* x>
-void shibble() {
-  *x += 1;
+template <typename Arg, void (*func)(Arg)>
+void call(Arg arg) {
+  cout << "Wrap\n";
+  func(arg);
 }
 
-template <const Func& f>
-void call() {
-  f(10);
+template <typename ArgT>
+struct signature {
+  using Arg = ArgT;
+};
+
+template <typename Arg>
+signature<Arg> infer(void (*)(Arg));
+
+template <typename F, F f, typename T>
+void infer(T x) {
+  using Sig = decltype(infer(f));
+  call<typename Sig::Arg, f>(x);
 }
 
-// void yar(int x) {}
-
-
+void stuff(int x) {
+  cout << "stuff(" << x << ")\n";
+}
 
 int main() {
-  // Func f = make_func();
-  // Func f2 = [](int x) { cout << "Yar " << x << endl; };
-  // call<&yar>();
+  {
+    Func b = wrap<tmp::a2>();
+    b();
+  }
 
-  // f(10);
+  {
+    static const Func a = []() { cout << "A3\n"; };
+    Func b = []() { a(); };
+    b();
+  }
 
-  static int x = 1;
-  shibble<&x>();
-  cout << x << endl;
+  {
+    call<int, stuff>(10);
+    infer<decltype(stuff), stuff>(10);
+  }
 
-  struct Biscuit {
-    static void shit(int) {
-      cout << "Bullshit\n";
-    }
-  };
-  static const Func a = [] (int y) {
-    cout << "Less bullshit\n";
-  };
-  Func b = [](int x) {
-    cout << "Moar bullshit\n";
-    Biscuit::shit(x);
-    a(x);
-    // call<a>();
-  };
-
-  b(1);
+  // {
+  //   auto a = []() { cout << "A3\n"; };
+  //   using A = decltype(a);
+  //   Func b = []() { A a{}; a(); };
+  //   b();
+  // }
 
   return 0;
 }
