07f55e796d230286eb03ad57d0abf9fbfe68e4db
Eric Cousineau <eric.cousineau@tri.global>
2018-03-10 15:47:43 -0500

    Restore

--- python/pybind11/dtype_stuff/test_basic.cc
+++ python/pybind11/dtype_stuff/test_basic.cc
@@ -174,144 +174,6 @@ struct npy_format_descriptor<Custom> {
 
 } }  // namespace detail } namespace pybind11
 
-
-
-// static PyNumberMethods custom_as_number = {
-//     0,          /* nb_add */
-//     0,     /* nb_subtract */
-//     0,     /* nb_multiply */
-// #if PY_MAJOR_VERSION < 3
-//     0,       /* nb_divide */
-// #endif
-//     0,    /* nb_remainder */
-//     0,                       /* nb_divmod */
-//     0,                       /* nb_power */
-//     0,     /* nb_negative */
-//     0,     /* nb_positive */
-//     0,     /* nb_absolute */
-//     0,      /* nb_nonzero */
-//     0,                       /* nb_invert */
-//     0,                       /* nb_lshift */
-//     0,                       /* nb_rshift */
-//     0,                       /* nb_and */
-//     0,                       /* nb_xor */
-//     0,                       /* nb_or */
-// #if PY_MAJOR_VERSION < 3
-//     0,                       /* nb_coerce */
-// #endif
-//     0,          /* nb_int */
-// #if PY_MAJOR_VERSION < 3
-//     0,          /* nb_long */
-// #else
-//     0,                       /* reserved */
-// #endif
-//     0,        /* nb_float */
-// #if PY_MAJOR_VERSION < 3
-//     0,                       /* nb_oct */
-//     0,                       /* nb_hex */
-// #endif
-
-//     0,                       /* nb_inplace_add */
-//     0,                       /* nb_inplace_subtract */
-//     0,                       /* nb_inplace_multiply */
-// #if PY_MAJOR_VERSION < 3
-//     0,                       /* nb_inplace_divide */
-// #endif
-//     0,                       /* nb_inplace_remainder */
-//     0,                       /* nb_inplace_power */
-//     0,                       /* nb_inplace_lshift */
-//     0,                       /* nb_inplace_rshift */
-//     0,                       /* nb_inplace_and */
-//     0,                       /* nb_inplace_xor */
-//     0,                       /* nb_inplace_or */
-
-//     0, /* nb_floor_divide */
-//     0,       /* nb_true_divide */
-//     0,                       /* nb_inplace_floor_divide */
-//     0,                       /* nb_inplace_true_divide */
-//     0,                       /* nb_index */
-// };
-
-typedef struct {
-    PyObject_HEAD
-    Custom r;
-} PyCustom;
-
-extern PyTypeObject PyCustom_Type;
-
-static PyObject*
-PyCustom_FromCustom(Custom x) {
-    PyCustom* p = (PyCustom*)PyCustom_Type.tp_alloc(&PyCustom_Type,0);
-    if (p) {
-        p->r = x;
-    }
-    return (PyObject*)p;
-}
-
-static PyObject*
-custom_new(PyTypeObject* type, PyObject* args, PyObject* kwds) {
-    return PyCustom_FromCustom(Custom{});
-}
-
-PyTypeObject PyCustom_Type = {
-#if defined(NPY_PY3K)
-    PyVarObject_HEAD_INIT(NULL, 0)
-#else
-    PyObject_HEAD_INIT(NULL)
-    0,                                        /* ob_size */
-#endif
-    "Custom",                               /* tp_name */
-    sizeof(PyCustom),                       /* tp_basicsize */
-    0,                                        /* tp_itemsize */
-    0,                                        /* tp_dealloc */
-    0,                                        /* tp_print */
-    0,                                        /* tp_getattr */
-    0,                                        /* tp_setattr */
-#if defined(NPY_PY3K)
-    0,                                        /* tp_reserved */
-#else
-    0,                                        /* tp_compare */
-#endif
-    0,                          /* tp_repr */
-    0,                    /* tp_as_number */
-    0,                                        /* tp_as_sequence */
-    0,                                        /* tp_as_mapping */
-    0,                          /* tp_hash */
-    0,                                        /* tp_call */
-    0,                           /* tp_str */
-    0,                                        /* tp_getattro */
-    0,                                        /* tp_setattro */
-    0,                                        /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
-    "Minimal wrap for numpy",       /* tp_doc */
-    0,                                        /* tp_traverse */
-    0,                                        /* tp_clear */
-    0,                   /* tp_richcompare */
-    0,                                        /* tp_weaklistoffset */
-    0,                                        /* tp_iter */
-    0,                                        /* tp_iternext */
-    0,                                        /* tp_methods */
-    0,                                        /* tp_members */
-    0,                        /* tp_getset */
-    0, /* tp_base - To be set when numpy is imported */
-    0,                                        /* tp_dict */
-    0,                                        /* tp_descr_get */
-    0,                                        /* tp_descr_set */
-    0,                                        /* tp_dictoffset */
-    0,                                        /* tp_init */
-    0,                                        /* tp_alloc */
-    custom_new,                           /* tp_new */
-    0,                                        /* tp_free */
-    0,                                        /* tp_is_gc */
-    0,                                        /* tp_bases */
-    0,                                        /* tp_mro */
-    0,                                        /* tp_cache */
-    0,                                        /* tp_subclasses */
-    0,                                        /* tp_weaklist */
-    0,                                        /* tp_del */
-    0,                                        /* tp_version_tag */
-};
-
 int main() {
     py::scoped_interpreter guard;
 
@@ -322,22 +184,18 @@ int main() {
     py::module m("__main__");
 
     using Class = Custom;
-    // py::class_<Class> cls(m, "Custom");
-    // cls
-    //     .def(py::init<double>())
-    //     .def(py::self == Class{})
-    //     .def(py::self * Class{})
-    //     .def("value", &Class::value)
-    //     .def("__repr__", [](const Class* self) {
-    //         return py::str("Custom({})").format(self->value());
-    //     });
+    py::class_<Class> cls(m, "Custom");
+    cls
+        .def(py::init<double>())
+        .def(py::self == Class{})
+        .def(py::self * Class{})
+        .def("value", &Class::value)
+        .def("__repr__", [](const Class* self) {
+            return py::str("Custom({})").format(self->value());
+        });
 
     // Register thing.
-    auto raw_type = &PyCustom_Type;
-    auto py_type = py::handle((PyObject*)raw_type);
-    PyCustom_Type.tp_base = &PyGenericArrType_Type;
-    PY_ASSERT_EX(PyType_Ready(&PyCustom_Type) >= 0, "Crap");
-    m.attr("Custom") = py_type;
+    auto py_type = (PyTypeObject*)cls.ptr();
 
     typedef struct { char c; Class r; } align_test;
 
@@ -345,7 +203,7 @@ int main() {
     
     static PyArray_Descr descr = {
         PyObject_HEAD_INIT(0)
-        raw_type,                /* typeobj */
+        py_type,                /* typeobj */
         'V',                    /* kind (V = arbitrary) */
         'r',                    /* type */
         '=',                    /* byteorder */
@@ -411,9 +269,8 @@ int main() {
     };
     Py_TYPE(&descr) = &PyArrayDescr_Type;
     npy_custom = PyArray_RegisterDataType(&descr);
-    auto py_descr =
-        py::reinterpret_borrow<py::object>(py::handle((PyObject*)&descr));
-    // py::setattr(py_type, "dtype", py_descr);
+    cls.attr("dtype") = py::reinterpret_borrow<py::object>(
+        py::handle((PyObject*)&descr));
 
     using Unary = type_pack<Class>;
     using Binary = type_pack<Class, Class>;
