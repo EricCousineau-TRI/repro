89423bf39d496a0a12bfcccfdd77d03ae858369b
Eric Cousineau <eric.cousineau@tri.global>
2018-03-13 13:11:45 -0400

    Can kind-of get things to work. Very annoying.

--- bug/clion_python_debug_shared/from_cc.cc
+++ bug/clion_python_debug_shared/from_cc.cc
@@ -1,6 +1,14 @@
 #include <cstdlib>
+#include <unistd.h>
+#include <stdio.h>
 
 int main() {
+//    char* const args[] = {"bash", "-c", "test -d bazel-bin && cd ./bazel-bin/noddy_test.runfiles/clion_python_debug_shared && pwd", nullptr};
+//    execvp(args[0], args);
+    if (chdir("./bazel-bin/noddy_test.runfiles/clion_python_debug_shared") != 0) {
+        printf("Already in dir\n");
+    }
     system("which python");
+//    system("env");
     return system("python ./noddy_test");
 }
