58f8fd78fe000cbbc32e8df8de5537d7815f6bec
Eric Cousineau <eric.cousineau@tri.global>
2018-03-13 22:22:33 -0400

    All works now as expected

--- python/pybind11/dtype_stuff/test_basic.py
+++ python/pybind11/dtype_stuff/test_basic.py
@@ -12,15 +12,6 @@ def check_meta():
     print(Custom)
     a = Custom(1)
     print(str(a), repr(a))
-    av = np.array([a])  # Implicit cast.
-    print("---")
-    a2v_bad = np.array([a, 10])  # Cannot mix easily.
-    print(repr(a2v_bad))
-    a2v = np.array([a, 10], dtype=Custom)  # Cannot mix easily.
-    print(repr(a2v))
-    ov = av.astype(object)
-    print(av == av)
-    print(ov == ov)
     print(Custom)
     print(np.dtype(Custom))
     print(Custom.dtype)
@@ -31,6 +22,18 @@ def check_meta():
 def check_op():
     a = Custom(1)
     print(a == a)
+
+    av = np.array([a])  # Implicit cast.
+
+    print("---")
+    a2v_bad = np.array([a, 10])  # Cannot mix easily.
+    print(repr(a2v_bad))
+    a2v = np.array([a, 10], dtype=Custom)  # Cannot mix easily.
+    print(repr(a2v))
+    ov = av.astype(object)
+    print(av == av)  # non-logical
+    print(ov == ov)  # logical
+
     av = np.array([[Custom(1), Custom(2)], [Custom(3), Custom(4)]], dtype=Custom)
     print(av)
     print(av == av)
@@ -45,7 +48,7 @@ def check_op():
     print(repr(ov))
     print(repr(ov.astype(Custom)))
 
-    a0 = np.zeros(2, 2, Custom)
+    a0 = np.zeros((2, 2), Custom)
     print(a0)
 
 def check_rational():
@@ -58,5 +61,5 @@ def check_rational():
 
 # check_rational()
 # check_bad()
-check_zero()
-# # check_op()
+# check_zero()
+check_op()
