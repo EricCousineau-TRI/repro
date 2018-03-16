979368f5e91dbcfb987d26d5f35bf0a2f89639e6
Eric Cousineau <eric.cousineau@tri.global>
2018-03-16 01:29:39 -0400

    Can do apply stuff

--- cpp/type_pack_stuff.cc
+++ cpp/type_pack_stuff.cc
@@ -6,13 +6,27 @@ using namespace std;
 #include "cpp/type_pack.h"
 #include "cpp/name_trait.h"
 
+template <typename T>
+using identity_t = T;
+template <typename T>
+using ptr_t = T*;
+
 template <typename... A, typename... B>
 auto type_pack_concat(type_pack<A...> = {}, type_pack<B...> = {}) {
   return type_pack<A..., B...>{};
 }
 
+template <template <typename> class Apply, typename... T>
+auto type_pack_apply(type_pack<T...> = {}) {
+  return type_pack<Apply<T>...>{};
+}
+
 int main() {
-  auto x = type_pack_concat(type_pack<int, void>{}, type_pack<double, void*>{});
+  using A = type_pack<int, void>;
+  using B = type_pack<double, void*>;
+  auto x = type_pack_concat(A{}, B{});
   cout << nice_type_name<decltype(x)>() << endl;
+  auto y = type_pack_apply<ptr_t>(x);
+  cout << nice_type_name<decltype(y)>() << endl;
   return 0;
 }
