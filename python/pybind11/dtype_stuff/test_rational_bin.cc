233a39355c0b3d6eba5f87aaa97e97ad325c3200
Eric Cousineau <eric.cousineau@tri.global>
2018-03-19 15:06:54 -0400

    Not sure why debugging doesn't work out of the box in CLion...

--- python/pybind11/dtype_stuff/test_rational_bin.cc
+++ python/pybind11/dtype_stuff/test_rational_bin.cc
@@ -9,12 +9,17 @@ int main(int argc, char** argv) {
   for (int i = 0; i < argc; ++i) py_argv.append(argv[i]);
   py::module::import("sys").attr("argv") = py_argv;
 
+  // TODO(eric.cousineau): CLion does NOT pick up on dynamically loaded
+  // modules, but it works with GDBserver. Why???
+  // Cannot put breakpoints in `test_rational*.cc`... but can do so for Pybind stuff.
   py::globals()["__file__"] = "python/pybind11/dtype_stuff/test_rational_bin.py";
   py::exec(R"""(
 import os, sys
 os.chdir(sys.argv[0] + ".runfiles/repro")
 
 sys.path.insert(0, os.path.dirname(__file__))
+import test_rational
+print(test_rational)
 execfile(__file__)
 )""");
 
