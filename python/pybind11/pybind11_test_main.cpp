53f2b78e7aecb059ca88dda27ad95964f087f6d5
Eric Cousineau <eric.cousineau@tri.global>
2018-03-19 13:26:24 -0400

    Can now trace pytest things

--- python/pybind11/pybind11_test_main.cpp
+++ python/pybind11/pybind11_test_main.cpp
@@ -10,14 +10,17 @@ int main(int argc, char** argv) {
     py_argv.append(argv[i]);
   py::module::import("sys").attr("argv") = py_argv;
 
+  py::globals()["__file__"] = py_argv[0];
+
   py::exec(R"""(
 import os
 import pytest
 import sys
+import trace
 
 os.chdir("python/pybind11/tests")
-print(os.getcwd())
-pytest.main(args=sys.argv[1:])
+tracer = trace.Trace(trace=1, count=0, ignoredirs=sys.path)
+tracer.run('pytest.main(args=sys.argv[1:])')
 )""");
 
   return 0;
