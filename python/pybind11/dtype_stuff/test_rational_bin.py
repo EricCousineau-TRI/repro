344212eda91fe71a9f466650ff47ec2b4bef98e0
Eric Cousineau <eric.cousineau@tri.global>
2018-03-22 18:10:54 -0400

    Cleaning up

--- python/pybind11/dtype_stuff/test_rational_bin.py
+++ python/pybind11/dtype_stuff/test_rational_bin.py
@@ -1,15 +1,20 @@
-import subprocess
-subprocess.Popen(
-    "export -p | sed 's# PWD=# OLD_PWD=#g' | tee /tmp/env.sh",
-    shell=True)
+# import subprocess
+# subprocess.Popen(
+#     "export -p | sed 's# PWD=# OLD_PWD=#g' | tee /tmp/env.sh",
+#     shell=True)
 
 import numpy as np
 # from test_rational_min import rational
 import test_rational_min
-from test_rational import rational
-# from numpy.core.test_rational import rational
+# from test_rational import rational
+from numpy.core.test_rational import rational
 
-print(np.ones((2, 2), dtype=rational))
+# print(np.ones((2, 2), dtype=rational))
+x = np.array([0.])
+x[0] = rational(1)
+print(x)
+print(x + rational(1))
+print(rational(1) + x)
 # print(rational)
 # print(rational(1))
 
