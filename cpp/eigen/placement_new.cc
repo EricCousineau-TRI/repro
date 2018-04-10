f98a145e26cb1ba6f06aaf05a39bd8a806321a17
Eric Cousineau <eric.cousineau@tri.global>
2018-04-10 15:57:25 -0400

    Alignment don't matter

--- cpp/eigen/placement_new.cc
+++ cpp/eigen/placement_new.cc
@@ -14,15 +14,13 @@ using storage_for = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
 
 int main() {
   constexpr int n = sizeof(AutoDiffXd);
-  storage_for<AutoDiffXd> buffer;
-  AutoDiffXd* value = reinterpret_cast<AutoDiffXd*>(&buffer);
-  memset(&buffer, 0, n);
-  // new (value) VectorXd(0);
+  char buffer[n];
+  AutoDiffXd* value = reinterpret_cast<AutoDiffXd*>(buffer);
+  memset(buffer, 0, n);
   *value = AutoDiffXd(10, VectorXd::Constant(3, 10));
   cout
       << "value: " << *value << " - "
       << value->derivatives().transpose() << endl;
-  // delete value;
   value->~AutoDiffXd();
   return 0;
 }
