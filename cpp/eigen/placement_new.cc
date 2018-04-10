5fa50e81c89bb768be3f69399bf05db461032220
Eric Cousineau <eric.cousineau@tri.global>
2018-04-10 15:56:58 -0400

    AutoDiffXd also seems to work just fine

--- cpp/eigen/placement_new.cc
+++ cpp/eigen/placement_new.cc
@@ -1,23 +1,28 @@
 #include <iostream>
 
 #include <Eigen/Dense>
+#include <unsupported/Eigen/AutoDiff>
 
 using std::cout;
 using std::endl;
 using Eigen::VectorXd;
 
+using AutoDiffXd = Eigen::AutoDiffScalar<Eigen::VectorXd>;
+
 template <typename T>
 using storage_for = typename std::aligned_storage<sizeof(T), alignof(T)>::type;
 
 int main() {
-  constexpr int n = sizeof(VectorXd);
-  storage_for<VectorXd> buffer;
-  VectorXd* value = reinterpret_cast<VectorXd*>(&buffer);
+  constexpr int n = sizeof(AutoDiffXd);
+  storage_for<AutoDiffXd> buffer;
+  AutoDiffXd* value = reinterpret_cast<AutoDiffXd*>(&buffer);
   memset(&buffer, 0, n);
   // new (value) VectorXd(0);
-  *value = VectorXd::Constant(3, 10);
-  cout << "value: " << value->transpose() << endl;
+  *value = AutoDiffXd(10, VectorXd::Constant(3, 10));
+  cout
+      << "value: " << *value << " - "
+      << value->derivatives().transpose() << endl;
   // delete value;
-  value->~VectorXd();
+  value->~AutoDiffXd();
   return 0;
 }
