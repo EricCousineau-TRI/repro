9d899f94d2f96b0eef53c7cb6ecf12278efa509b
Eric Cousineau <eric.cousineau@tri.global>
2018-04-11 01:31:02 -0400

    Odd ordering in logs

--- python/numpy/patching/patch_all.sh
+++ python/numpy/patching/patch_all.sh
@@ -8,6 +8,7 @@ versions="1.11.0 1.12.0 1.13.0 1.14.0 1.14.1 master"
 for version in $versions; do
     output="tmp/patch_${version}-output.txt"
     echo "Version ${version}: ${output}"
+    # TODO(eric.cousineau): Dup some pipes? Seems like some of them collided or got mis-ordered?
     {
         echo "Starting ${output}"
         python patchify.py --version=${version} > ${output} 2>&1 || echo "FAILED ${version}"
