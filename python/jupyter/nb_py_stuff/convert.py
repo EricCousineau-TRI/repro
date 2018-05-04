e17b0914c3dd013bd664a9f34b8686e2c137afc1
Eric Cousineau <eric.cousineau@tri.global>
2018-05-04 12:05:07 -0400

    Example of converting from json v4 to py v3

--- python/jupyter/nb_py_stuff/convert.py
+++ python/jupyter/nb_py_stuff/convert.py
@@ -1,1 +1,19 @@
-from nbformat.v3 import nbpy
+import os
+
+from nbformat.v4 import nbjson as nbjson4
+from nbformat.v3 import nbpy as nbpy3
+from nbformat.converter import convert
+
+# Load v4 notebook.
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+src = "notebooks/test.ipynb"
+dst = "notebooks/test.v3.py"
+
+with open(src) as f:
+    src_nb4 = nbjson4.read(f)
+
+src_nb3 = convert(src_nb4, to_version=3)
+
+with open(dst, 'w') as f:
+    dst_nb = nbpy3.write(src_nb3, f)
---
+++ python/jupyter/nb_py_stuff/notebooks/test.v3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+print("I want nbformat 3")
+
+# <codecell>
+
+
