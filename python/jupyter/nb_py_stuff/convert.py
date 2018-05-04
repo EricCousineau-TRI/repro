b337b1ae69affabf35e6d0b9b0d846b8a6ad2596
Eric Cousineau <eric.cousineau@tri.global>
2018-05-04 12:47:32 -0400

    Add adjustment bugfix

--- python/jupyter/nb_py_stuff/convert.py
+++ python/jupyter/nb_py_stuff/convert.py
@@ -13,6 +13,14 @@ conversions = {
 }
 choices = conversions.keys()
 
+# https://stackoverflow.com/a/35720002/7829525
+def pad_markdown(text):
+    return text + "\n# <markdowncell>\n\n# Bugfix\n"
+
+adjust_from_map = {
+    ("py_v3", "json"): pad_markdown,
+}
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--to", type=str, choices=choices, default="json")
 parser.add_argument("--from", dest="from_", type=str, choices=choices, default="py_v3")
@@ -23,8 +31,12 @@ args = parser.parse_args()
 
 (v_from, m_from) = conversions[args.from_]
 (v_out, m_to) = conversions[args.to]
+adjust_from = adjust_from_map.get((args.from_, args.to))
 
-nb_from = m_from.read(args.input)
+text_from = args.input.read()
+if adjust_from is not None:
+    text_from = adjust_from(text_from)
+nb_from = m_from.reads(text_from)
 if v_from != v_out:
     nb_from = convert(nb_from, to_version=v_out)
 m_to.write(nb_from, args.output)
