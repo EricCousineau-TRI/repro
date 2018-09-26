52b73961dabb7d174a3783262ab3fb3f91dc7ab8
Eric Cousineau <eric.cousineau@tri.global>
2018-09-26 12:53:05 -0400

    Test with large generated file

--- cpp/doc_struct_nest.cc
+++ cpp/doc_struct_nest.cc
@@ -1,19 +1,34 @@
 #include <iostream>
 
-constexpr struct {
-  struct {
-    struct {
-      struct {
-        const char* doc = "Hello world";
-        const char* doc_2 = "Hello world 2";
+#include "doc_struct_nest_big.h"
+
+using str = const char*;
+
+constexpr struct /* doc */ {
+
+  struct /* top */ {
+
+    struct /* mid */ {
+
+      struct /* bottom */ {
+        const char* doc[2] = {
+          "Hello world",
+          "Hello world 2",
+        };
+
       } bottom;
+
     } mid;
+
   } top;
+
 } doc;
 
 int main() {
-  std::cout << doc.top.mid.bottom.doc << std::endl;
+  std::cout << doc.top.mid.bottom.doc[0] << std::endl;
   auto& bottom = doc.top.mid.bottom;
-  std::cout << bottom.doc_2 << std::endl;
+  std::cout << bottom.doc[1] << std::endl;
+  auto& cls = root.drake.multibody.MultibodyForces;
+  std::cout << cls.num_bodies.doc << std::endl;
   return 0;
 }
