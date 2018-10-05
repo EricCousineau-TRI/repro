c80dc2360068a256665275c52518ed2876f5d112
Eric Cousineau <eric.cousineau@tri.global>
2018-10-05 18:50:32 -0400

    Simplify repro

--- bug/clang_unused_lambda_capture/repro.sh
+++ bug/clang_unused_lambda_capture/repro.sh
@@ -1,14 +1,65 @@
 #!/bin/bash
-set -eux -o pipefail
+set -u -o pipefail
 
-compile() {
-    clang++-6.0 -std=c++14 -Wall -Werror "$@"
-    # g++ -std=c++14 -Wall -Werror "$@"
+test() {
+    echo "test.cc"
+    echo
+    cat test.cc | sed 's#^#   #g'
+    echo
+    args="-std=c++14 -Wall -Werror ./test.cc -o ./test"
+    (
+        set -x
+        clang++-6.0 ${args} && ./test
+        g++-5 ${args} && ./test
+    )
+    echo
+    echo
 }
 
-compile ./orig.cc -o ./orig
-./orig
-echo
+echo '[ non-auto storage: fails ]'
+cat > test.cc <<EOF
+#include <iostream>
+const char top_doc[] = "Works";
+int main() {
+  auto& doc = top_doc;
+  [&doc]() { std::cout << doc << std::endl; }();
+  return 0;
+}
+EOF
+test
 
-compile ./test.cc -o ./test
-./test
+echo '[ auto storage: works ]'
+cat > test.cc <<EOF
+#include <iostream>
+int main() {
+  const char top_doc[] = "Works";
+  auto& doc = top_doc;
+  [&doc]() { std::cout << doc << std::endl; }();
+  return 0;
+}
+EOF
+test
+
+echo '[ auto&: fails ]'
+cat > test.cc <<EOF
+#include <iostream>
+constexpr char top_doc[] = "Works";
+int main() {
+  auto& doc = top_doc;
+  [&doc]() { std::cout << doc << std::endl; }();
+  return 0;
+}
+EOF
+test
+
+echo '[ constexpr auto&: works ]'
+cat > test.cc <<EOF
+#include <iostream>
+constexpr char top_doc[] = "Works";
+int main() {
+  constexpr auto& doc = top_doc;
+  []() { std::cout << doc << std::endl; }();
+  return 0;
+}
+EOF
+test
