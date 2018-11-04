909b134d935b7787f75db4fbb6dc054898204562
Eric Cousineau <eric.cousineau@tri.global>
2018-11-04 12:16:25 -0500

    Make simplified godbolt versoin

--- cpp/generator_godbolt.cc
+++ cpp/generator_godbolt.cc
@@ -1,14 +1,8 @@
 #include <cassert>
-#include <iostream>
 #include <functional>
 #include <experimental/optional>
-#include <memory>
-#include <vector>
 
-using std::cerr;
-using std::endl;
 using std::experimental::optional;
-using std::unique_ptr;
 
 /**
 Provides a generator class that can be iterated upon:
@@ -100,7 +94,7 @@ class generator_t {
 
 template <
     typename T, typename result_t = optional<T>,
-    typename Func> // = std::function<result_t()>>
+    typename Func>
 auto generator(Func&& func) {
   return generator_t<T, result_t, Func>(std::forward<Func>(func));
 }
@@ -110,87 +104,18 @@ auto null_generator() {
   return generator<T, result_t>([]() { return result_t{}; });
 }
 
-template <typename T, typename result_t, typename Func>
-class chain_func {
- public:
-  using Generator = generator_t<T, result_t, Func>;
-  chain_func(std::vector<Generator> g_list)
-      : g_list_(std::move(g_list)) {}
-
-  result_t operator()() {
-    if (!next()) return {};
-    return *iter_;
-  }
-
- private:
-  inline Generator& g() { return g_list_[i_]; }
-
-  inline bool next() {
-    if (i_ >= g_list_.size()) return false;
-    if (!iter_.valid()) {
-      iter_ = g().begin();
-    } else {
-      ++iter_;
-    }
-    while (iter_ == g().end()) {
-      if (++i_ >= g_list_.size()) return false;
-      iter_ = g().begin();
-    }
-    return true;
-  }
-
-  std::vector<Generator> g_list_;
-  typename Generator::iterator iter_;
-  int i_{0};
-};
-
-template <
-    typename T, typename result_t = optional<T>,
-    typename Func = std::function<result_t()>>
-auto chain(std::vector<generator_t<T, result_t, Func>> g) {
-  auto tmp = chain_func<T, result_t, Func>(std::move(g));
-  return generator<T, result_t, Func>(std::move(tmp));
-}
-
+int visit_count = 0;
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const unique_ptr<T>& p) {
-  if (p) os << "unique_ptr(" << *p << ")";
-  else os << "unique_ptr(nullptr)";
-  return os;
-}
+void visit(T&&) { ++visit_count; }
 
 template <typename Container>
-void print_container(Container&& gen) {
+void visit_container(Container&& gen) {
   for (int rep : {0, 1}) {
-    cerr << "[ " << rep << " ] ";
     for (auto&& value : gen) {
-      cerr << value << " ";
+      visit(value);
     }
-    cerr << endl;
   }
-  cerr << endl;
 }
-
-class positive_int {
- public:
-  positive_int() {}
-  positive_int(int value) : value_(value) {}
-  // N.B. Seems to be *very* picky about using `move` and `T&&` here.
-  int&& operator*() { return std::move(value_); }
-  operator bool() const { return value_ >= 0; }
- private:
-  int value_{-1};
-};
-
-class move_only_func {
- public:
-  optional<int> operator()() {
-    if (*value_ < 5) return (*value_)++;
-    else return {};
-  }
- private:
-  unique_ptr<int> value_{new int{0}};
-};
 
 int main() {
   auto simple_gen = []() {
@@ -199,32 +124,7 @@ int main() {
       return {};
     });
   };
-  cerr << "simple:" << endl;
-  print_container(simple_gen());
-  cerr << "chain:" << endl;
-  print_container(chain<int>({
-      simple_gen(), null_generator<int>(), simple_gen()}));
-  cerr << "optional<unique_ptr>:" << endl;
-  auto unique = generator<unique_ptr<int>>(
-    [i = 0]() mutable -> optional<unique_ptr<int>> {
-      if (i < 5) return unique_ptr<int>(new int{i++});
-      return {};
-    });
-  print_container(unique);
-  cerr << "positive_int:" << endl;
-  print_container(generator<int, positive_int>(
-    [i = 0]() mutable {
-      if (i < 3) return i++;
-      return -1;
-    }));
-  cerr << " - implicit null sequence:" << endl;
-  print_container(generator<int, positive_int>([]() mutable { return -1; }));
-  cerr << "unique_ptr:" << endl;
-  print_container(generator<int, unique_ptr<int>>([i = 0]() mutable {
-    if (i < 3) return unique_ptr<int>(new int{i++});
-    return unique_ptr<int>();
-  }));
-  cerr << "move_only:" << endl;
-  print_container(generator<int>(move_only_func{}));
+  visit_container(simple_gen());
+  visit_container(null_generator<int>());
   return 0;
 }
