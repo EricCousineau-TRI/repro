9461c75b6baf662aa6794212483ef3be4f8e6fad
Eric Cousineau <eric.cousineau@tri.global>
2018-12-23 10:21:07 -0500

    Add guess at C99

--- c/anon_struct.c
+++ c/anon_struct.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+struct stuff_t {
+    /* input, vector */
+    struct {
+        int size;
+    } p;
+    /* output, matrix */
+    struct {
+        int rows;
+        int cols;
+    } m;
+};
+
 int main(int argc, char** argv) {
     (void)argc; (void)argv;
-    printf("Hello world\n");
+    struct stuff_t s = { {2}, {2, 2} };
+    printf("Hello world: %d\n", s.m.cols);
     return 0;
 }
