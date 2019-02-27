4ebc0017e3a6955aaae52280599f795b5295a844
Eric Cousineau <eric.cousineau@tri.global>
2019-02-27 05:35:46 -0500

    Check extending lifetime with class + raw pointers

--- python/vtk_pybind/vtk_pybind.cpp
+++ python/vtk_pybind/vtk_pybind.cpp
@@ -3,8 +3,22 @@
 
 #include "third_party/vtk_pybind_conversion.h"
 
+namespace py = pybind11;
+
+struct Test {
+  vtkNew<vtkPolyData> poly;
+  vtkPolyData* get_poly() { return poly.GetPointer(); }
+};
+
 PYBIND11_MODULE(vtk_pybind, m) {
-  m.def("make_polydata", []() {
+  // Use a class to control the lifetime of a VTK object.
+  py::class_<Test>(m, "Test")
+    .def(py::init())
+    .def("get_poly", &Test::get_poly);
+
+  // This segfaults because I (Eric) don't know how to transfer ownership from
+  // a smart pointer to Python...
+  m.def("make_poly", []() {
     return vtkSmartPointer<vtkPolyData>::New().GetPointer();
   });
 }
--- python/vtk_pybind/vtk_pybind_test.py
+++ python/vtk_pybind/vtk_pybind_test.py
@@ -10,11 +10,19 @@ def info(x):
     return "<{} at {}>".format(type(x).__name__, id(x))
 
 
+def check_poly(x):
+    print(info(x))
+    assert isinstance(x, vtk.vtkPolyData)
+
+
 def main():
     print("Hello")
-    print(info(vtk.vtkPolyData()))
-    # Segfault...
-    print(m.make_polydata())
+    poly_py = vtk.vtkPolyData()
+    check_poly(poly_py)
+    poly_cc_owned = m.Test().get_poly()
+    check_poly(poly_cc_owned)
+    poly_cc_transfer = m.make_poly()  # Segfault...
+    check_poly(poly_cc_transfer)
 
 
 if __name__ == "__main__":
