8a3a199911a7cf0c35fbd0937de32aac5981d805
Eric Cousineau <eric.cousineau@tri.global>
2019-03-23 18:33:53 -0400

    Just needed globbing

--- ros/ros2_bazel_prototype/pub.cc
+++ ros/ros2_bazel_prototype/pub.cc
@@ -8,28 +8,29 @@ namespace {
 
 class Pub : public rclcpp::Node {
  public:
-  Pub() : Node("pub"), count_(0)
-  {      
+  Pub() : Node("pub") {
     using namespace std::chrono_literals;
     auto publisher = this->create_publisher<std_msgs::msg::String>("topic");
-    auto callback = [publisher, mutable int count = 0]() {
+    auto callback = [this, publisher]() {
       std_msgs::msg::String message;
-      message.data = "Hello, world! " + std::to_string(count);
+      message.data = "Hello, world! " + std::to_string(count_);
+      count_ += 1;
       std::cout << "Pub: " << message.data;
       publisher->publish(message);
     };
-    timer_ = this->create_wall_timer(500ms, timer_callback);
+    timer_ = this->create_wall_timer(500ms, callback);
   }
 
  private:
   rclcpp::TimerBase::SharedPtr timer_;
+  int count_{};
 };
 
 }  // namespace
 
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<Pub>());
   rclcpp::shutdown();
   return 0;
 }
--- ros/ros2_bazel_prototype/tools/skylark/cmake/cmake_cc.BUILD.tpl
+++ ros/ros2_bazel_prototype/tools/skylark/cmake/cmake_cc.BUILD.tpl
@@ -4,6 +4,7 @@ package(default_visibility = ["//visibility:public"])
 
 cc_library(
     name = %{name},
+    hdrs = glob(["{}/**/*".format(x) for x in %{includes}]),
     includes = %{includes},
     linkopts = %{linkopts},
     deps = %{deps},
