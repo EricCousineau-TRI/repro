fdb67a80f7d4fcafa3c1a96adae5c160d8fb9184
Eric Cousineau <eric.cousineau@tri.global>
2019-04-12 19:08:52 -0400

    Fix da issues

--- python/namedlist_alt.py
+++ python/namedlist_alt.py
@@ -1,5 +1,5 @@
 # Alternative impl, motivated by: https://bitbucket.org/ericvsmith/namedlist
-from functools import partial
+from collections import namedtuple
 
 import numpy as np
 
@@ -11,17 +11,28 @@ def _item_property(cls, index):
 
 
 def namedview(name, fields):
+    # Prevent mutation.
+    fields = tuple(fields)
 
     class NamedView(object):
         def __init__(self, obj):
             assert len(obj) == len(fields)
-            self._obj = obj
+            object.__setattr__(self, '_obj', obj)
+
+        @staticmethod
+        def get_fields():
+            return fields
 
         def __getitem__(self, index):
             return self._obj.__getitem__(index)
 
         def __setitem__(self, index, value):
             self._obj.__setitem__(index, value)
+
+        def __setattr__(self, name, value):
+            if not hasattr(self, name):
+                raise AttributeError("Cannot add attributes!")
+            object.__setattr__(self, name, value)
 
         def __len__(self):
             return self._obj.__len__
@@ -30,10 +41,10 @@ def namedview(name, fields):
             return self._obj.__iter__()
 
         def __repr__(self):
-            return self._obj.__repr__()
-
-        def __str__(self):
-            return self._obj.__str__()
+            values = [
+                "{}={}".format(field, repr(self[i]))
+                for i, field in enumerate(fields)]
+            return "{}({})".format(name, ", ".join(values))
 
     NamedView.__name__ = NamedView.__qualname__ = name
     for i, field in enumerate(fields):
@@ -43,21 +54,36 @@ def namedview(name, fields):
 
 def test_main():
     MyView = namedview("MyView", ['a', 'b', 'c'])
+    MyTuple = namedtuple("MyTuple", ['a', 'b', 'c'])
+
     print(MyView)
+    print(MyTuple)
 
     print("[ Simple List ]")
     value = [1, 2, 3]
     view = MyView(value)
+    print(MyTuple._fields)
+    print(MyView.get_fields())
+    print(view.get_fields())
     print(view.a)
     view[0] = 10
     print(view.a)
     view[1] = -100
     view.c = 1000
+
+    tup = MyTuple(a=1, b=2, c=3)
+    print(tup)
+
     print(view)
     print(value)
     view[:] = [111, 222, 333]
     print(view)
     print(value)
+    try:
+        view.bad_value = 1
+        assert False
+    except AttributeError:
+        pass
 
     print("[ Array 1D ]")
     array = np.array([4, 5, 6])
@@ -71,6 +97,7 @@ def test_main():
     print("[ Array 2D ]")
     mat = np.eye(3)
     mview = MyView(mat)
+    print(mview)
     print(mview.a)
     print(mview[0, 0])
     mview.a[0] = 10
@@ -82,18 +109,24 @@ def test_main():
 Example output:
 
 <class '__main__.MyView'>
+<class '__main__.MyTuple'>
 [ Simple List ]
+('a', 'b', 'c')
+('a', 'b', 'c')
+('a', 'b', 'c')
 1
 10
+MyTuple(a=1, b=2, c=3)
+MyView(a=10, b=-100, c=1000)
 [10, -100, 1000]
-[10, -100, 1000]
-[111, 222, 333]
+MyView(a=111, b=222, c=333)
 [111, 222, 333]
 [ Array 1D ]
 4
 [ 4 50 60]
-[ 4 50 60]
+MyView(a=4, b=50, c=60)
 [ Array 2D ]
+MyView(a=array([ 1.,  0.,  0.]), b=array([ 0.,  1.,  0.]), c=array([ 0.,  0.,  1.]))
 [ 1.  0.  0.]
 1.0
 [ 0.  1.  0.]
@@ -101,7 +134,6 @@ Example output:
  [  0.   1.   0.]
  [  0.   0.   1.]]
 """
-
 
 
 if __name__ == "__main__":
