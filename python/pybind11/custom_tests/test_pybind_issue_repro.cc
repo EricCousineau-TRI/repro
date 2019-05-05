96669a0d584f21f956c1ad6e7c0fad029e36a7c1
Eric Cousineau <eacousineau@gmail.com>
2019-05-05 09:30:28 -0400

    Tinker with recursive variant

--- python/pybind11/custom_tests/test_pybind_issue_repro.cc
+++ python/pybind11/custom_tests/test_pybind_issue_repro.cc
@@ -1,47 +1,68 @@
-// https://github.com/pybind/pybind11/issues/1729
+// https://github.com/pybind/pybind11/issues/1765
 
+#include <iostream>
+#include <map>
 #include <string>
+#include <variant>
 #include <vector>
 
-#include <pybind11/embed.h>
-#include <pybind11/eval.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+// #include <pybind11/embed.h>
+// #include <pybind11/eval.h>
+// #include <pybind11/pybind11.h>
+// #include <pybind11/stl.h>
 
-namespace py = pybind11;
-using namespace py::literals;
+// namespace py = pybind11;
 
-class Test {
-    public:
-        using Ptr = std::shared_ptr<Test>;
-        using Data = std::vector<std::string>;
+struct node_t;
 
-        Test(Data data_)
-            : data{std::move(data_)}{}
+typedef std::variant<int, float, std::string, node_t, std::vector<node_t>> value_t;
 
-        const Data data;
-};
-
-void init_module(py::module m) {
-  py::class_<Test, Test::Ptr>(m, "Test")
-      .def(py::init<Test::Data>(), "data"_a)
-      .def_readonly("data", &Test::data);
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& xs) {
+  os << "[";
+  for (auto& x : xs)
+    os << x << ", ";
+  os << "]";
+  return os;
 }
 
+// Needs one non-pack parameter?
+template <typename Arg0, typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::variant<Arg0, Args...>& x) {
+  std::visit([&os](auto &&arg) { os << arg; }, x);
+  return os;
+}
+
+template <typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::map<Args...>& xs) {
+  os << "{";
+  for (auto& pair : xs)
+    os << pair.first << ": " << pair.second << ", ";
+  os << "}";
+  return os;
+}
+
+struct node_t : public std::map<std::string, value_t> {};
+
+// void init_module(py::module m) {
+// }
+
 int main(int, char**) {
-  py::scoped_interpreter guard{};
+  node_t x = {{{"x", 1}}};
+  std::cout << x << std::endl;
 
-  py::module m("test_module");
-  init_module(m);
-  py::globals()["m"] = m;
+//   py::scoped_interpreter guard{};
 
-  py::print("[ Eval ]");
-  py::exec(R"""(
-print(m.Test([]).data)
-print(m.Test(["a", "b", "c"]).data)
-)""");
+//   py::module m("test_module");
+//   init_module(m);
+//   py::globals()["m"] = m;
 
-  py::print("[ Done ]");
+//   py::print("[ Eval ]");
+//   py::exec(R"""(
+// print("Something")
+// )""");
+
+//   py::print("[ Done ]");
 
   return 0;
 }
