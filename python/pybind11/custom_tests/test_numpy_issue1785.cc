2c3066808334d01ae1e0e8e822b1edc5f898a43c
Eric Cousineau <eacousineau@gmail.com>
2019-05-11 12:19:35 -0400

    Struggling with scalar dtypes + array_t...

--- python/pybind11/custom_tests/test_numpy_issue1785.cc
+++ python/pybind11/custom_tests/test_numpy_issue1785.cc
@@ -16,13 +16,44 @@ static_assert(sizeof(float128) == 16, "Bad size");
 template <typename T, int Dim>
 using Vector = Eigen::Matrix<T, Dim, 1>;
 
+namespace pybind11 { namespace detail {
+
+template <>
+struct type_caster<float128> {
+  PYBIND11_TYPE_CASTER(float128, _("float128"));
+  using my_array = array_t<float128>;
+
+  bool load(handle src, bool convert) {
+    if (!convert && !isinstance<my_array>(src))
+      return false;
+    my_array tmp = my_array::ensure(src);
+    if (tmp) {
+      this->value = *tmp.data();
+    }
+    return false;
+  }
+
+  static handle cast(float128 src, return_value_policy, handle) {
+    // // This causes a segfault:
+    // using Wrapped = Vector<float128, 1>;
+    // using wrap_caster = type_caster<Wrapped>;
+    // return wrap_caster::cast(
+    //     Wrapped(src), return_value_policy::move, handle()))
+    //     .attr("reshape")(py::make_tuple());
+
+    // This does not work (circular 'base')
+    return array_t<float128>({1}, {1}, &src, handle());
+  }
+};
+
+}}  // namespace pybind11::detail
+
 void init_module(py::module m) {
   m.def("make_array", []() {
     return Vector<float128, 3>(1, 2, 3);
   });
   m.def("sum_array", [](Vector<float128, -1> x) {
-    // Return as 1x1 - I don't know how to easily return NumPy scalars...
-    return Vector<float128, 1>(x.array().sum());
+    return x.array().sum();
   });
 }
 
