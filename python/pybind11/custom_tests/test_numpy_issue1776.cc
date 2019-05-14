43407e3d5bd1a13e2ac7d8ec7072817e83ac8cca
Eric Cousineau <eacousineau@gmail.com>
2019-05-14 09:02:13 -0400

    Enable float16 numpy stuff

--- python/pybind11/custom_tests/test_numpy_issue1776.cc
+++ python/pybind11/custom_tests/test_numpy_issue1776.cc
@@ -5,6 +5,7 @@
 #include <pybind11/pybind11.h>
 
 #include "half.h"
+#include "pybind11_numpy_scalar.h"
 
 namespace py = pybind11;
 
@@ -13,14 +14,35 @@ static_assert(sizeof(float16) == 2, "Bad size");
 
 namespace pybind11 { namespace detail {
 
+// Similar to enums in `pybind11/numpy.h`. Determined by doing:
+// python3 -c 'import numpy as np; print(np.dtype(np.float16).num)'
+constexpr int NPY_FLOAT16 = 23;
+
+// Kinda following: https://github.com/pybind/pybind11/blob/9bb3313162c0b856125e481ceece9d8faa567716/include/pybind11/numpy.h#L1000
+template <>
+struct npy_format_descriptor<float16> {
+  static constexpr auto name = _("float16");
+  static pybind11::dtype dtype() {
+    handle ptr = npy_api::get().PyArray_DescrFromType_(NPY_FLOAT16);
+    return reinterpret_borrow<pybind11::dtype>(ptr);
+  }
+};
+
+template <>
+struct type_caster<float16> : npy_scalar_caster<float16> {
+  static constexpr auto name = _("float16");
+};
+
 }}  // namespace pybind11::detail
 
 void init_module(py::module m) {
   m.def("make_array", []() {
-    py::array_t<float16> x({1});
+    py::array_t<float16> x({2});
     x.mutable_at(0) = float16{1.};
+    x.mutable_at(1) = float16{10.};
     return x;
   });
+  m.def("make_scalar", []() { return float16{2.}; });
 }
 
 int main(int, char**) {
@@ -31,6 +53,8 @@ int main(int, char**) {
   py::globals()["m"] = m;
 
   py::print("[ Eval ]");
+  // See `test_numpy_issue1785` for more expansive tests for
+  // `npy_scalar_caster`.
   py::exec(R"""(
 import numpy as np
 
@@ -39,6 +63,7 @@ def info(x):
 
 def main():
     info(m.make_array())
+    info(m.make_scalar())
 
 main()
 )""");
@@ -49,5 +74,8 @@ main()
 }
 
 /* Output:
+
+array([  1.,  10.], dtype=float16) <class 'numpy.ndarray'>
+2.0 <class 'numpy.float16'>
 
 */
