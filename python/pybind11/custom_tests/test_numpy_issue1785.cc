b8f940a48b85934fa0767a3c28727cae6d03ae41
Eric Cousineau <eacousineau@gmail.com>
2019-05-14 09:01:42 -0400

    Split numpy scalar into header

--- python/pybind11/custom_tests/test_numpy_issue1785.cc
+++ python/pybind11/custom_tests/test_numpy_issue1785.cc
@@ -8,6 +8,8 @@
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 
+#include "pybind11_numpy_scalar.h"
+
 namespace py = pybind11;
 
 using float128 = long double;
@@ -17,34 +19,6 @@ template <typename T, int Dim>
 using Vector = Eigen::Matrix<T, Dim, 1>;
 
 namespace pybind11 { namespace detail {
-
-template <typename T>
-struct npy_scalar_caster {
-  PYBIND11_TYPE_CASTER(T, _("PleaseOverride"));
-  using Array = array_t<T>;
-
-  bool load(handle src, bool convert) {
-    // Taken from Eigen casters. Permits either scalar dtype or scalar array.
-    handle type = dtype::of<T>().attr("type");  // Could make more efficient.
-    if (!convert && !isinstance<Array>(src) && !isinstance(src, type))
-      return false;
-    Array tmp = Array::ensure(src);
-    if (tmp && tmp.size() == 1 && tmp.ndim() == 0) {
-      this->value = *tmp.data();
-      return true;
-    }
-    return false;
-  }
-
-  static handle cast(T src, return_value_policy, handle) {
-    Array tmp({1});
-    tmp.mutable_at(0) = src;
-    tmp.resize({});
-    // You could also just return the array if you want a scalar array.
-    object scalar = tmp[py::tuple()];
-    return scalar.release();
-  }
-};
 
 template <>
 struct type_caster<float128> : npy_scalar_caster<float128> {
