ee4b03a98a448d947de469366c511ec49bbf77d6
Eric Cousineau <eacousineau@gmail.com>
2019-11-24 15:58:39 -0500

    trampoline does not work

--- python/pybind11/custom_tests/test_tmp.cc
+++ python/pybind11/custom_tests/test_tmp.cc
@@ -1,3 +1,5 @@
+// For: https://github.com/pybind/pybind11/issues/1640
+
 #include <Eigen/Dense>
 
 #include <pybind11/eigen.h>
@@ -6,35 +8,21 @@
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
-
-struct Blerg {
-  Eigen::Matrix4d matrix{Eigen::Matrix4d::Identity()};
-};
-
 using rvp = py::return_value_policy;
 
-// For reference, see:
-// https://github.com/pybind/pybind11/blob/bd24155/include/pybind11/pybind11.h#L1192-L1199
-template <typename PyClass, typename C, typename D>
-void def_read_write_mutable(PyClass& cls, const char* name, D C::* pm) {
-  cls.def_property(
-      name,
-      [pm](C& self) -> auto& { return self.*pm; },
-      [pm](C& self, const D& value) { self.*pm = value; });
-}
+class Original {
+ public:
+  virtual ~Original() {}
+
+  int stuff() const { return 1; }
+};
+
+class PyOriginal : public Original {};
 
 void init_module(py::module m) {
-  py::class_<Blerg> cls(m, "Blerg");
-  cls  // BR
+  py::class_<Original, PyOriginal>(m, "Original")
     .def(py::init())
-    // Normally, def_readwrite returns `const D&`. Here, we explicitly return
-    // it as mutable.
-    .def_property(
-        "matrix",
-        [](Blerg& self) -> auto& { return self.matrix; },
-        [](Blerg& self, Eigen::Matrix4d value) { self.matrix = value; });
-  // Alternative to use external function.
-  def_read_write_mutable(cls, "matrix_again", &Blerg::matrix);
+    .def("stuff", &Original::stuff);
 }
 
 int main(int, char**) {
@@ -46,16 +34,40 @@ int main(int, char**) {
 
   py::print("[ Eval ]");
   py::exec(R"""(
-obj = m.Blerg()
-print(".matrix")
-print(obj.matrix)
-obj.matrix[:] = 0
-print(obj.matrix)
+Original = m.Original
 
-print(".matrix_again")
-print(obj.matrix_again)
-obj.matrix_again[:] = 1
-print(obj.matrix_again)
+class ExtendA(Original):
+    def extra_a(self):
+        return 2
+
+
+class ExtendB(Original):
+    def extra_b(self):
+        return 3
+
+
+def main():
+    # N.B. These do work if the creation is changed to `ExtendA()` / 
+    # `ExtendB()`.
+    obj = Original()
+    obj.__class__ = ExtendA
+    assert obj.stuff() == 1
+    assert isinstance(obj, Original)
+    assert obj.extra_a() == 2
+    assert type(obj) == ExtendA
+
+    obj = Original()
+    obj.__class__ = ExtendB
+    assert obj.stuff() == 1
+    assert isinstance(obj, Original)
+    assert obj.extra_b() == 3
+    assert type(obj) == ExtendB
+
+    print("[ Done ]")
+
+
+assert __name__ == "__main__"
+main()
 )""");
 
   py::print("[ Done ]");
@@ -63,28 +75,10 @@ print(obj.matrix_again)
   return 0;
 }
 
-/**
+/*
 Output:
 
-[ Eval ]
-.matrix
-[[ 1.  0.  0.  0.]
- [ 0.  1.  0.  0.]
- [ 0.  0.  1.  0.]
- [ 0.  0.  0.  1.]]
-[[ 0.  0.  0.  0.]
- [ 0.  0.  0.  0.]
- [ 0.  0.  0.  0.]
- [ 0.  0.  0.  0.]]
-.matrix_again
-[[ 0.  0.  0.  0.]
- [ 0.  0.  0.  0.]
- [ 0.  0.  0.  0.]
- [ 0.  0.  0.  0.]]
-[[ 1.  1.  1.  1.]
- [ 1.  1.  1.  1.]
- [ 1.  1.  1.  1.]
- [ 1.  1.  1.  1.]]
-[ Done ]
+terminate called after throwing an instance of 'pybind11::error_already_set'
+  what():  TypeError: __class__ assignment: 'ExtendB' deallocator differs from 'test_module.Original'
 
 */
