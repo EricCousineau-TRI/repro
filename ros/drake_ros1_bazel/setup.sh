bc2769abe1d71ac22845ca4d51679a6cf85b4f1c
Eric Cousineau <eric.cousineau@tri.global>
2020-04-28 01:50:06 -0400

    yoohoo

--- ros/drake_ros1_bazel/setup.sh
+++ ros/drake_ros1_bazel/setup.sh
@@ -16,11 +16,14 @@ _venv_dir=${_cur_dir}/venv
         tar xfz ${file} -C ${_venv_dir} --strip-components=1
         python3 -m virtualenv -p python3 --system-site-packages ${_venv_dir}
 
+        py3_pkg_dir=${_venv_dir}/lib/python3.6/site-packages
+        ros_py2_pkg_dir=/usr/lib/python2.7/dist-packages
+
         # If this fails, we should fail fast...
-        test -d ${_venv_dir}/lib/python3.6
+        test -d ${py3_pkg_dir}
 
         # Hack.
-        ln -s /usr/lib/python2.7/dist-packages/rospkg ${_venv_dir}/lib/python3.6/site-packages
+        ln -s ${ros_py2_pkg_dir}/rospkg ${py3_pkg_dir}/
 
         py3_rebuild_dir=$(dirname ${_cur_dir})/ros1_py3_rebuild
         py3_rebuild_tar=${py3_rebuild_dir}/py3_rebuild.tar.gz
