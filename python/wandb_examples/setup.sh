cc2c86af3fd096952bab373aadb98ddb744ca88f
Eric Cousineau <eric.cousineau@tri.global>
2020-07-03 18:47:15 -0400

    add hack wrapper script

---
+++ python/wandb_examples/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+_script_dir=$(cd $(dirname ${BASH_SOURCE}) && pwd)
+_venv_dir=${_script_dir}/venv
+
+_venv-setup() { (
+    set +eux
+    finished_token=${_venv_dir}/.finished_token
+    if [[ ! -f ${finished_token} ]]; then
+        python3 -m virtualenv -p python3 ${_venv_dir}
+        ${_venv_dir}/bin/pip install -r ${_script_dir}/requirements.txt
+        touch ${finished_token}
+    fi
+) }
+
+_venv-setup
+source $(dirname ${BASH_SOURCE})/venv/bin/activate
