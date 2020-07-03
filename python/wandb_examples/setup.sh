fbd24bf899acd3a50a85dfb113f20f46b00b5a40
Eric Cousineau <eric.cousineau@tri.global>
2020-07-03 19:15:04 -0400

    able to reproduc

--- python/wandb_examples/setup.sh
+++ python/wandb_examples/setup.sh
@@ -5,11 +5,18 @@ _venv_dir=${_script_dir}/venv
 
 _venv-setup() { (
     set +eux
-    finished_token=${_venv_dir}/.finished_token
-    if [[ ! -f ${finished_token} ]]; then
+
+    files-equal() {
+        cmp "$@" > /dev/null 2>&1
+    }
+
+    requirements_in=${_script_dir}/requirements.txt
+    requirements_used=${_venv_dir}/requirements_used.txt
+    if ! files-equal ${requirements_in} ${requirements_used}; then
+        rm -rf ${_venv_dir}
         python3 -m virtualenv -p python3 ${_venv_dir}
-        ${_venv_dir}/bin/pip install -r ${_script_dir}/requirements.txt
-        touch ${finished_token}
+        ${_venv_dir}/bin/pip install -r ${requirements_in}
+        cp ${requirements_in} ${requirements_used}
     fi
 ) }
 
