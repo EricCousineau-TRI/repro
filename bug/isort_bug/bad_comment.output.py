e73591778217dff2937db6eae5155e5574d184c0
Eric Cousineau <eric.cousineau@tri.global>
2020-07-18 21:42:07 -0400

    see duplicated comment...

---
+++ bug/isort_bug/bad_comment.output.py
@@ -0,0 +1,12 @@
+"""
+Example edge case for isort==5.1.2:
+
+    isort --stdout ./bad_comment.input.py | tee ./bad_comment.output.py
+
+See: https://github.com/RobotLocomotion/drake/pull/13709#pullrequestreview-451092149
+"""
+
+# Comment for B - not A!
+import a
+# Comment for B - not A!
+import b
---
+++ bug/isort_bug/code.py
@@ -0,0 +1,39 @@
+from difflib import unified_diff
+from textwrap import dedent, indent
+
+import isort
+
+text_in_map = {
+    "single_line": dedent(r'''
+        """Single-line docstring"""
+        # Comment for A.
+        import a
+        # Comment for B - not A!
+        import b
+        ''').lstrip(),
+    "multi_line": dedent(r'''
+        """
+        Multi-line docstring
+        """
+        # Comment for A.
+        import a
+        # Comment for B - not A!
+        import b
+        ''').lstrip(),
+}
+
+for force_sort_within_sections in [False, True]:
+    for text_key, text_in in text_in_map.items():
+        config = isort.Config(force_sort_within_sections=force_sort_within_sections)
+        text_out = isort.code(text_in, config=config)
+        print(f"force_sort_within_sections = {force_sort_within_sections}")
+        print(f"text_in_map[{text_key}]")
+        diff = unified_diff(
+            text_in.splitlines(keepends=True),
+            text_out.splitlines(keepends=True),
+        )
+        diff_text = "".join(diff)
+        if not diff_text:
+            diff_text = "<no diff>"
+        print(indent(diff_text, "    "))
+        print()
