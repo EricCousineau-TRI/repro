8ef5c428738b9461b702856674536ee7ac130a93
Eric Cousineau <eric.cousineau@tri.global>
2020-08-01 11:08:01 -0400

    hack 'ems

--- python/bindings/pybind_clang/pybind_clang/scan_clang.py
+++ python/bindings/pybind_clang/pybind_clang/scan_clang.py
@@ -1,33 +1,43 @@
 import os
-from os.path import dirname
+from os.path import dirname, join
 from subprocess import run, PIPE
+from textwrap import dedent
+
 # :(
 from autopybind11.__main__ import BindingsGenerator, parse_options
-
-# :( :(
-fake_response = """
-includes: /usr/lib/llvm-9/include
-defines:
-c_std: --std=c++17
-"""
 
 
 def main():
     os.chdir(dirname(__file__))
 
-    output_dir = "/tmp/autopybind11"
-    rsp_path = f"{output_dir}/fake_response.txt"
+    llvm_inc = "/usr/lib/llvm-9/include"
+
+    tmp_dir = "/tmp/autopybind11"
+    input_dir = join(tmp_dir, "input")
+    output_dir = join(tmp_dir, "output")
+    os.makedirs(input_dir, exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
+
+    # :( :( :(
+    fake_response = dedent(f"""\
+    includes: {llvm_inc};{output_dir}
+    defines:
+    c_std: --std=c++17
+    """).strip()
+
+    rsp_path = join(input_dir, "fake_response.txt")
     with open(rsp_path, "w") as f:
         f.write(fake_response)
 
     castxml_path = run(
         "which castxml",
         shell=True, encoding="utf8", stdout=PIPE, check=True
-    ).strip()
+    ).stdout.strip()
 
     argv = [
-        "--output", f"{output_dir}/output",
-        "--input_yaml", "scan_clang_config.yml",
+        "--output", output_dir,
+        "--input_yaml", "./scan_clang_config.yml",
+        "--module_name", "sample_auto",
         "--castxml-path", castxml_path,
         "--input_response", rsp_path,        
     ]
