2d031085f59de1447cea80a6ee4584e3b2a0a390
Eric Cousineau <eric.cousineau@tri.global>
2020-07-31 21:54:24 -0400

    reorg

--- python/bindings/pybind_clang/pybind_clang/test/sample_py_test.py
+++ python/bindings/pybind_clang/pybind_clang/test/sample_py_test.py
@@ -1,4 +1,5 @@
-import sample as mut
+import pybind_clang.sample as mut
+from pybind_clang.execution import traced, reexecute_if_unbuffered
 
 
 class MyVisitor:
@@ -6,21 +7,21 @@ class MyVisitor:
         pass
 
     def visit_decl(self, d):
-        print(type(d))
         d.dump()
-        print(d.getDeclKindName())
+        print(d.getDeclKindName(), type(d))
         if isinstance(d, mut.NamedDecl):
             print(d.getQualifiedNameAsString())
         print("---")
         return True
 
 
+@traced
 def main():
     # WARNING: This is exhibiting singleton behavior. This setup is wrong
     # (using LibTooling setup, mimicing Binder). Should instead use CastXML
     # setup?
     for _ in range(1):
-        argv = [__file__, "src/example.cc"]
+        argv = [__file__, "pybind_clang/test/example.cc"]
         op = mut.CommonOptionsParser(argv)
         tool = mut.ClangTool(op.getCompilations(), op.getSourcePathList())
         visitor = MyVisitor()
@@ -29,4 +30,5 @@ def main():
 
 
 if __name__ == "__main__":
+    reexecute_if_unbuffered()
     main()
