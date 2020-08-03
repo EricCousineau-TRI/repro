67c4f94fc1724a2e320f4464fcd667e78ae3ad8e
Eric Cousineau <eric.cousineau@tri.global>
2020-08-03 00:16:55 -0400

    fix + simplify example

--- python/bindings/binder_sandbox/example_bind.sh
+++ python/bindings/binder_sandbox/example_bind.sh
@@ -3,12 +3,11 @@ set -eux -o pipefail
 
 cd $(dirname ${BASH_SOURCE})
 
-drake_path=~/tmp/venv/drake
 binder_bin=~/devel/binder/build/source/binder
 
 ${binder_bin} \
     --root-module test_struct \
-    --prefix /tmp/example_bind \
+    --prefix /tmp/example_bind/ \
     --bind testers \
     example.cc \
     -- \
