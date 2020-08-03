af2f8ffc9366860cdebf95f8d159fd5631d86781
Eric Cousineau <eric.cousineau@tri.global>
2020-08-02 22:14:32 -0400

    try out using binder... to not much avail?

---
+++ python/bindings/binder_sandbox/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux -o pipefail
+
+cd $(dirname ${BASH_SOURCE})
+
+drake_path=~/tmp/venv/drake
+binder_bin=~/devel/binder/build/source/binder
+
+${binder_bin} \
+    --root-module sample \
+    --prefix /tmp/binder \
+    --bind drake \
+    ./headers.h \
+    -- \
+    --std=c++17 \
+    -x c++ \
+    -I${drake_path}/include \
+    -I${drake_path}/include/fmt \
+    -I/usr/include/eigen3
