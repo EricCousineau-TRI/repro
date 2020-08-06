d6b9ebff8646a19774845b1cd01e3f23a02c410c
Eric Cousineau <eric.cousineau@tri.global>
2020-08-06 13:05:15 -0400

    fixup

--- python/bindings/pybind_clang/tmp/drake_render_camera_py.cc
+++ python/bindings/pybind_clang/tmp/drake_render_camera_py.cc
@@ -5,19 +5,17 @@
 
 namespace py = pybind11;
 void auto_bind_ClippingRange(py::module& m) {
-  py::class_<::drake::geometry::render::ClippingRange>(
-      m, "ClippingRange", py::module_local())
-      .def(py::init<::drake::geometry::render::ClippingRange const&>())
+  using Class = ClippingRange;
+  py::class_<Class>(
+      m, "ClippingRange")
+      .def(py::init<Class const&>())
       .def(py::init<double, double>())
-      .def_static("DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE",
-          static_cast<void (*)()>(&::drake::geometry::render::ClippingRange::
-                                      DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE))
       .def("far",
-          static_cast<double (::drake::geometry::render::ClippingRange::*)()
-                  const>(&::drake::geometry::render::ClippingRange::far))
+          static_cast<double (Class::*)()
+                  const>(&Class::far))
       .def("near",
-          static_cast<double (::drake::geometry::render::ClippingRange::*)()
-                  const>(&::drake::geometry::render::ClippingRange::near))
+          static_cast<double (Class::*)()
+                  const>(&Class::near))
 
       ;
 }
@@ -27,22 +25,19 @@ void auto_bind_ClippingRange(py::module& m) {
 
 namespace py = pybind11;
 void auto_bind_ColorRenderCamera(py::module& m) {
-  py::class_<::drake::geometry::render::ColorRenderCamera>(
-      m, "ColorRenderCamera", py::module_local())
-      .def(py::init<::drake::geometry::render::ColorRenderCamera const&>())
-      .def(py::init<::drake::geometry::render::RenderCameraCore, bool>())
-      .def_static("DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE",
-          static_cast<void (*)()>(
-              &::drake::geometry::render::ColorRenderCamera::
-                  DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE))
+  using Class = ColorRenderCamera;
+  py::class_<Class>(
+      m, "ColorRenderCamera")
+      .def(py::init<Class const&>())
+      .def(py::init<RenderCameraCore, bool>())
       .def("core",
-          static_cast<::drake::geometry::render::RenderCameraCore const& (
-              ::drake::geometry::render::ColorRenderCamera::*)() const>(
-              &::drake::geometry::render::ColorRenderCamera::core))
+          static_cast<RenderCameraCore const& (
+              Class::*)() const>(
+              &Class::core))
       .def("show_window",
-          static_cast<bool (::drake::geometry::render::ColorRenderCamera::*)()
+          static_cast<bool (Class::*)()
                   const>(
-              &::drake::geometry::render::ColorRenderCamera::show_window))
+              &Class::show_window))
 
       ;
 }
@@ -52,19 +47,17 @@ void auto_bind_ColorRenderCamera(py::module& m) {
 
 namespace py = pybind11;
 void auto_bind_DepthRange(py::module& m) {
-  py::class_<::drake::geometry::render::DepthRange>(
-      m, "DepthRange", py::module_local())
-      .def(py::init<::drake::geometry::render::DepthRange const&>())
+  using Class = DepthRange;
+  py::class_<Class>(
+      m, "DepthRange")
+      .def(py::init<Class const&>())
       .def(py::init<double, double>())
-      .def_static("DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE",
-          static_cast<void (*)()>(&::drake::geometry::render::DepthRange::
-                                      DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE))
       .def("max_depth",
-          static_cast<double (::drake::geometry::render::DepthRange::*)()
-                  const>(&::drake::geometry::render::DepthRange::max_depth))
+          static_cast<double (Class::*)()
+                  const>(&Class::max_depth))
       .def("min_depth",
-          static_cast<double (::drake::geometry::render::DepthRange::*)()
-                  const>(&::drake::geometry::render::DepthRange::min_depth))
+          static_cast<double (Class::*)()
+                  const>(&Class::min_depth))
 
       ;
 }
@@ -74,23 +67,20 @@ void auto_bind_DepthRange(py::module& m) {
 
 namespace py = pybind11;
 void auto_bind_DepthRenderCamera(py::module& m) {
-  py::class_<::drake::geometry::render::DepthRenderCamera>(
-      m, "DepthRenderCamera", py::module_local())
-      .def(py::init<::drake::geometry::render::DepthRenderCamera const&>())
-      .def(py::init<::drake::geometry::render::RenderCameraCore,
-          ::drake::geometry::render::DepthRange>())
-      .def_static("DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE",
-          static_cast<void (*)()>(
-              &::drake::geometry::render::DepthRenderCamera::
-                  DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE))
+  using Class = DepthRenderCamera;
+  py::class_<Class>(
+      m, "DepthRenderCamera")
+      .def(py::init<Class const&>())
+      .def(py::init<RenderCameraCore,
+          DepthRange>())
       .def("core",
-          static_cast<::drake::geometry::render::RenderCameraCore const& (
-              ::drake::geometry::render::DepthRenderCamera::*)() const>(
-              &::drake::geometry::render::DepthRenderCamera::core))
+          static_cast<RenderCameraCore const& (
+              Class::*)() const>(
+              &Class::core))
       .def("depth_range",
-          static_cast<::drake::geometry::render::DepthRange const& (
-              ::drake::geometry::render::DepthRenderCamera::*)() const>(
-              &::drake::geometry::render::DepthRenderCamera::depth_range))
+          static_cast<DepthRange const& (
+              Class::*)() const>(
+              &Class::depth_range))
 
       ;
 }
@@ -100,31 +90,29 @@ void auto_bind_DepthRenderCamera(py::module& m) {
 
 namespace py = pybind11;
 void auto_bind_RenderCameraCore(py::module& m) {
-  py::class_<::drake::geometry::render::RenderCameraCore>(
-      m, "RenderCameraCore", py::module_local())
-      .def(py::init<::drake::geometry::render::RenderCameraCore const&>())
-      .def(py::init<::std::string, ::drake::systems::sensors::CameraInfo,
-          ::drake::geometry::render::ClippingRange,
-          ::drake::math::RigidTransformd>())
-      .def_static("DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE",
-          static_cast<void (*)()>(&::drake::geometry::render::RenderCameraCore::
-                                      DRAKE_COPYABLE_DEMAND_COPY_CAN_COMPILE))
+  using Class = RenderCameraCore;
+  py::class_<Class>(
+      m, "RenderCameraCore")
+      .def(py::init<Class const&>())
+      .def(py::init<::std::string, CameraInfo,
+          ClippingRange,
+          RigidTransformd>())
       .def("clipping",
-          static_cast<::drake::geometry::render::ClippingRange const& (
-              ::drake::geometry::render::RenderCameraCore::*)() const>(
-              &::drake::geometry::render::RenderCameraCore::clipping))
+          static_cast<ClippingRange const& (
+              Class::*)() const>(
+              &Class::clipping))
       .def("intrinsics",
-          static_cast<::drake::systems::sensors::CameraInfo const& (
-              ::drake::geometry::render::RenderCameraCore::*)() const>(
-              &::drake::geometry::render::RenderCameraCore::intrinsics))
+          static_cast<CameraInfo const& (
+              Class::*)() const>(
+              &Class::intrinsics))
       .def("renderer_name",
           static_cast<::std::string const& (
-              ::drake::geometry::render::RenderCameraCore::*)() const>(
-              &::drake::geometry::render::RenderCameraCore::renderer_name))
+              Class::*)() const>(
+              &Class::renderer_name))
       .def("sensor_pose_in_camera_body",
-          static_cast<::drake::math::RigidTransformd const& (
-              ::drake::geometry::render::RenderCameraCore::*)() const>(
-              &::drake::geometry::render::RenderCameraCore::
+          static_cast<RigidTransformd const& (
+              Class::*)() const>(
+              &Class::
                   sensor_pose_in_camera_body))
 
       ;
