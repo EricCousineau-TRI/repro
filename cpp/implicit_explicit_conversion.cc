f04b47c15196e1ad1e7c235669ebee22609ed912
Eric Cousineau <eric.cousineau@tri.global>
2020-08-10 12:34:17 -0400

    add some more sample

--- cpp/implicit_explicit_conversion.cc
+++ cpp/implicit_explicit_conversion.cc
@@ -2,12 +2,15 @@
 
 struct A {
   A() { }
-  A(int) { std::cout << "A(int)\n"; }
+  A(const A&) { std::cout << "A(A)\n"; }
+  // If this were explicit, construction of `A x = 1` would fail.
+  /*explicit*/ A(int) { std::cout << "A(int)\n"; }
   A& operator=(int) { std::cout << "A=int\n"; return *this; }
 };
 
 int main() {
   A x = 1;
+  // A x(1);
   x = 2;
   A y;
   y = 3;
