f9f9c4a6eca68d4f4c9c5ccd37d0804029c76fed
Eric Cousineau <eric.cousineau@tri.global>
2020-10-01 12:00:24 -0400

    make more minimal

--- cpp/op_overload_ref.cc
+++ cpp/op_overload_ref.cc
@@ -4,7 +4,6 @@ namespace nested {
 
 struct C {
   friend int operator+(C, C) { return 10; }
-  friend int operator+(C, int) { return 20; }
 };
 
 }
@@ -18,13 +17,9 @@ int main() {
   std::cout << (C{} + C{}) << std::endl;
 
   MyFunc func1 = &operator+;
-  auto func2 = static_cast<int (*)(C, int)>(&nested::operator+);
-  auto func3 = static_cast<int (*)(C, C)>(&nested::operator+);
 
   std::cout << "addresses:" << std::endl
-      << "  " << func1 << "  " << func1(C{}, C{}) << std::endl
-      << "  " << func2 << "  " << func2(C{}, int{}) << std::endl
-      << "  " << func3 << "  " << func3(C{}, C{}) << std::endl;
+      << "  " << func1 << "  " << func1(C{}, C{}) << std::endl;
 
   return 0;
 }
@@ -39,11 +34,5 @@ using nested::operator+;
 ./op_overload_ref.cc:20:19: error: use of undeclared 'operator+'
   MyFunc func1 = &operator+;
                   ^
-./op_overload_ref.cc:21:54: error: no member named 'operator+' in namespace 'nested'
-  auto func2 = static_cast<int (*)(C, int)>(&nested::operator+);
-                                             ~~~~~~~~^
-./op_overload_ref.cc:22:52: error: no member named 'operator+' in namespace 'nested'
-  auto func3 = static_cast<int (*)(C, C)>(&nested::operator+);
-                                           ~~~~~~~~^
-4 errors generated.
+2 errors generated.
 */
