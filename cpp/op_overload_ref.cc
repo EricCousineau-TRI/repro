290c6b929bd33065c79e84adc2c8638e0f48e2d2
Eric Cousineau <eric.cousineau@tri.global>
2020-10-01 12:07:21 -0400

    revisit: does not work

--- cpp/op_overload_ref.cc
+++ cpp/op_overload_ref.cc
@@ -20,10 +20,10 @@ typedef int (*MyFunc)(C, C);
 int main() {
   std::cout << (C{} + C{}) << std::endl;
 
-  // MyFunc func1 = &operator+;
+  MyFunc func1 = &operator+;
 
-  // std::cout
-  //     << "&func1=" << func1 << ",  value=" << func1(C{}, C{}) << std::endl;
+  std::cout
+      << "&func1=" << func1 << ",  value=" << func1(C{}, C{}) << std::endl;
 
   return 0;
 }
@@ -32,5 +32,9 @@ int main() {
 Output:
 
 $ clang++-9 -std=c++17 ./op_overload_ref.cc && ./a.out
-100
+./op_overload_ref.cc:23:19: error: use of undeclared 'operator+'
+  MyFunc func1 = &operator+;
+                  ^
+1 error generated.
+
 */
