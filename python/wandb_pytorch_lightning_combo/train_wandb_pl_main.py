842f9e4e668fdc333e88cb5ca7965f41d4aed8de
Eric Cousineau <eric.cousineau@tri.global>
2021-02-18 15:50:28 -0500

    simplify more

--- python/wandb_pytorch_lightning_combo/train_wandb_pl_main.py
+++ python/wandb_pytorch_lightning_combo/train_wandb_pl_main.py
@@ -1,50 +1,9 @@
 #!/usr/bin/env python3
 
-import os
-
 import argparse
-import pytorch_lightning as pl
-import torch
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
+
+import numpy as np
 import wandb
-
-
-class ConstantMultiply(pl.LightningModule):
-    def __init__(self):
-        super().__init__()
-        self.K = torch.nn.Parameter(torch.tensor([1.0]))
-
-    def forward(self, x):
-        return self.K * x
-
-    def _step(self, phase, batch, batch_idx):
-        x, y = batch
-        yhat = self(x)
-        loss = F.mse_loss(yhat, y)
-        self.log(f"{phase}/my_loss", loss)
-        return loss
-
-    def training_step(self, batch, batch_idx):
-        return self._step("train", batch, batch_idx)
-
-    def validation_step(self, batch, batch_idx):
-        return self._step("val", batch, batch_idx)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.01)
-        return optimizer
-
-
-@torch.no_grad()
-def create_datasets(K, count):
-    xs = [torch.tensor([i]) for i in range(count * 2)]
-    ys = [K * x for x in xs]
-    dataset = [(x, y) for x, y in zip(xs, ys)]
-    dataset_train = dataset[:count]
-    dataset_val = dataset[count:]
-    return dataset_train, dataset_val
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -53,38 +12,17 @@ def main():
     args = parser.parse_args()
 
     if args.is_wandb_sweep:
-        assert args.json is not None
-        assert args.json != ""
+        print(f"I am a sweep! {args.wandb_sweep_json}")
+        assert args.wandb_sweep_json is not None
+        assert args.wandb_sweep_json != ""
 
-    torch.random.manual_seed(0)
-
-    with torch.no_grad():
-        K_gt = torch.tensor(2.0, requires_grad=False)
-    model = ConstantMultiply()
-
-    count = 8
-    dataset_train, dataset_val = create_datasets(K_gt, count)
-    batch_size = 4
-    dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=False)
-    dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=False)
-
-    logger = pl.loggers.WandbLogger(
+    wandb.init(
         name="test-run",
         project="uncategorized",
-        log_model=True,
     )
-
-    trainer = pl.Trainer(
-        logger=logger,
-        max_epochs=5,
-        num_processes=2,
-        accelerator="ddp_cpu",
-        gpus=None,
-    )
-    trainer.fit(model, dataloader_train, dataloader_val)
-
-    with torch.no_grad():
-        print(f"Final error for K: {model.K - K_gt}", flush=True)
+    loss = np.array(1.0)
+    wandb.log({"val/loss": loss})
+    print("[ Done ]")
 
 
 if __name__ == "__main__":
