5ce6f13770e842572470642f1bb1c6427d08a246
Eric Cousineau <eric.cousineau@tri.global>
2021-03-12 16:06:11 -0500

    show focused repro for just the xforms

--- bug/assimp_mesh_dae_to_obj/setup_and_run.py
+++ bug/assimp_mesh_dae_to_obj/setup_and_run.py
@@ -52,44 +52,10 @@ def main():
     if not isfile("base.dae"):
         shell(f"wget https://raw.githubusercontent.com/ros-industrial/universal_robot/{commit}/ur_description/meshes/ur10/visual/base.dae")
 
-    # Download and build assimp C/C++ stuff.
-    cd(source_tree)
-    assimp_dir = abspath("assimp")
-    if not isdir("assimp"):
-        # Download and build assmip.
-        # shell("git clone https://github.com/assimp/assimp -b v5.0.1")
-        shell("git clone https://github.com/assimp/assimp -b v4.1.0")
-        cd(assimp_dir)
-
-        mkcd("build")
-        shell(f"cmake .. -GNinja")
-        shell("ninja")
-
-    # Make venv.
-    venv_dir = abspath("venv")
-    cd(source_tree)
-    if not isdir(venv_dir):
-        shell(f"python3 -m venv {venv_dir} --system-site-packages")
-
-        # Retarget assimp to install to venv.
-        cd(assimp_dir)
-        cd("build")
-        shell(f"cmake . -DCMAKE_INSTALL_PREFIX={venv_dir}")
-        shell(f"ninja install")
-        # Do Python install.
-        cd("../port/PyAssimp")
-        shell(f"{venv_dir}/bin/python ./setup.py install")
-        # Futz around with solib path.
-        cd(f"{venv_dir}/lib/python3.6/site-packages/pyassimp")
-        shell("ln -s ../../../libassimp.so ./")
-
     # Run w/ different versions.
     cd(source_tree)
     print("[ System Version ]")
     shell("python3 ./repro.py", check=False)
-    print()
-    print("[ Rebuilt Version ]")
-    shell(f"{venv_dir}/bin/python ./repro.py", check=False)
 
 
 if __name__ == "__main__":
