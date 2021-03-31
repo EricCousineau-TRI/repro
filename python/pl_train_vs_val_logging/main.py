f897437e56daa231b95a55815e1fb7a79816a97b
Eric Cousineau <eric.cousineau@tri.global>
2021-03-31 12:49:39 -0400

    k, this works as would be expected

--- python/pl_train_vs_val_logging/main.py
+++ python/pl_train_vs_val_logging/main.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Seems fine in current setup:
+
+    val: N=2, batch_idx=0, K_pre=1.0
+    val: N=2, batch_idx=1, K_pre=1.0
+    train: N=2, batch_idx=0, K_pre=1.0
+    train: N=2, batch_idx=1, K_pre=2.0
+    val: N=2, batch_idx=0, K_pre=3.0
+    val: N=2, batch_idx=1, K_pre=3.0
+    train: N=2, batch_idx=0, K_pre=3.0
+    train: N=2, batch_idx=1, K_pre=4.0
+    val: N=2, batch_idx=0, K_pre=5.0
+    val: N=2, batch_idx=1, K_pre=5.0
+"""
+
+import warnings
+
 import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader
@@ -9,16 +28,14 @@ class ConstantMultiply(pl.LightningModule):
         self.K = torch.nn.Parameter(torch.tensor([1.0]))
 
     def forward(self, x):
-        return self.K * x
+        return -self.K
 
     def _step(self, phase, batch, batch_idx):
         K_pre, = self.K.detach().cpu().numpy()
-        print(f"{phase}: N={N}, batch_idx={batch_idx}, K_pre={K_pre}")
         N = len(batch)
-        x = batch
-        yhat = self(x)
+        print(f"{phase}: N={N}, batch_idx={batch_idx}, K_pre={K_pre}")
         # Goal here is to get parameter to increment once per batch.
-        loss = -yhat.mean()
+        loss = self(batch).mean()
         self.log(f"{phase}/loss", loss)
         return loss
 
@@ -35,23 +52,30 @@ class ConstantMultiply(pl.LightningModule):
 
 
 @torch.no_grad()
-def create_datasets(count):
+def create_dataset(count):
     # These values don't actually value.
     xs = [torch.tensor([0.0]) for i in range(count)]
-    dataset = [(x, y) for x, y in zip(xs, ys)]
-    return dataset
+    return xs
 
 
 def main():
     count = 4
     N = 2
-    dataset = create_datasets(count)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-    dataloader_val = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    dataset = create_dataset(count)
+    dataloader = DataLoader(dataset, batch_size=N, shuffle=False)
+    dataloader_val = DataLoader(dataset, batch_size=N, shuffle=False)
 
     model = ConstantMultiply()
-    trainer = pl.Trainer(max_epochs=5)
-    trainer.fit(model, train_dataloader=dataloader, val_dataloaders=dataloader)
+    warnings.simplefilter("ignore", UserWarning)  # Denoise pl
+    trainer = pl.Trainer(
+        max_epochs=2,
+        progress_bar_refresh_rate=0,
+    )
+    trainer.fit(
+        model,
+        train_dataloader=dataloader, 
+        val_dataloaders=dataloader,
+    )
 
 
 if __name__ == "__main__":
