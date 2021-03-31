4d6d75765c655ff725b7cd1f33dcc60da17f2bfb
Eric Cousineau <eric.cousineau@tri.global>
2021-03-31 13:31:47 -0400

    try inpsect... werid

--- python/pl_train_vs_val_logging/main.py
+++ python/pl_train_vs_val_logging/main.py
@@ -15,6 +15,8 @@ Seems fine in current setup:
     val: N=2, batch_idx=1, K_pre=5.0
 """
 
+import pprint as pp
+from textwrap import indent
 import warnings
 
 import pytorch_lightning as pl
@@ -71,11 +73,78 @@ def main():
         max_epochs=2,
         progress_bar_refresh_rate=0,
     )
+    # TODO(eric.cousineau): This causes training to break?
+    # pprint_trainer_args(trainer)
     trainer.fit(
         model,
         train_dataloader=dataloader, 
         val_dataloaders=dataloader,
     )
+
+
+def pprint_trainer_args(trainer):
+    ignore = {"progress_bar_dict"}
+    out = {}
+    for attr in dir(trainer):
+        if attr.startswith("_") or attr in ignore:
+            continue
+        try:
+            v = getattr(trainer, attr)
+        except AttributeError as e:
+            raise RuntimeError((e, attr))
+        if is_primitive(v):
+            out[attr] = v
+    print(pformat(out))
+
+
+def is_primitive(v):
+    if isinstance(v, (bool, int, float, str)):
+        return True
+    elif isinstance(v, (list, tuple)):
+        for vi in v:
+            if not is_primitive(vi):
+                return False
+        return True
+    elif isinstance(v, dict):
+        for ki, vi in v.items():
+            if not is_primitive(ki) or not is_primitive(vi):
+                return False
+        return True
+    else:
+        return False
+    assert False
+
+
+
+def pformat(obj, incr="  "):
+    """
+    Pretty formatting for values with more vertical whitespace, less hanging
+    indents.
+    """
+    def sub_pformat(obj):
+        txt = pformat(obj, incr=incr)
+        return indent(txt, incr)
+    # Try short version.
+    short_len = 60
+    maybe_short = pp.pformat(obj)
+    if "\n" not in maybe_short and len(maybe_short) <= short_len:
+        return maybe_short
+
+    if isinstance(obj, list):
+        out = f"[\n"
+        for obj_i in obj:
+            out += sub_pformat(obj_i) + ",\n"
+        out += f"]"
+        return out
+    elif isinstance(obj, dict):
+        out = f"{{\n"
+        for k_i, obj_i in obj.items():
+            txt = sub_pformat(obj_i)
+            out += f"{incr}{repr(k_i)}: {txt.strip()},\n"
+        out += f"}}"
+        return out
+    else:
+        return indent(pp.pformat(obj), incr)
 
 
 if __name__ == "__main__":
