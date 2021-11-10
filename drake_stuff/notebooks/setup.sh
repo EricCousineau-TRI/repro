7d7002b20982ac2cf8dc79c8321c08b2f5c8f220
Eric Cousineau <eric.cousineau@tri.global>
2021-11-10 10:09:39 -0500

    Partial revert "upgrade to pip install drake" - see drake issue 16069
    This reverts commit 77233c56c79662e9d5b3a73e90eb744908f218ec.

--- drake_stuff/notebooks/setup.sh
+++ drake_stuff/notebooks/setup.sh
@@ -14,10 +14,25 @@
 _cur_dir=$(cd $(dirname ${BASH_SOURCE}) && pwd)
 _venv_dir=${_cur_dir}/venv
 
+_download_drake() { (
+    # See: https://drake.mit.edu/from_binary.html
+    # Download and echo path to stdout for capture.
+    set -eux
+
+    # v0.32.0
+    base=drake-20210714-bionic.tar.gz
+    dir=~/Downloads
+    uri=https://drake-packages.csail.mit.edu/drake/nightly
+    if [[ ! -f ${dir}/${base} ]]; then
+        wget ${uri}/${base} -O ${dir}/${base}
+    fi
+    echo ${dir}/${base}
+) }
+
 _setup_venv() { (
     set -eu
     cd ${_cur_dir}
-    completion_token="2021-11-10.0"
+    completion_token="2021-07-15.0"
     completion_file=${_venv_dir}/.completion-token
 
     if [[ -f ${completion_file} && "$(cat ${completion_file})" == "${completion_token}" ]]; then
@@ -27,7 +42,11 @@ _setup_venv() { (
     set -x
     rm -rf ${_venv_dir}
 
-    python3 -m venv ${_venv_dir}
+    mkdir -p ${_venv_dir}
+    tar -xzf $(_download_drake) -C ${_venv_dir} --strip-components=1
+
+    # See: https://drake.mit.edu/python_bindings.html#inside-virtualenv
+    python3 -m venv ${_venv_dir} --system-site-packages
     cd ${_venv_dir}
     ./bin/pip install -U pip wheel
     ./bin/pip install --force-reinstall -r ${_cur_dir}/requirements.txt
