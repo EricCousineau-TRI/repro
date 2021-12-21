d1cbe5430e6faa3f35aafcc370f9e74f973c6d9c
Eric Cousineau <eric.cousineau@tri.global>
2021-12-21 13:08:54 -0500

    meh

--- python/torch/torch_simple_grad.py
+++ python/torch/torch_simple_grad.py
@@ -52,19 +52,20 @@ def torch_forward_diff(net):
     assert False, type(net)
 
 
-class FwdDiff:
+class FwdDiff(nn.Module):
     @classmethod
     def create(cls, net) -> "Optional[FwdDiff]":
         raise NotImplemented()
 
     def __init__(self, net):
+        super().__init__()
         self.net = net
 
     def dual(self, x, dx) -> "Tuple[Tensor, Tensor]":
         # Must be batched.
         raise NotImplemented()
 
-    def __call__(self, x):
+    def forward(self, x):
         N, L = x.shape
         dx = torch.eye(L, device=x.device, dtype=x.dtype)
         dx = dx.repeat(N, 1, 1)
@@ -97,7 +98,7 @@ class ReLUFwdDiff(FwdDiff):
         if isinstance(net, nn.ReLU):
             return cls(net)
         return None
-    
+
     def dual(self, x, dx):
         dx = torch_col_zero(dx, x <= 0)
         x = self.net(x)
@@ -106,15 +107,17 @@ class ReLUFwdDiff(FwdDiff):
 
 @register
 class SequentialFwdDiff(FwdDiff):
+#     __constants__ = ["fwds"]
     @classmethod
     def create(cls, net):
         if isinstance(net, nn.Sequential):
-            return cls(net)
+            fwds = nn.ModuleList([torch_forward_diff(net_i) for net_i in net])
+            return cls(net, fwds)
         return None
     
-    def __init__(self, net):
+    def __init__(self, net, fwds):
         super().__init__(net)
-        self.fwds = [torch_forward_diff(net_i) for net_i in net]
+        self.fwds = fwds
 
     def dual(self, x, dx):
         for fwd in self.fwds:
