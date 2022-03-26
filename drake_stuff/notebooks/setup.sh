6642ed8638d028891f6a250d17812ceb01604795
Eric Cousineau <eric.cousineau@tri.global>
2022-03-25 21:32:12 -0400

    add example of geometry id's not being stable

--- drake_stuff/notebooks/setup.sh
+++ drake_stuff/notebooks/setup.sh
@@ -14,27 +14,10 @@
 _cur_dir=$(cd $(dirname ${BASH_SOURCE}) && pwd)
 _venv_dir=${_cur_dir}/venv
 
-_download_drake() { (
-    # TODO(eric): Use pip once drake#16069 is resolved.
-
-    # See: https://drake.mit.edu/from_binary.html
-    # Download and echo path to stdout for capture.
-    set -eux
-
-    # v0.35.0
-    base=drake-20211021-bionic.tar.gz
-    dir=~/Downloads
-    uri=https://drake-packages.csail.mit.edu/drake/nightly
-    if [[ ! -f ${dir}/${base} ]]; then
-        wget ${uri}/${base} -O ${dir}/${base}
-    fi
-    echo ${dir}/${base}
-) }
-
 _setup_venv() { (
     set -eu
     cd ${_cur_dir}
-    completion_token="2021-11-10.1"
+    completion_token="2022-03-25.2"
     completion_file=${_venv_dir}/.completion-token
 
     if [[ -f ${completion_file} && "$(cat ${completion_file})" == "${completion_token}" ]]; then
@@ -44,14 +27,11 @@ _setup_venv() { (
     set -x
     rm -rf ${_venv_dir}
 
-    mkdir -p ${_venv_dir}
-    tar -xzf $(_download_drake) -C ${_venv_dir} --strip-components=1
-
     # See: https://drake.mit.edu/python_bindings.html#inside-virtualenv
-    python3 -m venv ${_venv_dir} --system-site-packages
+    python3 -m venv ${_venv_dir}
     cd ${_venv_dir}
     ./bin/pip install -U pip wheel
-    ./bin/pip install --force-reinstall -r ${_cur_dir}/requirements.txt
+    ./bin/pip install -r ${_cur_dir}/requirements.txt
     ./bin/pip freeze > ${_cur_dir}/requirements.freeze.txt
 
     echo "${completion_token}" > ${completion_file}
