974bedf8b3ffa3a73814ff2556d5767c2a8db8dc
Marco A. Gutierrez <marco@openrobotics.org>
2022-04-10 16:03:33 +0000

    moving generate_sdf function to python

--- drake_stuff/mbp_robot_arm_joint_limit_stuff/compare_model_via_drake_and_ingition_images.sh
+++ drake_stuff/mbp_robot_arm_joint_limit_stuff/compare_model_via_drake_and_ingition_images.sh
@@ -2,68 +2,7 @@
 
 _cur_dir=$(cd $(dirname ${BASH_SOURCE}) && pwd)
 
-_generate_sdf() { (
-
-    echo '<sdf version="1.6">
-    <world name="default">
-        <plugin
-        filename="ignition-gazebo-physics-system"
-        name="ignition::gazebo::systems::Physics">
-        </plugin>
-        <plugin
-        filename="ignition-gazebo-sensors-system"
-        name="ignition::gazebo::systems::Sensors">
-        <render_engine>ogre2</render_engine>
-        <background_color>0, 1, 0</background_color>
-        </plugin>
-        <plugin
-            filename="ignition-gazebo-user-commands-system"
-            name="ignition::gazebo::systems::UserCommands">
-        </plugin>
-        <plugin
-        filename="ignition-gazebo-scene-broadcaster-system"
-        name="ignition::gazebo::systems::SceneBroadcaster">
-        </plugin>
-        <include>
-        <uri>'$1'</uri>
-        <plugin
-            filename="ignition-gazebo-model-photo-shoot-system"
-            name="ignition::gazebo::systems::ModelPhotoShoot">
-            <translation_data_file>'$2'</translation_data_file>
-            <random_joints_pose>'$3'</random_joints_pose>
-        </plugin>
-        </include>
-        <model name="photo_shoot">
-        <pose>2.2 0 0 0 0 -3.14</pose>
-        <link name="link">
-            <pose>0 0 0 0 0 0</pose>
-            <sensor name="camera" type="camera">
-            <camera>
-                <horizontal_fov>1.047</horizontal_fov>
-                <image>
-                <width>960</width>
-                <height>540</height>
-                </image>
-                <clip>
-                <near>0.1</near>
-                <far>100</far>
-                </clip>
-            </camera>
-            <always_on>1</always_on>
-            <update_rate>30</update_rate>
-            <visualize>true</visualize>
-            <topic>camera</topic>
-            </sensor>
-        </link>
-        <static>true</static>
-        </model>
-    </world>
-    </sdf>' > $4
-
-) }
-
 _test_models() { (
-    source /usr/share/gazebo/setup.bash
 
     cd "$3"
     mkdir -p visual/model/
@@ -76,12 +15,12 @@ _test_models() { (
     # Generate model pics using the gazebo plugin.
     mkdir -p $3/visual/pics/default_pose/
     cd "$3/visual/pics/default_pose/"
-    _generate_sdf "$3/visual/model/$2" "$3/visual/pics/default_pose/poses.txt" "false" "$3/visual/pics/default_pose/plugin_config.sdf"
+    python -c "import sdf_manipulation; sdf_manipulation.generate_sdf('$3/visual/model/$2', '$3/visual/pics/default_pose/poses.txt', 'false', '$3/visual/pics/default_pose/plugin_config.sdf')"
     ign gazebo -s -r "$3/visual/pics/default_pose/plugin_config.sdf" --iterations 50
 
     mkdir -p $3/visual/pics/random_pose/
     cd "$3/visual/pics/random_pose/"
-    _generate_sdf "$3/visual/model/$2" "$3/visual/pics/random_pose/poses.txt" "true" "$3/visual/pics/random_pose/plugin_config.sdf"
+    python -c "import sdf_manipulation; sdf_manipulation.generate_sdf('$3/visual/model/$2', '$3/visual/pics/random_pose/poses.txt', 'true', '$3/visual/pics/random_pose/plugin_config.sdf')"
     ign gazebo -s -r "$3/visual/pics/random_pose/plugin_config.sdf" --iterations 50
 
     # Generate model pics using drake then run
@@ -109,12 +48,12 @@ _test_models() { (
     # Generate model pics using the gazebo plugin.
     mkdir -p $3/collisions/pics/default_pose/
     cd "$3/collisions/pics/default_pose/"
-    _generate_sdf "$3/collisions/model/$2" "$3/collisions/pics/default_pose/poses.txt" "false" "$3/collisions/pics/default_pose/plugin_config.sdf"
+    python -c "import sdf_manipulation; sdf_manipulation.generate_sdf('$3/collisions/model/$2', '$3/collisions/pics/default_pose/poses.txt', 'false', '$3/collisions/pics/default_pose/plugin_config.sdf')"
     ign gazebo -s -r "$3/collisions/pics/default_pose/plugin_config.sdf" --iterations 50
 
     mkdir -p $3/collisions/pics/random_pose/
     cd "$3/collisions/pics/random_pose/"
-    _generate_sdf "$3/collisions/model/$2" "$3/collisions/pics/random_pose/poses.txt" "true" "$3/collisions/pics/random_pose/plugin_config.sdf"
+    python -c "import sdf_manipulation; sdf_manipulation.generate_sdf('$3/collisions/model/$2', '$3/collisions/pics/random_pose/poses.txt', 'true', '$3/collisions/pics/random_pose/plugin_config.sdf')"
     ign gazebo -s -r "$3/collisions/pics/random_pose/plugin_config.sdf" --iterations 50
 
     # Generate model pics using drake then run
