c9e30e90664e8b715a98376c27eda87ca012425d
Eric Cousineau <eric.cousineau@tri.global>
2022-06-16 16:30:27 -0400

    use clang, cat param file

--- bazel/bazel_asan_features/repro.sh
+++ bazel/bazel_asan_features/repro.sh
@@ -17,12 +17,14 @@ bazel_ clean --expunge --async
 (
     bazel version
     bazel_ build -j 1 -s //:example
+    cat bazel-out/k8-opt/bin/example-2.params
 ) 2>&1 | scrub | tee repro.without.txt
 
 bazel_ clean --expunge --async
 (
     bazel version
     bazel_ build --features=asan -j 1 -s //:example
+    cat bazel-out/k8-opt/bin/example-2.params
 ) 2>&1 | scrub | tee repro.with.txt
 
 git --no-pager diff --no-index repro.*.txt | tee repro.diff
