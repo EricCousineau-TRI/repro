2999dc2f642ceacfa4b4834e1da6c0496f0b8a96
Eric Cousineau <eric.cousineau@tri.global>
2022-08-01 20:54:51 -0400

    how to make this work?

---
+++ python/exec_closure.py
@@ -0,0 +1,18 @@
+ignore_globals = None
+my_globals = dict()
+my_locals = dict()
+
+def my_exec(code):
+    exec(code, my_globals, my_locals)
+
+def print_vars():
+    filtered_globals = {k: v for k, v in my_globals.items() if k not in ignore_globals}
+    print(f"globals: {filtered_globals}\nlocals: {my_locals}")
+
+my_exec("")
+ignore_globals = set(my_globals.keys())
+my_exec("b = 1")
+print_vars()
+my_exec("def my_func(x): return x + b")
+print_vars()
+my_exec("print(my_func(2))")
