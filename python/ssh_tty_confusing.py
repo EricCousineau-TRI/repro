966280cd30ea39a9d2bd739e356bf9b7bd8721b3
Eric Cousineau <eric.cousineau@tri.global>
2022-08-05 13:13:12 -0400

    this still seems fin

--- python/ssh_tty_confusing.py
+++ python/ssh_tty_confusing.py
@@ -3,38 +3,43 @@ import subprocess
 from textwrap import dedent
 
 
-def dumb_run(args, *, shell):
-    out = subprocess.run(
-        args,
-        shell=shell,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        check=True,
-        text=True,
-    )
-    print(out.stdout)
+def dumb_run(args, *, redirect):
+    if redirect:
+        out = subprocess.run(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            check=True,
+            text=True,
+        )
+        print(out.stdout)
+    else:
+        subprocess.run(args, check=True)
 
 
 def main():
     # todo: need something that relies on "weird" tty behavior... like `bazel build` or `tshark` :(
-    command = dedent("""
-    watch -n 0.5 -g date
+    command = dedent(r"""
+    for i in $(seq 5); do
+        echo -en '\rStep '${i}
+        sleep 0.1
+    done
+    echo
     """)
-    dumb_run(command, shell=True)
+    dumb_run(["bash", "-c", command], redirect=False)
+    dumb_run(["bash", "-c", command], redirect=True)
     user_host = os.environ["USER"] + "@localhost"
-    dumb_run(
-        [
-            "ssh",
-            "-tt",
-            "-o",
-            "BatchMode=yes",
-            "-o",
-            "LogLevel=ERROR",
-            user_host,
-            command,
-        ],
-        shell=False,
-    )
+    ssh_args = [
+        "ssh",
+        # "-tt",
+        "-o",
+        "BatchMode=yes",
+        "-o",
+        "LogLevel=ERROR",
+        user_host,
+    ]
+    dumb_run(ssh_args + [command], redirect=False)
+    dumb_run(ssh_args + [command], redirect=True)
 
 
 assert __name__ == "__main__"
