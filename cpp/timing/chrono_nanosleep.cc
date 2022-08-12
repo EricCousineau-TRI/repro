851ee233761d24dbdab39f4e492573d5c81feee1
Eric Cousineau <eric.cousineau@tri.global>
2022-08-12 16:07:50 -0400

    simple sleep

---
+++ cpp/timing/chrono_nanosleep.cc
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <ctime>
+
+#include <chrono>
+#include <stdexcept>
+
+void sleep_high_resolution(std::chrono::nanoseconds amount) {
+  timespec amount_c{
+    .tv_sec = amount.count() / std::nano::den,
+    .tv_nsec = amount.count() % std::nano::den
+  };
+  if (nanosleep(&amount_c, nullptr) != 0) {
+    throw std::runtime_error("bad sleep");
+  }
+}
+
+int main() {
+  using namespace std::literals::chrono_literals;
+
+  sleep_high_resolution(50us);
+  sleep_high_resolution(50ms);
+}
