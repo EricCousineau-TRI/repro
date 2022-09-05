5ba2a9d1190ccf34bd2f9cced1f69f76e3d36fca
Eric Cousineau <eric.cousineau@tri.global>
2022-09-05 14:29:02 -0400

    equiv, good

--- bazel/bazel_test_suite_tags/repro.sh
+++ bazel/bazel_test_suite_tags/repro.sh
@@ -18,3 +18,7 @@ bazel-test() {
 bazel-test //tests/...
 bazel-test //tests/... --test_tag_filters=a
 bazel-test //tests/... --test_tag_filters=-b
+
+bazel-test //suites:all_tests
+bazel-test //suites:all_tests --test_tag_filters=a
+bazel-test //suites:all_tests --test_tag_filters=-b
