b833082c8c14cf718827b07eec967b246d703a10
Eric Cousineau <eric.cousineau@tri.global>
2022-11-04 15:57:04 -0400

    merp

---
+++ python/video_cat.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def main():
+    size_a = np.array([1668, 938])
+    size_b = np.array([1920, 1080])
+
+    print(size_a + size_b)
+
+
+assert __name__ == "__main__"
+main()
