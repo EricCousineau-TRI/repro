d87f44d5e78133dfcb6bdb4235ed1f48d25ab051
Eric Cousineau <eric.cousineau@tri.global>
2022-12-21 09:28:46 -0500

    blarg

--- cpp/lambda_const_this_capture.cc
+++ cpp/lambda_const_this_capture.cc
@@ -2,15 +2,18 @@ struct MyThing {
   int value{};
 
   auto MakeLambda() {
-    return [const this]() {
-      return value;
+    // TODO(eric.cousineau): Better method for this?
+    const auto& self = *this;
+    return [&self]() {
+      // self.value = 3;  // should fail compilation
+      return self.value;
     };
   }
-}
+};
 
 int main() {
-  MyThing{.value = 10};
-  auto lambda = MakeLambda();
+  MyThing my_thing{.value = 10};
+  auto lambda = my_thing.MakeLambda();
   if (lambda() == 10) {
     return 0;
   } else {
