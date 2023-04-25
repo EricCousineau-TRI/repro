469a9eb11fc700c93ef1386ccaec22aa13284b55
Eric Cousineau <eric.cousineau@tri.global>
2023-04-25 14:20:47 -0400

    sigh

--- drake_stuff/control_study/control_study/main.py
+++ drake_stuff/control_study/control_study/main.py
@@ -188,10 +188,11 @@ def make_panda_limits(plant):
     plant_limits = PlantLimits.from_plant(plant)
     # Avoid elbow lock.
     plant_limits.q.upper[3] = np.deg2rad(-15.0)
-    plant_limits.v = plant_limits.v.scaled(0.8)
+    plant_limits.v = plant_limits.v.scaled(0.9)
     # Scale down velocities... a lot.
     # plant_limits.v = plant_limits.v.scaled(0.5)
     # plant_limits.vd = plant_limits.vd.scaled(np.inf)
+    plant_limits.u = plant_limits.u.scaled(0.95)
     # plant_limits.u = plant_limits.u.scaled(np.inf)
     return plant_limits
 
@@ -217,7 +218,7 @@ def make_controller_qp_costs(plant, frame_W, frame_G):
         gains=make_osc_gains(),
         plant_limits=make_panda_limits(plant),
         acceleration_bounds_dt=CONTROL_DT,
-        posture_weight=0.1,
+        posture_weight=0.01,
         # split_costs=[1.0, 1.0],
         split_costs=None,
     )
@@ -231,11 +232,12 @@ def make_controller_qp_constraints(plant, frame_W, frame_G):
         gains=make_osc_gains(),
         plant_limits=make_panda_limits(plant),
         acceleration_bounds_dt=CONTROL_DT,
-        # posture_weight=0.01,
-        posture_weight=1.0,
-        use_natural_weights=True,
+        posture_weight=0.01,
+        use_natural_weights=False,
+        # posture_weight=1.0,
+        # use_natural_weights=True,
     )
-    controller.check_limits = False
+    # controller.check_limits = False
     return controller
 
 
@@ -250,7 +252,7 @@ def main():
     make_controllers = {
         # "osc": make_controller_osc,
         "qp costs": make_controller_qp_costs,
-        # "qp constr": make_controller_qp_constraints,
+        "qp constr": make_controller_qp_constraints,
     }
     for scenario_name, scenario in scenarios.items():
         print(scenario_name)
