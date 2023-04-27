d356f8420975169564911406db67957f9a01484a
Eric Cousineau <eric.cousineau@tri.global>
2023-04-26 22:18:26 -0400

    seems decent, but can go infeasible

--- drake_stuff/control_study/control_study/main.py
+++ drake_stuff/control_study/control_study/main.py
@@ -257,9 +257,9 @@ def np_print_more_like_matlab():
 def main():
     np_print_more_like_matlab()
     scenarios = {
-        "slow": run_slow_waypoints,
+        # "slow": run_slow_waypoints,
         # "rot": run_rotation_coupling,
-        "fast": run_fast_waypoints,
+        # "fast": run_fast_waypoints,
         "fast singular": run_fast_waypoints_singular,
     }
     make_controllers = {
