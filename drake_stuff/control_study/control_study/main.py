7afa3dc1eeeed5ca3e3fece9ea21604345245abb
Eric Cousineau <eric.cousineau@tri.global>
2023-05-03 00:26:50 -0400

    oooh clp is great on this

--- drake_stuff/control_study/control_study/main.py
+++ drake_stuff/control_study/control_study/main.py
@@ -313,9 +313,9 @@ def np_print_more_like_matlab():
 def main():
     np_print_more_like_matlab()
     scenarios = {
-        # "slow": run_slow_waypoints,
-        # "rot": run_rotation_coupling,
-        # "fast": run_fast_waypoints,
+        "slow": run_slow_waypoints,
+        "rot": run_rotation_coupling,
+        "fast": run_fast_waypoints,
         "fast singular": partial(run_fast_waypoints_singular, rotate=False),
         "fast singular rot": partial(run_fast_waypoints_singular, rotate=True),
     }
