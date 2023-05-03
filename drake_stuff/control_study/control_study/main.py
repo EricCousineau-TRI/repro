0b9299d646e96b891ffe79d5c4b37ed6d7744664
Eric Cousineau <eric.cousineau@tri.global>
2023-05-02 22:17:59 -0400

    35 deg works

--- drake_stuff/control_study/control_study/main.py
+++ drake_stuff/control_study/control_study/main.py
@@ -229,7 +229,8 @@ def make_panda_limits(plant):
     # plant_limits.q.upper[3] = np.deg2rad(-15.0)
     # plant_limits.q.upper[3] = np.deg2rad(-20.0)
     # plant_limits.q.upper[3] = np.deg2rad(-25.0)
-    plant_limits.q.upper[3] = np.deg2rad(-45.0)
+    plant_limits.q.upper[3] = np.deg2rad(-35.0)
+    # plant_limits.q.upper[3] = np.deg2rad(-45.0)
     # plant_limits.q.lower[6] = np.deg2rad(-30.0)
     # plant_limits.q.upper[6] = np.deg2rad(30.0)
     plant_limits.v = plant_limits.v.scaled(0.99)
@@ -309,9 +310,9 @@ def np_print_more_like_matlab():
 def main():
     np_print_more_like_matlab()
     scenarios = {
-        "slow": run_slow_waypoints,
-        "rot": run_rotation_coupling,
-        "fast": run_fast_waypoints,
+        # "slow": run_slow_waypoints,
+        # "rot": run_rotation_coupling,
+        # "fast": run_fast_waypoints,
         "fast singular": partial(run_fast_waypoints_singular, rotate=False),
         "fast singular rot": partial(run_fast_waypoints_singular, rotate=True),
     }
