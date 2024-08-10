4f02f71f27b06167be313f08d1fd5a093e750630
Eric Cousineau <eric.cousineau@tri.global>
2024-08-09 20:19:52 -0400

    add some stats

--- ros/ros2_rclpy_rclcpp_perf/src/pub_sub.py
+++ ros/ros2_rclpy_rclcpp_perf/src/pub_sub.py
@@ -1,3 +1,5 @@
+import ctypes
+import functools
 import multiprocessing as mp
 import os
 import signal
@@ -5,6 +7,8 @@ import time
 
 import rclpy
 from ros2_rclpy_rclcpp_perf.msg import ExampleStatus, ExampleCommand
+
+from running_stats import TimingStats, header_timing_stats, format_timing_stats
 
 RATE_HZ = 1000.0
 
@@ -19,68 +23,98 @@ def make_command():
     return msg
 
 
-def wrap_rclpy(main):
+def wrap_rclpy(func, *args, **kwargs):
     rclpy.init(signal_handler_options=rclpy.SignalHandlerOptions.NO)
     try:
-        main()
+        func(*args, **kwargs)
     except KeyboardInterrupt:
         pass
     finally:
         rclpy.shutdown()
 
 
-def pub_main():
+def pub_main(count=1, ready_flag=None):
     node = rclpy.create_node("pub")
     executor = rclpy.executors.SingleThreadedExecutor()
     executor.add_node(node)
 
-    pub_status = node.create_publisher(
-        ExampleStatus, "/status", 1
-    )
-    pub_command = node.create_publisher(
-        ExampleCommand, "/command", 1
-    )
+    pub_status = [None] * count
+    stats_status = [None] * count
+    pub_command = [None] * count
+    stats_command = [None] * count
+    for i in range(count):
+        pub_status[i] = node.create_publisher(
+            ExampleStatus, f"/status_{i}", 1
+        )
+        stats_status[i] = TimingStats()
+        pub_command[i] = node.create_publisher(
+            ExampleCommand, f"/command_{i}", 1
+        )
+        stats_command[i] = TimingStats()
 
-    # rate = node.create_rate(RATE_HZ)
     rate = LoopRate(RATE_HZ)
     print("Pub running")
+    t_total_start = time.perf_counter()
+    if ready_flag is not None:
+        ready_flag.value = 1
     try:
         while rclpy.ok():
-            pub_status.publish(make_status())
-            pub_command.publish(make_command())
+            for i in range(count):
+                pub_status[i].publish(make_status())
+                stats_status[i].tick()
+                pub_command[i].publish(make_command())
+                stats_command[i].tick()
             executor.spin_once(timeout_sec=50e-6)
             rate.sleep()
     finally:
-        print("Pub done")
+        dt_total = time.perf_counter() - t_total_start
+        print(f"Pub done after {dt_total:.3g} sec")
+        print_stats_status_and_command("pub.", stats_status, stats_command)
+        print()
 
 
-def sub_main():
+def sub_main(count=1, ready_flag=None):
     node = rclpy.create_node("sub")
     executor = rclpy.executors.SingleThreadedExecutor()
     executor.add_node(node)
 
-    def status_callback(msg):
-        pass
+    stats_status = [None] * count
+    stats_command = [None] * count
 
-    def command_callback(msg):
-        pass
+    def callback_status(i, msg):
+        stats_status[i].tick()
 
-    sub_status = node.create_subscription(
-        ExampleStatus, "/status", status_callback, 1
-    )
-    sub_command = node.create_subscription(
-        ExampleCommand, "/command", command_callback, 1
-    )
+    def callback_command(i, msg):
+        stats_command[i].tick()
 
-    # rate = node.create_rate(RATE_HZ)
+    sub_status = [None] * count
+    sub_command = [None] * count
+    for i in range(count):
+        callback_status_i = functools.partial(callback_status, i)
+        stats_status[i] = TimingStats()
+        sub_status[i] = node.create_subscription(
+            ExampleStatus, f"/status_{i}", callback_status_i, 1
+        )
+        callback_command_i = functools.partial(callback_command, i)
+        stats_command[i] = TimingStats()
+        sub_command[i] = node.create_subscription(
+            ExampleCommand, f"/command_{i}", callback_command_i, 1
+        )
+
     rate = LoopRate(RATE_HZ)
     print("Sub running")
+    t_total_start = time.perf_counter()
+    if ready_flag is not None:
+        ready_flag.value = 1
     try:
         while rclpy.ok():
             executor.spin_once(timeout_sec=50e-6)
             rate.sleep()
     finally:
-        print("Sub done")
+        dt_total = time.perf_counter() - t_total_start
+        print(f"Sub done after {dt_total:.3g} sec")
+        print_stats_status_and_command("sub.", stats_status, stats_command)
+        print()
 
 
 class LoopRate:
@@ -102,28 +136,74 @@ class LoopRate:
             self.t_next = time.perf_counter() + self.dt
 
 
-def main():
-    procs = [
-        mp.Process(target=wrap_rclpy, args=[pub_main]),
-        mp.Process(target=wrap_rclpy, args=[sub_main]),
-    ]
-    t_end = time.time() + 5.0
+def print_stats_status_and_command(prefix, stats_status, stats_command):
+    count = len(stats_status)
+    assert len(stats_command) == count
+    fmt_stats = "{:>10}{}"
+    header_text = header_timing_stats()
+    lines = []
+    lines.append(fmt_stats.format("", header_text))
+    for i in range(count):
+        status_text = format_timing_stats(stats_status[i].stats)
+        lines.append(fmt_stats.format(f"{prefix}status[{i}]", status_text))
+        command_text = format_timing_stats(stats_command[i].stats)
+        lines.append(fmt_stats.format(f"{prefix}command[{i}]", command_text))
+    print("\n".join(lines))
 
-    for proc in procs:
-        proc.start()
+
+class MpProcessGroup:
+    def __init__(self, procs):
+        self.procs = procs
+
+    def start(self):
+        for proc in self.procs:
+            proc.start()
+
+    def poll(self):
+        for proc in self.procs:
+            assert proc.is_alive()
+
+    def close(self):
+        for proc in self.procs:
+            if proc.is_alive():
+                os.kill(proc.pid, signal.SIGINT)
+                # Can hang here.
+                proc.join()
+
+
+def main():
+    count = 5
+    duration_sec = 1.0
+
+    pub_ready = mp.Value(ctypes.c_int)
+    pub_ready.value = 0
+    sub_ready = mp.Value(ctypes.c_int)
+    sub_ready.value = 0
+
+    procs = MpProcessGroup([
+        mp.Process(target=wrap_rclpy, args=[pub_main, count, pub_ready]),
+        mp.Process(target=wrap_rclpy, args=[sub_main, count, sub_ready]),
+    ])
+    procs.start()
+
+    while True:
+        if pub_ready.value != 0 and sub_ready.value != 0:
+            break
+        time.sleep(0.001)
+
+    print(f"Pub + sub ready. Stopping after {duration_sec}")
+    print()
+
+    t_end = time.time() + duration_sec
 
     try:
         while time.time() < t_end:
-            for proc in procs:
-                assert proc.is_alive()
+            procs.poll()
             time.sleep(1 / RATE_HZ)
     except KeyboardInterrupt:
         pass
     finally:
-        for proc in procs:
-            if proc.is_alive():
-                os.kill(proc.pid, signal.SIGINT)
-                proc.join()
+        procs.close()
 
 
 if __name__ == "__main__":
---
+++ ros/ros2_rclpy_rclcpp_perf/src/running_stats.py
@@ -0,0 +1,115 @@
+from math import sqrt
+import time
+
+
+class RunningStats:
+    """
+    Simple implementation of Welford's online statistics computations (like
+    PyTorch's BatchNorm, etc.):
+    https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm  # noqa
+
+    Note:
+        This makes an attempt to preserve some precision (by the nautre of
+        Welford's approach), but may have some minor issues.
+    """
+    def __init__(self):
+        self._count = 0
+        self._mean = 0.0
+        self._m2 = 0.0
+        self._min = 0.0
+        self._max = 0.0
+
+    def add(self, x):
+        """Adds new data point and updates internal state."""
+        if self._count == 0:
+            self._min = x
+            self._max = x
+        else:
+            if x < self._min:
+                self._min = x
+            if x > self._max:
+                self._max = x
+        self._count += 1
+        delta = x - self._mean
+        self._mean += delta / self._count
+        delta2 = x - self._mean
+        self._m2 += delta * delta2
+
+    def count(self):
+        """Number of data points."""
+        return self._count
+
+    def sum(self):
+        """Sum of all data points."""
+        return self._mean * self._count
+
+    def min(self):
+        """Min of all data points; 0 if no data points present."""
+        return self._min
+
+    def max(self):
+        """Max of all data points; 0 if no data points present."""
+        return self._max
+
+    def mean(self):
+        """Mean / average of data points."""
+        return self._mean
+
+    def m2(self):
+        return self._m2
+
+    def variance(self):
+        """
+        Population variance of all data points; 0 if no data points
+        present.
+        """
+        if self._count > 0:
+            return self._m2 / self._count
+        else:
+            return 0.0
+
+    def stddev(self):
+        """Standard deviation, or sqrt(variance). Population, not variance."""
+        return sqrt(self.variance())
+
+
+class TimingStats:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.stats = RunningStats()
+        self._t_prev = None
+
+    def tick(self):
+        t_now = time.perf_counter()
+        if self._t_prev is not None:
+            dt = t_now - self._t_prev
+            self.stats.add(dt)
+        self._t_prev = t_now
+
+
+def header_timing_stats():
+    fmt_string_title = "{:>15}{:>15}{:>10}{:>15}{:>15}"
+    header_text = fmt_string_title.format(
+        "Mean Time (s)",
+        "Norm stddev",
+        "Samples",
+        "Min (s)",
+        "Max (s)",
+    )
+    return header_text
+
+
+def format_timing_stats(stats):
+    fmt_string = "{:>15.7g}{:>15.7g}{:>10}{:>15.7g}{:>15.7g}"
+    norm_stddev = stats.stddev()
+    if stats.mean() != 0.0:
+        norm_stddev /= stats.mean()
+    return fmt_string.format(
+        stats.mean(),
+        norm_stddev,
+        stats.count(),
+        stats.min(),
+        stats.max(),
+    )
