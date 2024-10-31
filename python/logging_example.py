9aa9e192e30dd193058eef3660b4cb450d95feba
Eric Cousineau <eric.cousineau@tri.global>
2024-10-31 15:54:13 -0400

    fixed

--- python/logging_example.py
+++ python/logging_example.py
@@ -4,15 +4,18 @@ import sys
 
 
 def get_logger():
+    base_name = "example"
+    # Use unique name to avoid accumulating handlers into the same logger.
     uid = str(uuid.uuid4())[:8]
-    logger = logging.getLogger(f"example[{uid}]")
+    name = f"{base_name}[{uid}]"
+    logger = logging.getLogger(name)
+
     # Configure terse stream handler.
     stream_handler = logging.StreamHandler(stream=sys.stderr)
     stream_formatter = logging.Formatter("%(message)s")
     stream_handler.setFormatter(stream_formatter)
     stream_handler.setLevel(logging.INFO)
     logger.addHandler(stream_handler)
-
 
     # Configure verbose file handler.
     file_handler = logging.FileHandler(
@@ -24,6 +27,10 @@ def get_logger():
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
     file_handler.setLevel(logging.DEBUG)
+
+    # We must set top-level logging level to ensure handlers fire.
+    min_level = min(x.level for x in logger.handlers)
+    logger.setLevel(min_level)
 
     return logger
 
@@ -45,23 +52,26 @@ if __name__ == "__main__":
     main()
 
 """
-Currently, doesn't respect level?
-
-console:
-
+[ stderr ]
+info
 warn
 error
 
+info
 warn
 error
 
 
-file:
+[ file: tail -f /tmp/example_log.txt ]
 
 tail: /tmp/example_log.txt: file truncated
-2024-10-31 15:47:39,300 - example[3b20f328] - WARNING - warn
-2024-10-31 15:47:39,300 - example[3b20f328] - ERROR - error
+2024-10-31 15:53:31,117 - example[029f530a] - DEBUG - debug
+2024-10-31 15:53:31,117 - example[029f530a] - INFO - info
+2024-10-31 15:53:31,117 - example[029f530a] - WARNING - warn
+2024-10-31 15:53:31,117 - example[029f530a] - ERROR - error
 tail: /tmp/example_log.txt: file truncated
-2024-10-31 15:47:39,300 - example[7ca514ac] - WARNING - warn
-2024-10-31 15:47:39,300 - example[7ca514ac] - ERROR - error
+2024-10-31 15:53:31,118 - example[b45997ba] - DEBUG - debug
+2024-10-31 15:53:31,118 - example[b45997ba] - INFO - info
+2024-10-31 15:53:31,118 - example[b45997ba] - WARNING - warn
+2024-10-31 15:53:31,118 - example[b45997ba] - ERROR - error
 """
